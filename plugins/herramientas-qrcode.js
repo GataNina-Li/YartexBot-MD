ab4000176372dc225ca1ad845013819ec475b4a9
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-10 00:43:38 -0600

    Delete plugins directory

--- plugins/herramientas-qrcode.js
+++
@@ -1,9 +0,0 @@
-import { toDataURL } from 'qrcode'
-let handler = async (m, { text, conn }) => {
-if (!text) throw `*⚠️ INGRESE EL TEXTO QUE QUIERE CONVERTIR EN CÓDIGO QR*`
-conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '*CÓDIGO QR 📍*', fliveLoc, m)
-}
-handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
-handler.tags = ['tools']
-handler.command = /^qr(code)?$/i
-export default handler
--- plugins/owner-unbanchat.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m) => {
-global.db.data.chats[m.chat].isBanned = false
-m.reply('*⚠️ ESTE CHAT FUE  DESBANEADO*\n\n*📑 AHORA ESTOY DISPONIBLE*')
-}
-handler.help = ['unbanchat']
-handler.tags = ['owner']
-handler.command = /^unbanchat$/i
-export default handler
