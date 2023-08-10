e459817b7c9efcdc41064d3c104447500614350a
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-08-10 14:02:10 -0300

    Add files via upload

---
+++ plugins/juegos/tops.js
@@ -0,0 +1,52 @@
+import util from 'util'
+import path from 'path'
+let user = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata, command, conn, participants }) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b = ps.getRandom()
+let c = ps.getRandom()
+let d = ps.getRandom()
+let e = ps.getRandom()
+let f = ps.getRandom()
+let g = ps.getRandom()
+let h = ps.getRandom()
+let i = ps.getRandom()
+let j = ps.getRandom()
+
+if (command == 'topgays') {
+let top = `*🌈TOP 10 GAYS/LESBIANAS DEL GRUPO🌈*
+    
+*_1.- ${user(a)}_*
+*_2.- ${user(b)}_*
+*_3.- ${user(c)}_*
+*_4.- ${user(d)}_*
+*_5.- ${user(e)}_*
+*_6.- ${user(f)}_*
+*_7.- ${user(g)}_*
+*_8.- ${user(h)}_*
+*_9.- ${user(i)}_*
+*_10.- ${user(j)}_*`
+m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j]})
+}
+    
+if (command == 'topotakus') {
+let top = `*🌸 TOP 10 OTAKUS DEL GRUPO 🌸*
+    
+*_1.- ${user(a)}_*
+*_2.- ${user(b)}_*
+*_3.- ${user(c)}_*
+*_4.- ${user(d)}_*
+*_5.- ${user(e)}_*
+*_6.- ${user(f)}_*
+*_7.- ${user(g)}_*
+*_8.- ${user(h)}_*
+*_9.- ${user(i)}_*
+*_10.- ${user(j)}_*`
+m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j]})
+}}
+handler.help = handler.command = ['topgays','topotakus']
+handler.tags = ['game']
+handler.group = true
+handler.register = true 
+export default handler  
