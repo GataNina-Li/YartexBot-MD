d92ce2298305b33bfd5694c189dcdd5c1300c725
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-08-10 11:31:48 -0300

    Create google.js

---
+++ plugins/Buscadores/google.js
@@ -0,0 +1,33 @@
+import { googleIt } from '@bochilteam/scraper'
+let handler = async (m, { conn, command, args, usedPrefix }) => {
+const fetch = (await import('node-fetch')).default
+let full = /f$/i.test(command)
+let text = args.join` ` 
+if (!text) return conn.reply(m.chat, `*⚠️ ESCRIBA LO QUE QUIERE BUSCAR*\n\n💡 EJEMPLO:\n*${usedPrefix + command} Curiosity*`, m)
+let pp = './media/menus/Menu1.jpg'
+let url = 'https://google.com/search?q=' + encodeURIComponent(text)
+let search = await googleIt(text)
+let msg = search.articles.map(({
+// header,
+title,
+url,
+description
+}) => {
+return `*🔍 ${title}*\n\n💻 ${url}\n\n📡 _${description}_\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈`
+}).join('\n\n')
+
+
+try {
+let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).arrayBuffer()
+if (/<!DOCTYPE html>/i.test(ss.toBuffer().toString())) throw ''
+await conn.sendFile(m.chat, ss, 'error.png', url + '\n\n' + msg, m)
+} catch (e) {
+m.reply(msg)
+}}
+handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
+handler.tags = ['internet']
+handler.command = /^googlef?$/i
+handler.exp = 40
+handler.exp = 3
+handler.register = true
+export default handler
