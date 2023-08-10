b9284bf3dd2933fa039983dc4220bee8e1084e14
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-10 00:22:56 -0600

    © CuriosityBot-MD

--- funciones/_templateResponse.js
+++ funciones/_templateResponse.js
@@ -14,21 +14,21 @@ return
 let id = m.message.buttonsResponseMessage?.selectedButtonId || m.message.templateButtonReplyMessage?.selectedId || m.message.listResponseMessage?.singleSelectReply?.selectedRowId
 let text = m.message.buttonsResponseMessage?.selectedDisplayText || m.message.templateButtonReplyMessage?.selectedDisplayText || m.message.listResponseMessage?.title
 let isIdMessage = false, usedPrefix
-for (let name in global.plugins) {
-let plugin = global.plugins[name]
-if (!plugin)
+for (let name in global.funciones) {
+let funcion = global.funciones[name]
+if (!funcion)
 continue
-if (plugin.disabled)
+if (funcion.disabled)
 continue
 if (!opts['restrict'])
-if (plugin.tags && plugin.tags.includes('admin'))
+if (funcion.tags && plugin.tags.includes('admin'))
 continue
-if (typeof plugin !== 'function')
+if (typeof funcion !== 'function')
 continue
-if (!plugin.command)
+if (!funcion.command)
 continue
 const str2Regex = str => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
-let _prefix = plugin.customPrefix ? plugin.customPrefix : this.prefix ? this.prefix : global.prefix
+let _prefix = funcion.customPrefix ? funcion.customPrefix : this.prefix ? this.prefix : global.prefix
 let match = (_prefix instanceof RegExp ? // RegExp Mode?
 [[_prefix.exec(id), _prefix]] :
 Array.isArray(_prefix) ? // Array?
@@ -46,15 +46,15 @@ if ((usedPrefix = (match[0] || '')[0])) {
 let noPrefix = id.replace(usedPrefix, '')
 let [command] = noPrefix.trim().split` `.filter(v => v)
 command = (command || '').toLowerCase()
-let isId = plugin.command instanceof RegExp ? // RegExp Mode?
-plugin.command.test(command) :
-Array.isArray(plugin.command) ? // Array?
-plugin.command.some(cmd => cmd instanceof RegExp ? // RegExp in Array?
+let isId = funcion.command instanceof RegExp ? // RegExp Mode?
+funcion.command.test(command) :
+Array.isArray(funcion.command) ? // Array?
+funcion.command.some(cmd => cmd instanceof RegExp ? // RegExp in Array?
 cmd.test(command) :
 cmd === command
 ) :
-typeof plugin.command === 'string' ? // String?
-plugin.command === command :
+typeof funcion.command === 'string' ? // String?
+funcion.command === command :
 false
 if (!isId)
 continue
