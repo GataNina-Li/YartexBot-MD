bbfa8eddbd4565ad664801dfca1f0882f3a630d8
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-10 18:13:43 -0600

    Update toptt.js

--- plugins/convertidor/toptt.js
+++ plugins/convertidor/toptt.js
@@ -1,4 +1,3 @@
-import { toPTT } from '../lib/converter.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let q = m.quoted ? m.quoted : m 
