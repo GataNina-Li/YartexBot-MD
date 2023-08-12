cd2fbdb78840d2978cc615901707af0d51f10ac7
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-11 23:42:21 -0600

    Update playstore.js

--- plugins/internet/playstore.js
+++ plugins/internet/playstore.js
@@ -1,4 +1,4 @@
-import hxz from 'hxz-api'
+/*import hxz from 'hxz-api'
 import fetch from 'node-fetch'
 import axios from 'axios'
 import cheerio from 'cheerio'
@@ -43,4 +43,34 @@ name: $(x).find('span.DdYX5').text(),
 rate: $(x).find('span.w2kbF').text(),
 dev: $(x).find('span.wMUdtb').text(),
 image: $(x).find('.j2FCNc img').attr('src').replace('s64', 's256'),
-link: 'https://play.google.com' + $(x).find('a.Si6A0c').attr('href')}})}
+link: 'https://play.google.com' + $(x).find('a.Si6A0c').attr('href')}})}*/
+
+import fetch from 'node-fetch';
+import translate from '@vitalets/google-translate-api';
+const handler = async (m, {conn, text, args}) => {
+  if (!args[0]) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙰𝙿𝙺 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`;
+  try {
+    const enc = encodeURIComponent(text);
+    const json = await fetch(`https://latam-api.vercel.app/api/playstore?apikey=nekosmic&q=${enc}`);
+    const gPlay = await json.json();
+
+    const mystic = await translate(`${gPlay.descripcion}`, {to: 'es', autoCorrect: true});
+    if (!gPlay.titulo) return m.reply(`*⚠️ SIN RESULTADOS*`);
+    conn.sendMessage(m.chat, {image: {url: gPlay.imagen}, caption: `🔍 Resultado: ${gPlay.titulo}
+🧬 Identificador: ${gPlay.id}
+⛓️ Link: ${gPlay.link}
+🖼️ Imagen: ${gPlay.imagen}
+✍️ Desarrollador: ${gPlay.desarrollador}
+📜 Descripcion: ${mystic.text}
+💲 Moneda: ${gPlay.moneda}
+🎭 Gratis?: ${gPlay.gratis}
+💸 Precio: ${gPlay.precio}
+📈 Puntuacion: ${gPlay.puntuacion}`}, {quoted: m});
+  } catch {
+    await m.reply('*⚠️ ERROR, VUELVA A INTENTAR*')
+  }
+};
+handler.help = ['playstore <aplicacion>'];
+handler.tags = ['internet'];
+handler.command = /^(playstore)$/i;
+export default handler;
