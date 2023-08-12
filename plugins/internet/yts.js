432a1dddc4c43f9928d056194238ff6b70471ca5
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-11 22:22:08 -0600

    © CuriosityBot-MD

--- plugins/internet/yts.js
+++ plugins/internet/yts.js
@@ -16,6 +16,6 @@ conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, fkontak, m)
 handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
 handler.tags = ['internet']
 handler.command = /^playlist|ytbuscar|yts(earch)?$/i
-handler.exp = 70;
-handler.limit = 2;
-handler.register = true;
+handler.exp = 70
+handler.limit = 2
+handler.register = true
