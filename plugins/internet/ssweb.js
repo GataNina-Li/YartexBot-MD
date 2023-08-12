d49ace49baa8d674f302bda36081bb1783affe9e
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-12 11:03:13 -0600

    © CuriosityBot-MD

--- plugins/internet/ssweb.js
+++ plugins/internet/ssweb.js
@@ -25,41 +25,37 @@ m.reply("*🚀 C A R G A N D O*")
  handler.register = true
  handler.limit = false
 
- export default handler 
+ export default handler
   
- async function ssweb(url, device = 'desktop'){ 
-      return new Promise((resolve, reject) => { 
-           const base = 'https://www.screenshotmachine.com' 
-           const param = { 
-             url: url, 
-             device: device, 
-             cacheLimit: 0 
-           } 
-           axios({url: base + '/capture.php', 
-                method: 'POST', 
-                data: new URLSearchParams(Object.entries(param)), 
-                headers: { 
-                     'content-type': 'application/x-www-form-urlencoded; charset=UTF-8' 
-                } 
-           }).then((data) => { 
-                const cookies = data.headers['set-cookie'] 
-                if (data.data.status == 'success') { 
-                     axios.get(base + '/' + data.data.link, { 
-                          headers: { 
-                               'cookie': cookies.join('') 
-                          }, 
-                          responseType: 'arraybuffer' 
-                     }).then(({ data }) => { 
-                        let result = { 
-                             status: 200, 
-                             author: 'Ryzn', 
-                             result: data 
-                         } 
-                          resolve(result) 
-                     }) 
-                } else { 
-                     reject({ status: 404, author: 'Ryzn', message: data.data }) 
-                } 
-           }).catch(reject) 
-      }) 
- }
+ async function ssweb(url, device = 'desktop'){
+return new Promise((resolve, reject) => {
+const base = 'https://www.screenshotmachine.com'
+const param = {
+url: url,
+device: device,
+cacheLimit: 0
+}
+axios({url: base + '/capture.php',
+method: 'POST',
+data: new URLSearchParams(Object.entries(param)),
+headers: {
+'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+}
+}).then((data) => {
+const cookies = data.headers['set-cookie']
+if (data.data.status == 'success') {
+axios.get(base + '/' + data.data.link, {
+headers: {
+'cookie': cookies.join('')
+},
+responseType: 'arraybuffer'
+}).then(({ data }) => {
+let result = {
+status: 200,
+author: 'AzamiJs',
+result: data
+}
+resolve(result)})
+} else {
+reject({ status: 404, author: 'AzamiJs', message: data.data })}}
+}).catch(reject) })}
