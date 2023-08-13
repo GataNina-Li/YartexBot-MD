e38c1aa095dee2b3bd1157d899258c1bbd819970
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-13 10:22:06 -0600

    © CuriosityBot-MD

--- plugins/internet/lyrics.js
+++ plugins/internet/lyrics.js
@@ -2,7 +2,7 @@ import { lyrics, lyricsv2 } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
-if (!teks) throw `Use example ${usedPrefix}${command} hallo`
+if (!teks) throw `*⚠️ INGRESE EL NOMBRE DE UNA CANCIÓN*\n\n❕ EJEMPLO:\n*${usedPrefix + command}* another love`
 const result = await lyricsv2(teks).catch(async _ => await lyrics(teks))
 m.reply(`
 Lyrics *${result.title}*
