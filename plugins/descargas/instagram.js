1d1874cd1729d24af43b9a47fe2a845f09d4c66d
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-14 14:28:07 -0600

    © CuriosityBot-MD

--- plugins/descargas/instagram.js
+++ plugins/descargas/instagram.js
@@ -9,8 +9,8 @@ m.reply(global.wait)
 conn.sendMessage(m.chat, { video : { url : media }}, global.wm, m)  
 } catch { 
 try { 
-let curiosity = await instagramdl(args[0])  
-let media2 = curiosity.url 
+let res2 = await instagramdl(args[0])  
+let media2 = res2.url 
 return conn.sendFile(m.chat, media2, 'instagram.mp4', global.wm, m) 
 } catch { 
 conn.sendFile(m.chat,  media, '','', global.wm, m) } finally { 
