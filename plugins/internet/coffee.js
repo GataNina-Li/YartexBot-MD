e174235b367251471bcabcb0da6c9ab507abe505
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-14 12:10:34 -0600

    © CuriosityBot-MD

--- plugins/internet/coffee.js
+++ plugins/internet/coffee.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, isOwner, usedPrefix, command, args }) => {
 try {
 await m.reply(wait)
 let cofe = "https://coffee.alexflipnote.dev/random"
-await conn.sendFile(m.chat, cofe, "", "*☕ Random Café ☕*", m)
+await conn.sendFile(m.chat, cofe, "", "*☕ Café ☕*", m)
 } catch (e) {
 throw eror
 }}
