cc2b78f43f48cb04c6414a8bbcc0769dc3ce377a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-15 11:58:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -31,12 +31,12 @@ protoType()
 serialize()
 
 global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') {
-return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString()
-} global.__dirname = function dirname(pathURL) {
-return path.dirname(global.__filename(pathURL, true));
-} global.__require = function require(dir = import.meta.url) {
-  return createRequire(dir)
-}
+  return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString()
+}; global.__dirname = function dirname(pathURL) {
+  return path.dirname(global.__filename(pathURL, true));
+}; global.__require = function require(dir = import.meta.url) {
+  return createRequire(dir);
+};
 
 global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({...query, ...(apikeyqueryname ? {[apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name]} : {})})) : '')
 
