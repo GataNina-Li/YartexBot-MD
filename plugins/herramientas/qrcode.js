907ea164ae8753d844223ebe71aa1df2005eade6
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-17 10:17:07 -0600

    © CuriosityBot-MD

--- plugins/herramientas/qrcode.js
+++
@@ -1,9 +0,0 @@
-import { toDataURL } from 'qrcode'
-let handler = async (m, { text, conn }) => {
-if (!text) throw `*⚠️ INGRESE EL TEXTO QUE QUIERE CONVERTIR EN CÓDIGO QR*`
-conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '*[ CÓDIGO QR ✓ ]*', m)
-}
-handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
-handler.tags = ['tools']
-handler.command = /^qr(code)?$/i
-export default handler
