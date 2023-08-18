4151565276e482e17d4e927b16e5859f4ea86568
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-18 14:27:10 -0600

    © CuriosityBot-MD

--- plugins/command.js
+++ plugins/command.js
@@ -2,6 +2,7 @@ var handler = async (m, { conn, usedPrefix, command }) => {
 
 conn.reply(m.chat, `${wm}`, adReply, m)
 
+}
 handler.tags = ['pruebas']
 handler.command = ['sendmessage', 'reply', 'sendfile']
 handler.owner = true
