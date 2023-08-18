846e1f78018bbc2dab5eda81d52cf0d96f6a6198
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-18 14:22:29 -0600

    © CuriosityBot-MD

--- plugins/command.js
+++ plugins/command.js
@@ -1,1 +1,10 @@
+var handler = async (m, { conn, usedPrefix, command }) => { 
 
+conn.reply(m.chat, `${wm}`, adReply, m)
+
+handler.help = ['toimg (reply)']
+handler.tags = ['convertido']
+handler.command = ['sendmessage', 'reply', 'sendfile']
+handler.owner = true
+
+export default handler
