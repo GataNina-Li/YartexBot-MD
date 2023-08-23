e213c2cb70b1ce828e418d4dcef5f1b7db178b9d
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-08-23 11:11:33 -0600

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isAdmin }) => {  
 if (m.fromMe) return
-if (isAdmin) throw '*🍓HOLA CREADOR, USTED YA TIENE PODER EN ESTE GRUPO🍓*'
+if (isAdmin) throw '*[ 🍓 ] 𝙃𝙊𝙇𝘼 𝘾𝙍𝙀𝘼𝘿𝙊𝙍 𝙐𝙎𝙏𝙀𝘿 𝙔𝘼 𝙏𝙄𝙀𝙉𝙀 𝙋𝙊𝘿𝙀𝙍  𝙀𝙎𝙏𝙀 𝙂𝙍𝙐𝙋𝙊*'
 try {  
 await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 } catch {
