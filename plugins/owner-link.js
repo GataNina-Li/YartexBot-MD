84f71184386e05b7e741689b8a35ef8b84d7904b
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-23 16:47:09 -0600

    © CuriosityBot-MD

--- plugins/owner-link.js
+++ plugins/owner-link.js
@@ -10,8 +10,9 @@ try {
   let res = await conn.groupAcceptInvite(code)
   //await m.reply(`${JSON.stringify(res, null, 1)}`)
   await m.reply(`*Haciendo cositas malas >:) . . .*`)
-  await conn.sendMessage(res, { text: grupo, mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: fliveLoc })
-  await conn.sendMessage(res, { text: grupo, mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: fliveLoc })
+  //await conn.sendMessage(res, { text: grupo, mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: fliveLoc })
+  //await conn.sendMessage(res, { text: grupo, mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: fliveLoc })
+  await conn.relayMessage(res, {extendedTextMessage:{text: 'Mario.js', contextInfo: { externalAdReply: { showAdAttribution: true, title: `Mario`, body: wm, sourceUrl: yt, thumbnail: ig }, mentionedJid: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }}}, { quoted: fakegif })
   await conn.groupLeave(res)
   await m.reply(`*Ya se spameo el grupo :D*`)
 } catch (e) {
