b02eca5f76c4399c4129843a5dec64effde82ceb
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-08-23 15:34:59 -0300

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -1,4 +1,4 @@
-let handler = m => m
+/*let handler = m => m
 handler.all = async function (m) {
 if (stopped === 'close' || !conn || !conn.user) return
 let setting = global.db.data.settings[this.user.jid]
@@ -25,4 +25,4 @@ let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
 return [d, ' Día(s) ️', h, ' Hora(s) ', m, ' Minuto(s)'].map(v => v.toString().padStart(2, 0)).join('')
-}
+}*/
