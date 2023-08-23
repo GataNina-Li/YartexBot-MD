cc6f4c371d15710405d78a280f5562ac47ef3d80
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-08-23 13:53:17 -0500

    Update herramientas-delete.js

--- plugins/herramientas-delete.js
+++ plugins/herramientas-delete.js
@@ -19,7 +19,7 @@ return conn.sendMessage(m.chat, {delete: m.quoted.vM.key})
 }
 
 handler.help = ["delete"]
-handler.tags = ["group"]
+handler.tags = ["dl"]
 handler.command = /^del(ete)?$/i
 handler.group = false
 handler.admin = true
