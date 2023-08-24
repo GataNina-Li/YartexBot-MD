6a0e523cb89c44b573d120e79fc1377f08c480e9
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-24 01:56:17 -0600

    © CuriosityBot-MD

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -2,7 +2,7 @@ import fs from "fs"
 let handler = m => m
 handler.all = async function (m) {
 let bot = `*🚀 Hola soy Bot*\nEn que te puedo ayudar?`
-let vn = [imagen5, imagen6, imagen7, imagen8, img1].getRandom()
+let vn = [imagen1, imagen2, imagen3, imagen4, img1].getRandom()
 let chat = global.db.data.chats[m.chat]
 const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {orderMessage: { itemCount : 2023, status: 1, surface : 1, message: 'Super WhatsApp Bot 🚀', orderTitle: 'Bang', thumbnail: fs.readFileSync('./storage/menus/Menu2.jpg'), sellerJid: '0@s.whatsapp.net'}}}  
 if (/^bot$/i.test(m.text) && !chat.isBanned) { 
