c05f90d6911a9401a5ad89e9707f34a0c6c41f25
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-08-23 18:31:38 -0600

    Update grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
-let kicktext = `*⚠️ ETIQUTE A LA PERSONA O RESPONDA AL MENSAJE DE LA PERSONA QUE QUIERE QUE ELIMINE*\n\n💡 EJEMPLO:\n*${usedPrefix + command} @${global.owner[0][0]}*`
+let kicktext = `*[⚠️] ETIQUTE A LA PERSONA O RESPONDA AL MENSAJE DE LA PERSONA QUE QUIERE QUE ELIMINE NO SOY ADIVINO😐*\n\n💡 EJEMPLO:\n*${usedPrefix + command} @${global.owner[0][0]}*`
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kicktext, m.chat, { mentions: conn.parseMention(kicktext)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
