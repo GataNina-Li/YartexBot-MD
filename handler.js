167b18e628d1f29ce9157d6bd713b6bae4beac63
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-24 01:16:31 -0600

    Update handler.js

--- handler.js
+++ handler.js
@@ -558,16 +558,6 @@ contextInfo: {
 "sourceUrl": 'https://www.xvideos.com' }}} 
 this.sendMessage(id, buttonMessage, fake)                          
 }}}
-                   /* let pp = './src/avatar_contact.png'
-                    try {
-                        pp = await this.profilePictureUrl(user, 'image')
-                    } catch (e) {
-                    } finally {
-                    let apii = await this.getFile(pp)
-                        text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user!').replace('@subject', await this.getName(id)).replace('@desc', groupMetadata.desc?.toString() || '*⚠️ ESTE GRUPO NO TIENE DESCRIPCIÓN ⚠️*') :
-                              (chat.sBye || this.bye || conn.bye || 'Bye, @user!')).replace('@user', '@' + user.split('@')[0])
-this.sendFile(id, apii.data, 'pp.jpg', text, null, false, { mentions: [user] })                             
-                   }*/
                 }
             }
             break
