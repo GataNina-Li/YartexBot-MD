e44f63ce8b07f968020fa3c0580865821b6431a2
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-23 23:30:05 -0600

    © CuriosityBot-MD

--- plugins/command.js
+++ plugins/command.js
@@ -1,26 +1,30 @@
+/* 
+* No es mio el plugin pero deja créditos 
+* https://github.com/AzamiJs/CuriosityBot-MD 
+*/
+
 import fetch from 'node-fetch'
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
-	if (!text) throw `*⚠️ INGRESE UN ENLACE DE TIKTOK QUE CONTENGA IMAGENES.*`
-	if (!(text.includes('http://') || text.includes('https://'))) return m.reply(`url invalid, please input a valid url. Try with add http:// or https://`)
-	if (!text.includes('tiktok.com')) return m.reply(`*⚠️ URL INVALIDA.*`)
-	try {
-		let res = await fetch(`https://api.lolhuman.xyz/api/tiktokslide?apikey=${global.lolkeysapi}&url=${text}`)
-		let anu = await res.json()
-		if (anu.status != '200') throw Error(anu.message)
-		anu = anu.result
-		if (anu.length == 0) throw Error('Error : no data')
-		let c = 0
-		for (let x of anu) {
-			if (c == 0) await conn.sendMessage(m.chat, { image: { url: x }, caption: `Enviando 1 de ${anu.length} diapositivas de imagen.\n_(El resto se enviará vía chat privado..)_` }, { quoted : m })
-			else await conn.sendMessage(m.sender, { image: { url: x } }, { quoted : m })
-			c += 1
-		}
-	} catch (e) {
-		console.log(e)
-		throw `*⚠️ ERROR, INTENTE DE NUEVO.*`
-	}
+if (!text) throw `*⚠️ INGRESE UN ENLACE DE TIKTOK QUE CONTENGA IMAGENES.*`
+if (!(text.includes('http://') || text.includes('https://'))) return m.reply(`url invalid, please input a valid url. Try with add http:// or https://`)
+if (!text.includes('tiktok.com')) return m.reply(`*⚠️ URL INVALIDA.*`)
+try {
+let res = await fetch(`https://api.lolhuman.xyz/api/tiktokslide?apikey=${global.lolkeysapi}&url=${text}`)
+let anu = await res.json()
+if (anu.status != '200') throw Error(anu.message)
+anu = anu.result
+if (anu.length == 0) throw Error('Error : no data')
+let c = 0
+for (let x of anu) {
+if (c == 0) await conn.sendMessage(m.chat, { image: { url: x }, caption: `Enviando 1 de ${anu.length} diapositivas de imagen.\n_(El resto se enviará vía chat privado..)_` }, { quoted : m })
+else await conn.sendMessage(m.sender, { image: { url: x } }, { quoted : m })
+c += 1
 }
+} catch (e) {
+console.log(e)
+throw `*⚠️ ERROR, INTENTE DE NUEVO.*`
+}}
 
 handler.menu = ['tiktokslide <url>']
 handler.tags = ['search']
@@ -30,38 +34,3 @@ handler.premium = true
 handler.limit = true
 
 export default handler
-
-
-/*import fetch from 'node-fetch' 
- let handler = async (m, { args, usedPrefix, command }) => { 
-     if (!args[0]) throw `✳️ Ingresa el link de Tiktok qué contenga images` 
-     if (!args[0].match(/tiktok/gi)) throw `❎ El enlace debe ser de TikTok` 
-  
-     m.react(rwait)    
-     try { 
-         let res = await fetch(`https://api.lolhuman.xyz/api/tiktokslide?apikey=${global.lolkeysapi}&url=${text}`) 
-         let data = await res.json() 
-  
-         let title = data.result.title 
-         let images = data.result.images 
-         let music = data.result.music.play_url 
-  
-         for (let tt of images) { 
-             //conn.sendFile(m.chat, tt.url, null, `▢ *Descripción:* ${title}`, m) 
-             conn.sendMessage(m.chat, { image: { url: tt.url }, caption: `▢ *Descripción:* ${title}` }, { quoted: m }) 
-         } 
-         conn.sendFile(m.chat, music, 'tiktok.mp3', '', m, null, { mimetype: 'audio/mp4' }) 
-         m.react(done) 
-  
-         } catch { 
-     m.reply(`❎ Error intenta más tarde`) 
- } 
-  
- } 
- handler.help = ['tiktokslide'] 
- handler.tags = ['dl'] 
- handler.command = ['tiktokslide', 'tiktokimg', 'ttslide'] 
- handler.diamond = true 
-  
- export default handler*/
-
