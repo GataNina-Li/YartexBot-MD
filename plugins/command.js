024463faf9586f076ec7433deeb4beb836a8762e
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-23 23:15:13 -0600

    © CuriosityBot-MD

--- plugins/command.js
+++ plugins/command.js
@@ -1,4 +1,38 @@
-import fetch from 'node-fetch' 
+import fetch from 'node-fetch'
+
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+	if (!text) throw `Example: ${usedPrefix + command} https://vt.tiktok.com/ZS81qJD5v/`
+	if (!(text.includes('http://') || text.includes('https://'))) return m.reply(`url invalid, please input a valid url. Try with add http:// or https://`)
+	if (!text.includes('tiktok.com')) return m.reply(`Invalid Tiktok URL.`)
+	try {
+		let res = await fetch(`https://api.lolhuman.xyz/api/tiktokslide?apikey=${global.lolkey}&url=${text}`)
+		let anu = await res.json()
+		if (anu.status != '200') throw Error(anu.message)
+		anu = anu.result
+		if (anu.length == 0) throw Error('Error : no data')
+		let c = 0
+		for (let x of anu) {
+			if (c == 0) await conn.sendMessage(m.chat, { image: { url: x }, caption: `Mengirim 1 dari ${anu.length} slide gambar.\n_(Sisanya akan dikirim via chat pribadi.)_` }, { quoted : m })
+			else await conn.sendMessage(m.sender, { image: { url: x } }, { quoted : m })
+			c += 1
+		}
+	} catch (e) {
+		console.log(e)
+		throw `invalid slideshow url / media isn't available.`
+	}
+}
+
+handler.menu = ['tiktokslide <url>']
+handler.tags = ['search']
+handler.command = /^((tt|tiktok)slide)$/i
+
+handler.premium = true
+handler.limit = true
+
+export default handler
+
+
+/*import fetch from 'node-fetch' 
  let handler = async (m, { args, usedPrefix, command }) => { 
      if (!args[0]) throw `✳️ Ingresa el link de Tiktok qué contenga images` 
      if (!args[0].match(/tiktok/gi)) throw `❎ El enlace debe ser de TikTok` 
@@ -30,3 +64,4 @@ import fetch from 'node-fetch'
  handler.diamond = true 
   
  export default handler
+*/
