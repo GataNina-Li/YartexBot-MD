e4c2db4c4ceb5a20058600043c91c73727643f77
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-24 23:22:06 -0600

    Update _Allfake.js

--- plugins/_Allfake.js
+++ plugins/_Allfake.js
@@ -1,4 +1,4 @@
-import fs from 'fs'
+/*import fs from 'fs'
 import fetch from 'node-fetch'
 import axios from 'axios'
 import moment from 'moment-timezone'
@@ -169,5 +169,5 @@ global.estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, .
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : +2022, status: 1, surface : 1, message: `${wm}\n${azami}`, orderTitle: 'Bang', thumbnail: imagen1, sellerJid: '0@s.whatsapp.net'}}}
 	
 } 
-export default handler
+export default handler*/
 
