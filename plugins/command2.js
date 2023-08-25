f194f9b35a9b10668d0a8f5e53e8044af906db2b
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-25 01:19:39 -0600

    Update command2.js

--- plugins/command2.js
+++ plugins/command2.js
@@ -14,7 +14,7 @@ const messageWithButtons = {
     ],
   };
 
-    conn.sendMessage(m.chat, messageWithButtons, MessageType.buttonsMessage)
+    conn.sendMessage(m.chat, messageWithButtons)
     
 let str = '*🍧 CuriosityBot-MD*'
 // await conn.sendUrl(m.chat, str, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen1, thumbnailUrl: imagen1, title: '🍓 Azami', }}})
