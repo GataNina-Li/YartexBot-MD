f807785df49d4c66d4bba408e5b89b597cb89416
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-08-24 20:22:53 -0600

    Update info-reporte.js

--- plugins/info-reporte.js
+++ plugins/info-reporte.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*⚠️ ESCRIBE EL REPORTE*\n\n💡 EJEMPLO\n*${usedPrefix + command} el comando ${usedPrefix}infobot no funciona.*`
+if (!text) throw `*[⚠️] ESCRIBE EL REPORTE*\n\n💡 EJEMPLO\n*${usedPrefix + command} el comando ${usedPrefix}infobot no funciona.*`
 if (text.length < 8) throw `⚠️ *Mínimo 10 caracteres para hacer El Reporte.*`
 if (text.length > 1000) throw `⚠️ *Máximo 1000 caracteres para hacer El Reporte.*`
 let teks = `*⚠️ REPORTE ⚠️*\n*📞 NÚMERO*\nWa.me/${m.sender.split`@`[0]}\n*📝 MENSAJE*\n${text}`
@@ -7,7 +7,7 @@ conn.reply('5214531173598@s.whatsapp.net', m.quoted ? teks + m.quoted.text : tek
 contextInfo: {
 mentionedJid: [m.sender]
 }})
-  m.reply(`*⚠️ El reporte ha sido enviado a mí Creadora. Tendrá una respuesta pronto. De ser Falso será Ignorado el reporte.*`)
+  m.reply(`*[⚠️] Exito el reporte ha sido enviado a mí Creadora. Tendrá una respuesta pronto. De ser Falso será Ignorado el reporte.*`)
 
 }
 
