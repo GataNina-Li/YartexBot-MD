fda6b80ee0ae1f3fbf9a318f33e8d0da912e06f9
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-25 04:30:21 -0600

    © CuriosityBot-MD

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -8,7 +8,7 @@ let handler = async (m, {conn, text}) => {
     }
   } catch {
     var update = execSync("git remote set-url origin https://github.com/AzamiJs/CuriosityBot-MD.git && git pull")
-    await conn.reply(m.chat, update.toString(), fproducto, m)
+    await conn.reply(m.chat, update.toString(), fakesmg, m)
   }
 };
 handler.help = ["update"]
