a5409d625619a58ac272ec9d32f576234dde86b9
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-25 00:29:16 -0600

    Update command2.js

--- plugins/command2.js
+++ plugins/command2.js
@@ -1,14 +1,8 @@
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
 let str = '*🍧 CuriosityBot-MD*'
- await conn.sendUrl(m.chat, str, m, { 
-   externalAdReply: { 
-      mediaType: 1, 
-      renderLargerThumbnail: true, 
-      thumbnail: imagen1, 
-      thumbnailUrl: imagen1, 
-      title: '🍓 Azami', 
-   }})
+ await conn.sendUrl(m.chat, str, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen1, thumbnailUrl: imagen1, title: '🍓 Azami', 
+ }})
 }
 
 handler.menu = ['tiktokslide <url>']
