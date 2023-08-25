e4228a6c7b9528497b5bc2dabdacbd8518743d43
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-25 03:54:15 -0600

    © CuriosityBot-MD

--- plugins/info-owner.js
+++ plugins/info-owner.js
@@ -1,6 +1,6 @@
 function handler(m) {
   const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), estilo, m)
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), fpay, m)
 
 }
 
