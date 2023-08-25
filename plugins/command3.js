30af083e828ae96729888a3e2d38869cc6ef3bf7
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-25 03:59:36 -0600

    Update command3.js

--- plugins/command3.js
+++ plugins/command3.js
@@ -1,5 +1,8 @@
 let handler = async (m, { conn, usedPrefix }) => {
-
+var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document'] 
+ var document = doc[Math.floor(Math.random() * doc.length)]     
+ let texto1 = `*https://github.com/AzamiJs/CuriosityBot-MD*`
+  
   m.reply('a')
   
 }
