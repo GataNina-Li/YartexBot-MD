d08cdb0e81dba083fea6cb956fed9f5ebcd1f939
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-24 21:01:01 -0600

    Update handler.js

--- handler.js
+++ handler.js
@@ -614,8 +614,7 @@ var document = doc[Math.floor(Math.random() * doc.length)]
         unreg: '⚠️ *REGÍSTRESE PARA USAR ESTA FUNCIÓN ESCRIBIENDO:*\n\n• */reg nombre.edad*\n\n*_💡 Ejemplo_* : */reg Azami.25*',
         restrict: '⚠️ *ESTA CARACTERÍSTICA ESTA DESACTIVADA*'
     }[type]
-    if (msg) return conn.sendMessage(msg, {mimetype: `application/${document}`})
-	    //conn.reply(m.chat, msg, m, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '👋 Hola!!', body: ucapan(), sourceUrl: global.paypal, thumbnail: imagen1 }}})
+    if (msg) return conn.reply(m.chat, msg, m, { contextInfo:{ forwardingScore: 2022, mimetype: `application/${document}`, isForwarded: true, externalAdReply: {title: '👋 Hola!!', body: ucapan(), sourceUrl: global.paypal, thumbnail: imagen1 }}})
 
 }
 
