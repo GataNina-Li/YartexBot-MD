5485508852ef980de82ca4d543075ac3b0fdeed3
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-25 12:18:26 -0600

    Update command3.js

--- plugins/command3.js
+++ plugins/command3.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, usedPrefix }) => {
+//let handler = async (m, { conn, usedPrefix }) => {
 /*  let git = '*🍧 Github*\nhttps://github.com/AzamiJs/CuriosityBot-MD'
   m.reply('a')
  await conn.sendUrl(m.chat, git, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen1, thumbnailUrl: imagen1, title: '\t\t\t\t\t\t\t᭡͡ᩬ🌵✩̣̣̣̣̣ͯ𝑪𝒖𝒓𝒊𝒐𝒔𝒊𝒕𝒚𝑩𝒐𝒕-𝑴𝑫᭡͡ᩬ🌵✩̣̣̣̣̣ͯ', }})
