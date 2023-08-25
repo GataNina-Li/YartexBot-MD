2aa0ec26044027597d41ee58ec4cf65b19091f3c
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-25 03:57:19 -0600

    Create command3.js

---
+++ plugins/command3.js
@@ -0,0 +1,8 @@
+let handler = async (m, { conn, usedPrefix }) => {
+
+}
+handler.tags =['info'] 
+handler.help = ['script'] 
+handler.command = ['sc', 'script'] 
+handler.register = true 
+export default handler
