179488cb6f1359caf0a11d1039cf8ce8e308bb5b
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-25 12:01:18 -0600

    © CuriosityBot-MD

--- plugins/command3.js
+++ plugins/command3.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix }) => {
   let git = '🗿'
   m.reply('a')
- await conn.sendUrl(m.chat, git, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen1, thumbnailUrl: imagen1, title: '😾 Script', }})
+ await conn.sendUrl(m.chat, git, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen1, thumbnailUrl: imagen1, title: '◯◡┈━─◡◡┈◯◡◡┈━─◡┈◯\n᭡͡ᩬ🌵✩̣̣̣̣̣ͯ𝑪𝒖𝒓𝒊𝒐𝒔𝒊𝒕𝒚𝑩𝒐𝒕-𝑴𝑫᭡͡ᩬ🌵✩̣̣̣̣̣ͯ\n◯◡┈━─◡◡┈◯◡◡┈━─◡┈◯', }})
  
 }
 
