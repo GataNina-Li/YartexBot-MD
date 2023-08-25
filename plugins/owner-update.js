91dcd43042614280fdc75d35889c9264d6c4a12c
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-25 04:37:49 -0600

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,15 +1,16 @@
 import {execSync} from "child_process"
 let handler = async (m, {conn, text}) => {
-  await conn.reply(m.chat, '❕ *A C T U A L I Z A N D O*', fkontak, m)
+  await conn.reply(m.chat, '❕ *A C T U A L I Z A N D O* ❕', fkontak, m)
   try {
     if (global.conn.user.jid == conn.user.jid) {
       let stdout = execSync("git pull" + (m.fromMe && text ? " " + text : ""))
-      await await await m.reply(update.toString())
+     await conn.reply(m.chat, update.toString(), fkontak, m)
+      //await await await m.reply(update.toString())
     }
   } catch {
    var update = execSync("git remote set-url origin https://github.com/AzamiJs/CuriosityBot-MD.git && git pull")
- await await await m.reply(update.toString())
-    //  await conn.reply(m.chat, update.toString(), fkontak, m)
+// await await await m.reply(update.toString())
+     await conn.reply(m.chat, update.toString(), fkontak, m)
   }
 }
 handler.help = ["update"]
