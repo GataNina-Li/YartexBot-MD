38dcbaa3eca4684032653a1c36530d91bfec1058
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-25 14:01:01 -0600

    © CuriosityBot-MD

--- handler.js
+++ handler.js
@@ -518,7 +518,8 @@ export async function participantsUpdate({ id, participants, action }) {
                     } catch (e) {
                     } finally {
                     let apii = await this.getFile(pp)
-                        text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user!').replace('@subject', await this.getName(id)).replace('@desc', groupMetadata.desc?.toString() || '*⚠️ ESTE GRUPO NO TIENE DESCRIPCIÓN ⚠️*') :
+                                       text = (chat.sDemote || this.sdemote || conn.sdemote || '@user ```is no longer Admin```')
+			    text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user!').replace('@date', global.fecha).replace('@time', global.tiempo).replace('@subject', await this.getName(id)).replace('@desc', groupMetadata.desc?.toString() || '*⚠️ ESTE GRUPO NO TIENE DESCRIPCIÓN ⚠️*') :
                               (chat.sBye || this.bye || conn.bye || 'Bye, @user!')).replace('@user', '@' + user.split('@')[0])
 this.sendFile(id, apii.data, 'pp.jpg', text, null, false, { mentions: [user] })                             
                    }
