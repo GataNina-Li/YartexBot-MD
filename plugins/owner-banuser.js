7d72889183282e98dae4848287b1483a1f03db66
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-08-24 21:46:42 -0600

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -6,7 +6,7 @@ if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 else who = m.chat
 let users = global.db.data.users
 users[who].banned = true
-m.reply('*☑️ EL USUARIO FUE BANEADO CON EXITO☑️*\n\n😔 ☑️EL USUARIO NO PODRA USAR CURIOSITY BOT☑️')    }
+m.reply('*[🛑] EL USUARIO FUE BANEADO CON EXITO*\n\n😔 🔰EL USUARIO NO PODRA USAR CURIOSITY BOT🔒')    }
 handler.tags = ['owner']
 handler.command = /^banuser$/i
 handler.rowner = true
