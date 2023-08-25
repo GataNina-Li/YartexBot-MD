f7c45988aa3c4a4e543119ea63e33c3b029c23cd
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-25 00:49:50 -0600

    Update command2.js

--- plugins/command2.js
+++ plugins/command2.js
@@ -1,9 +1,7 @@
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
 let str = '*🍧 CuriosityBot-MD*'
- await conn.sendUrl(m.chat, str, m, { externalAdReply: { toqued: fpay }, { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen1, thumbnailUrl: imagen1, title: '🍓 Azami',
-                                               
- }})
+ await conn.sendUrl(m.chat, str, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen1, thumbnailUrl: imagen1, title: '🍓 Azami', }}})
 }
 
 handler.command = /^((a|a)a)$/i
