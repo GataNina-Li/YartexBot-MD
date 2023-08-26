1aa451cedb51fb1f4322288ec2c9c9dbf43141ae
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-26 15:05:00 -0600

    Update command2.js

--- plugins/command2.js
+++ plugins/command2.js
@@ -1,5 +1,22 @@
 let handler = async(m, { conn, text, usedPrefix, command }) => {
-
+const ftrol = {
+    key : {
+    remoteJid: 'status@broadcast',
+    participant : '0@s.whatsapp.net'
+    },
+    message: {
+    orderMessage: {
+    itemCount : 2023,
+    status: 1,
+    surface : 1,
+    message: `Hola!`, 
+    orderTitle: `▮Menu ▸`,
+    thumbnail: imagen1,
+    sellerJid: '0@s.whatsapp.net' 
+    }
+    }
+}
+ await conn.reply(m.chat, '*Prueba . . .*', ftrol) 
 let str = '*🍧 CuriosityBot-MD*'
 await conn.sendUrl(m.chat, str, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen1, thumbnailUrl: imagen1, title: '🍓 Azami', }})
  
