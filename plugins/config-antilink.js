d4aab7355b6ef1bd5b2e654dc9d6e1ef1abed3e8
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-26 12:11:40 -0600

    © CuriosityBot-MD

--- plugins/config-antilink.js
+++ plugins/config-antilink.js
@@ -9,19 +9,18 @@ let bang = m.key.id
 let bot = global.db.data.settings[this.user.jid] || {}
 const isGroupLink = linkRegex.exec(m.text)
 const grupo = `https://chat.whatsapp.com`
-if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(`[ ⚠️ ] 𝙃𝙀𝙔! 𝙀𝙇 𝘼𝙉𝙏𝙄 𝙇𝙄𝙉𝙆
-𝙀𝙎𝙏𝘼 𝘼𝘾𝙏𝙄𝙑𝙊 𝙋𝙀𝙍𝙊 𝙀𝙍𝙀𝙎 𝘼𝘿𝙈𝙄𝙉!, ¡𝙀𝙎𝙏𝘼𝙎 𝙎𝘼𝙇𝙑𝘼𝘿𝙊!`)
+if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(`*⚠️ HEY!! EL ANTI LINK ESTA ACTIVO PERO ERES ADMIN, ¡SALVADO!*`)
 if (chat.antiLink && isGroupLink && !isAdmin) {
 if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 if (m.text.includes(linkThisGroup)) return !0
-}    
-await m.reply(`*[⚠️] ENLACE DETECTADO [⚠️]*\n\n*${await this.getName(m.sender)} 🛑MANDASTE UN ENLACE PROHIBIDO POR LO CUAL SERAS ELIMINADO🛑*`)  
-if (!isBotAdmin) return m.reply(`*[🛡️] NO SOY ADMIN, NO PUEDO ELIMINAR INTRUSOS😶*`)  
+}
+await m.reply(`*⚠️ ENLACE DETECTADO ⚠️*\n\n*${await this.getName(m.sender)} MANDASTE UN ENLACE PROHIBIDO POR LO CUAL SERAS ELIMINADO*`)
+if (!isBotAdmin) return m.reply(`*⚠️ NO SOY ADMIN, NO PUEDO ELIMINAR INTRUSOS*`)
 if (isBotAdmin) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-} else if (!bot.restrict) return m.reply(`*🛑 MI PROPIETARIO DEBE TENE EL MODO RESTRINGIDO ACTIVO🛑*`)
+} else if (!bot.restrict) return m.reply(`*⚠️ MI PROPIETARIO DEBE TENE EL MODO RESTRINGIDO ACTIVO*`)
 }
 return !0
 }
