a989788f1fd69a4e0e095a72ab4b09eaa87a567e
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-08-26 13:14:20 -0500

    Update fun-sorteo.js

--- plugins/fun-sorteo.js
+++ plugins/fun-sorteo.js
@@ -14,7 +14,7 @@ let l = Math.floor(Math.random() * x.length);
 let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
 let top = `*${x} EL GANADOR DEL SORTEO: ${text} ${x}*
     
-*ES:🎉 ${user(a)}*🎉
+🎉*ES:${user(a)}*🎉
 `
 let txt = '';
 let count = 0;
