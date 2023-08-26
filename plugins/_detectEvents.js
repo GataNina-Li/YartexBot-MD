2072da645b2beb5e58cd578d49b42e71911800db
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-25 19:08:04 -0600

    © CuriosityBot-MD

--- plugins/_detectEvents.js
+++ plugins/_detectEvents.js
@@ -19,17 +19,17 @@ export async function before(m, {conn, participants, groupMetadata}) {
   const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'}
 
   if (chat.detect2 && m.messageStubType == 29) {
-    let txt1 = `❕ *• NUEVO ADMIN •* ❕\n\n`
+    let txt1 = `❕ *NUEVO ADMIN* ❕\n\n`
     txt1 += `⚠️ *• Nombre:* @${m.messageStubParameters[0].split`@`[0]}\n`
-    txt1 += `⚠️ *• Le hizo admin:* @${m.sender.split`@`[0]}` 
+    txt1 += `⚠️ *• Le hizo admin:* @${m.sender.split`@`[0]}`
     await delay(3000 * 3);
     await conn.sendMessage(m.chat, {text: txt1, mentions: [...txt1.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt1.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.wm, "containsAutoReply": true, "mediaType": 1, "thumbnail": img, "mediaUrl": nn, "sourceUrl": nn}}});
   }
 
   if (chat.detect2 && m.messageStubType == 30) {
-    let txt2 = `╔═══[ ${groupName} ]═══╗\n║╭──────────────\n║├⫹⫺ *UN ADMINS MENOR*\n`
-    txt2 += `║├⫹⫺ *SE QUITO ADMINS A:* @${m.messageStubParameters[0].split`@`[0]}\n`
-    txt2 += `║├⫹⫺ *EJECUTADO POR:* @${m.sender.split`@`[0]}\n║├⫹⫺ Ya no eres admins 🚮 vete a llorar maldita puta\n║╰──────────────\n╚══════════════════⋆`
+    let txt2 = `❗ *UN ADMIN MENOS* ❗\n\n`
+    txt2 += `⚠️ *• Nombre:* @${m.messageStubParameters[0].split`@`[0]}\n`
+    txt2 += `⚠️ *• Le quito admin:* @${m.sender.split`@`[0]}`
     await delay(3000 * 3);
     await conn.sendMessage(m.chat, {text: txt2, mentions: [...txt2.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt2.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.wm, "containsAutoReply": true, "mediaType": 1, "thumbnail": img, "mediaUrl": md, "sourceUrl": yt}}});
    // await conn.sendMessage(m.chat, {image: img, caption: txt2, mentions: mentionsString}, {quoted: fliveLoc})
