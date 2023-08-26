c38fce26ded6efce11bb1a390707e4893cdadc92
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-25 19:05:42 -0600

    © CuriosityBot-MD

--- plugins/_detectEvents.js
+++ plugins/_detectEvents.js
@@ -21,7 +21,7 @@ export async function before(m, {conn, participants, groupMetadata}) {
   if (chat.detect2 && m.messageStubType == 29) {
     let txt1 = `❕ *• NUEVO ADMIN •* ❕\n\n`
     txt1 += `⚠️ *• Nombre:* @${m.messageStubParameters[0].split`@`[0]}\n`
-    txt1 += `⚠️ *• Le hizo admin:* @${m.sender.split`@`[0]}`
+    txt1 += `⚠️ *• Le hizo admin:* @${m.sender.split`@`[0]}` 
     await delay(3000 * 3);
     await conn.sendMessage(m.chat, {text: txt1, mentions: [...txt1.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt1.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.wm, "containsAutoReply": true, "mediaType": 1, "thumbnail": img, "mediaUrl": nn, "sourceUrl": nn}}});
   }
