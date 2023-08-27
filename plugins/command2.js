e3da22a7eded5ae3500a0c436c23e6cedcc428ce
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-26 18:11:23 -0600

    © CuriosityBot-MD

--- plugins/command2.js
+++ plugins/command2.js
@@ -9,32 +9,13 @@ const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/b
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let name = conn.getName(m.sender)
-    let pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+let pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
 
-    const ftrol = {
-    key : {
-    remoteJid: 'status@broadcast',
-    participant : '0@s.whatsapp.net'
-    },
-    message: {
-    orderMessage: {
-    itemCount : 2023,
-    status: 1,
-    surface : 1,
-    message: `Hola ${name}!`, 
-    orderTitle: `▮Menu ▸`,
-    thumbnail: await (await fetch(pp)).buffer(),
-    sellerJid: '0@s.whatsapp.net' 
-    }
-    }
-    } //  const ftrol = { key : { remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2023, status: 1, surface : 1, message: `Hola! ${name}`, orderTitle: `▮Menu ▸`, thumbnail: imagen1, sellerJid: '0@s.whatsapp' }}}
+const ftrol = { key : { remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2023, status: 1, surface : 1, message: `Hola! ${name}`, orderTitle: `▮Menu ▸`, thumbnail: imagen1, sellerJid: '0@s.whatsapp' }}}
 
-    await conn.reply(m.chat, '*Enviando el menu . . .*', ftrol)
-   // conn.sendMessage(m.chat, ftrol, { quoted: fload })}
- //await conn.reply(m.chat, '*Pruebaaaaaaaaaaaa . . .*', fload) 
+await conn.reply(m.chat, '*Enviando el menu . . .*', ftrol, m)
 let str = `*🍧 CuriosityBot-MD*`.trim()
-await conn.sendMessage(m.chat, { image: imagen4, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}, { quoted: fkontak }) 
-    //await conn.sendUrl(m.chat, str, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen1, thumbnailUrl: imagen1, title: '🍓 Azami', }})
+await conn.sendMessage(m.chat, { image: imagen4, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}, { quoted: fkontak })
  
 }
 handler.command = /^((a|a)a)$/i
