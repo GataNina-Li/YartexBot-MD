5c9e5bb5010f3f69f7ba3c4f92dd4bbdc297f98d
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-26 18:34:51 -0600

    Update command2.js

--- plugins/command2.js
+++ plugins/command2.js
@@ -6,143 +6,6 @@ import PhoneNumber from 'awesome-phonenumber'
 import { promises } from 'fs'
 import { join } from 'path'
 const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
-                                                 
-let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems }) => {
-try {
-let pp = imagen4
-//let vn = './media/menu.mp3'
-let img = await(await fetch('https://i.imgur.com/JP52fdP.jpg')).buffer()
-let d = new Date(new Date + 3600000)
-let locale = 'es'
-let week = d.toLocaleDateString(locale, { weekday: 'long' })
-let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
-let _uptime = process.uptime() * 1000
-let uptime = clockString(_uptime)
-let user = global.db.data.users[m.sender]
-let { money, joincount } = global.db.data.users[m.sender]
-let { exp, limit, level, role } = global.db.data.users[m.sender]
-let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
-let more = String.fromCharCode(8206)
-let readMore = more.repeat(850)   
-let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-m.reply('╭ׅׄ̇─ׅ̻ׄ╮۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹*\n├ ⚘݄𖠵⃕⁖𖥔.Ƈᴀʀɢᴀɴᴅᴏ,\n├Ąɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ❞ ꔷ──᜔◇⃟̣̣⃕✨\n╰ׁ̻─ׅׄ ⊹ٜ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜׄ߭꒦') 
-var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
-var document = doc[Math.floor(Math.random() * doc.length)] 
-let senco = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
-let ftrol = { key : { remoteJid: 'status@broadcast',   participant : '0@s.whatsapp.net'   },   message: {  orderMessage: {  itemCount : 2023, el status: 1,  surface : 1,  message: `Hola! ${dino}`,   orderTitle: `▮Menu ▸`, thumbnail: await (await fetch(senco)).buffer(), sellerJid: '0@s.whatsapp.net' }}}
-let dino = conn.getName(m.sender)
-let str = `₊˚✦୧︰ ᴡᴇʟᴄᴏᴍᴇ ╭・❒ ୨๑
-╭───╯ -*𝗛ola, ${taguser}*
-╰──➤ 🍉 𒉡  𓂃 𝗦̸̷͟͞ᧉ𝐧͢𝛓ᨵ : ᯤ̸ׄ↯ׄᤷ♡
-
-┌┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫┄̸࣭۫┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫┄̸࣭۫┄̸࣭࣭࣭࣭࣭ٜ۫┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫☪︎︎︎̸⃘̸࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬🍧⃘⃪۪֟፝֯۫۫۫۬◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸┄̸࣭࣭࣭࣭࣭ٜ۫╯
-├┈ ↷ *Menu*
-├• ✐; ₊˚✦୧︰*Random* .
-├┈・──・──・﹕₊˚ ✦・୨୧・
-├ׁ̟̇❍✎ _${usedPrefix}kpop *<blackpink / exo / bts>*_
-├ׁ̟̇❍✎ _${usedPrefix}cristianoronaldo_
- _${usedPrefix}messi_
-├ׁ̟̇❍✎ _${usedPrefix}cat_
-├ׁ̟̇❍✎ _${usedPrefix}dog_
-├ׁ̟̇❍✎ _${usedPrefix}meme_
-├ׁ̟̇❍✎ _${usedPrefix}itzy_
-├ׁ̟̇❍✎ _${usedPrefix}blackpink_
-├ׁ̟̇❍✎ _${usedPrefix}lolivid_
-├ׁ̟̇❍✎ _${usedPrefix}loli_
-├ׁ̟̇❍✎ _${usedPrefix}navidad_
-├ׁ̟̇❍✎ _${usedPrefix}ppcouple_
-├ׁ̟̇❍✎ _${usedPrefix}wpmontaña_
-├ׁ̟̇❍✎ _${usedPrefix}pubg_
-├ׁ̟̇❍✎ _${usedPrefix}wpgaming_
-├ׁ̟̇❍✎ _${usedPrefix}wpaesthetic_
-├ׁ̟̇❍✎ _${usedPrefix}wpaesthetic2_
-├ׁ̟̇❍✎ _${usedPrefix}wprandom_
-├ׁ̟̇❍✎ _${usedPrefix}wallhp_
-├ׁ̟̇❍✎ _${usedPrefix}wpvehiculo_
-├ׁ̟̇❍✎ _${usedPrefix}wpmoto_
-├ׁ̟̇❍✎ _${usedPrefix}coffee_
-├ׁ̟̇❍✎ _${usedPrefix}pentol_
-├ׁ̟̇❍✎ _${usedPrefix}caricatura_
-├ׁ̟̇❍✎ _${usedPrefix}ciberespacio_
-├ׁ̟̇❍✎ _${usedPrefix}technology_
-├ׁ̟̇❍✎ _${usedPrefix}doraemon_
-├ׁ̟̇❍✎ _${usedPrefix}hacker_
-├ׁ̟̇❍✎ _${usedPrefix}planeta_
-├ׁ̟̇❍✎ _${usedPrefix}randomprofile_
-├ׁ̟̇❍✎ _${usedPrefix}neko_
-├ׁ̟̇❍✎ _${usedPrefix}waifu_
-├ׁ̟̇❍✎ _${usedPrefix}akira_
-├ׁ̟̇❍✎ _${usedPrefix}akiyama_
-├ׁ̟̇❍✎ _${usedPrefix}anna_
-├ׁ̟̇❍✎ _${usedPrefix}asuna_
-├ׁ̟̇❍✎ _${usedPrefix}ayuzawa_
-├ׁ̟̇❍✎ _${usedPrefix}boruto_
-├ׁ̟̇❍✎ _${usedPrefix}chiho_
-├ׁ̟̇❍✎ _${usedPrefix}chitoge_
-├ׁ̟̇❍✎ _${usedPrefix}deidara_
-├ׁ̟̇❍✎ _${usedPrefix}erza_
-├ׁ̟̇❍✎ _${usedPrefix}elaina_
-├ׁ̟̇❍✎ _${usedPrefix}eba_
-├ׁ̟̇❍✎ _${usedPrefix}emilia_
-├ׁ̟̇❍✎ _${usedPrefix}hestia_
-├ׁ̟̇❍✎ _${usedPrefix}hinata_
-├ׁ̟̇❍✎ _${usedPrefix}inori_
-├ׁ̟̇❍✎ _${usedPrefix}isuzu_
-├ׁ̟̇❍✎ _${usedPrefix}itachi_
-├ׁ̟̇❍✎ _${usedPrefix}itori_
-├ׁ̟̇❍✎ _${usedPrefix}kaga_
-├ׁ̟̇❍✎ _${usedPrefix}kagura_
-├ׁ̟̇❍✎ _${usedPrefix}kaori_
-├ׁ̟̇❍✎ _${usedPrefix}keneki_
-├ׁ̟̇❍✎ _${usedPrefix}kotori_
-├ׁ̟̇❍✎ _${usedPrefix}kurumi_
-├ׁ̟̇❍✎ _${usedPrefix}madara_
-├ׁ̟̇❍✎ _${usedPrefix}mikasa_
-├ׁ̟̇❍✎ _${usedPrefix}miku_
-├ׁ̟̇❍✎ _${usedPrefix}minato_
-├ׁ̟̇❍✎ _${usedPrefix}naruto_
-├ׁ̟̇❍✎ _${usedPrefix}nezuko_
-├ׁ̟̇❍✎ _${usedPrefix}sagiri_
-├ׁ̟̇❍✎ _${usedPrefix}sasuke_
-├ׁ̟̇❍✎ _${usedPrefix}sakura_
-├ׁ̟̇❍✎ _${usedPrefix}cosplay_
-└┄̸࣭۫☪︎︎︎̸⃘̸࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬🌻⃘⃪۪֟፝֯۫۫۫۬◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸┄̸࣭۫┄࣭۫┄̸࣭۫┄࣭۫┄̸࣭۫┄̸࣭۫┄̸۫┄̸࣭۫┄࣭۫╯
-`.trim()
-if (m.isGroup) {
-//await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
-let fkontak2 = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }  
-conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}, { quoted: fkontak2 })  
-} else {    
-//await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
-let fkontak2 = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }  
-conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}, { quoted: fkontak2 })}
-} catch {
-conn.reply(m.chat, '*El menú tiene un error, si el error persiste por favor reportelo al propietario*', m)
-}}
-handler.command = /^(random|menurandom|randommenu)$/i
-handler.exp = 50
-handler.fail = null
-export default handler
-function clockString(ms) {
-let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
-
-
-
-
-
-
-
-/*import fs from 'fs'
-import fetch from 'node-fetch'
-import { xpRange } from '../lib/levelling.js'
-const { levelling } = '../lib/levelling.js'
-import PhoneNumber from 'awesome-phonenumber'
-import { promises } from 'fs'
-import { join } from 'path'
-const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 try {
@@ -166,7 +29,7 @@ let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
  
 const ftrol = { key : { remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2023, status: 1, surface : 1, message: `Hola! ${name}`, orderTitle: `▮Menu ▸`, thumbnail: imagen1, sellerJid: '0@s.whatsapp' }}}
 
-await conn.reply(m.chat, '╭ׅׄ̇─ׅ̻ׄ╮۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹*\n├ ⚘݄𖠵⃕⁖𖥔.Ƈᴀʀɢᴀɴᴅᴏ,\n├Ąɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ❞ ꔷ──᜔◇⃟̣̣⃕✨\n╰ׁ̻─ׅׄ ⊹ٜ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜׄ߭꒦', ftrol, m)
+await conn.reply(m.chat, '╭ׅׄ̇─ׅ̻ׄ╮۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹*\n├ ⚘݄𖠵⃕⁖𖥔.Ƈᴀʀɢᴀɴᴅᴏ,\n├Ąɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ❞ ꔷ──᜔◇⃟̣̣⃕✨\n╰ׁ̻─ׅׄ ⊹ٜ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜׄ߭꒦', ftrol)
 let str = `*🍧 CuriosityBot-MD*`.trim()
 await conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}, { quoted: fkontak })
  
@@ -186,5 +49,5 @@ let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
 return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
- */
+ 
 
