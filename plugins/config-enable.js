697787d8ee882557201f9ad9ad7b7e8617d70a75
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-08-27 15:42:38 -0600

    © CuriosityBot-MD

--- plugins/config-enable.js
+++ plugins/config-enable.js
@@ -1,5 +1,5 @@
 import fs from 'fs'
-let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) =>
+let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
 let isEnable = /true|enable|(turn)?on|1/i.test(command)
 let chat = global.db.data.chats[m.chat]
 let user = global.db.data.users[m.sender]
