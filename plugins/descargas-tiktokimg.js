ffd1f12e32e68808b834dd8597184525f53bb556
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-08-27 10:58:28 -0500

    Update descargas-tiktokimg.js

--- plugins/descargas-tiktokimg.js
+++ plugins/descargas-tiktokimg.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch'
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*⚠️ INGRESE UN ENLACE DE TIKTOK QUE CONTENGA IMÁGENES.* 😿`
+if (!text) throw `*⚠️ INGRESE UN ENLACE DE TIKTOK QUE CONTENGA IMÁGENES.* 📷`
 if (!(text.includes('http://') || text.includes('https://'))) return m.reply(`url invalid, please input a valid url. Try with add http:// or https://`)
-if (!text.includes('tiktok.com')) return m.reply(`*⚠️ URL INVALIDA.*`)
+if (!text.includes('tiktok.com')) return m.reply(`*📎 URL INVALIDA.*`)
 try {
 let res = await fetch(`https://api.lolhuman.xyz/api/tiktokslide?apikey=${global.lolkeysapi}&url=${text}`)
 let anu = await res.json()
@@ -12,7 +12,7 @@ anu = anu.result
 if (anu.length == 0) throw Error('Error : no data')
 let c = 0
 for (let x of anu) {
-if (c == 0) await conn.sendMessage(m.chat, { image: { url: x }, caption: `✅ *Se ha enviado 1 de ${anu.length} imágenes.* ✅\n_El resto podrá ser visible en el chat privado del bot_ 😸` }, { quoted : m })
+if (c == 0) await conn.sendMessage(m.chat, { image: { url: x }, caption: `✅ *Se ha enviado 1 de ${anu.length} imágenes.* ✅\n_El resto podrá ser visible en el chat privado del bot_ 🏮` }, { quoted : m })
 else await conn.sendMessage(m.sender, { image: { url: x } }, { quoted : m })
 c += 1
 }
