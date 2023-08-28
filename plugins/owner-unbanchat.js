d898097bddf63235fec529119f2500dc2718a906
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-08-27 19:27:37 -0600

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = false
-m.reply('*[💎] ESTE CHAT FUE  DESBANEADO [💎]*\n\n*🍓 ESTE CHAT YA TIENE PERMITIDO USARME🍓*')
+m.reply('*[💎] ESTE CHAT FUE  DESBANEADO*\n\n*🍓 ESTE CHAT YA TIENE PERMITIDO USARME*')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
