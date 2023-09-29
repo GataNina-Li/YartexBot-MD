803e1a96d7084b15d839fc7a664f36b9847e8639
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-09-29 12:59:54 -0600

    © CuriosityBot-MD

--- plugins/owner-unbanchat.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m) => {
-global.db.data.chats[m.chat].isBanned = false
-m.reply('*[💎] ESTE CHAT FUE  DESBANEADO*\n\n*🍓 ESTE CHAT YA TIENE PERMITIDO USARME*')
-}
-handler.help = ['unbanchat']
-handler.tags = ['owner']
-handler.command = /^unbanchat$/i
-export default handler
