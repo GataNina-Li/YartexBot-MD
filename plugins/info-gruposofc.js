23ae36828f0605880be210a80e846ff120135bc8
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-10-04 21:01:21 -0600

    © CuriosityBot-MD

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -2,27 +2,7 @@ import fs from "fs"
 let handler = async (m, { conn, command }) => {
 let estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {orderMessage: { itemCount : 2023, status: 1, surface : 1, message: 'By Azami ©', orderTitle: 'Bang', thumbnail: fs.readFileSync('./storage/menus/Menu2.jpg'), sellerJid: '0@s.whatsapp.net'}}}
 let foto = './storage/imagenes/grupos.jpg'
-let texto = `🍿 *＊•̩̩͙✩•̩̩͙˚ GRUPOS OFC ˚•̩̩͙✩•̩̩͙˚＊* 🍿
-
-*✧ GRUPO 1*
-╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄╮
-*${nn}*
-╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄╯
-
-*✧ CURIOSITYBOT - GLOBAL 🌎*
-╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄╮
-${nnn}         
-╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄╯
-
-*✧ ENLACE CURIOSITYBOT 🍓* 
-╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄╮
- ${nnnnn}
-╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄╯
-
-*✧ COLABORACIÓN (MULTI BOTS) 💖*
-╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄╮
-${nnnn}         
-╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄╯`
+let texto = `*${nn}*`
 
 
 await conn.sendFile(m.chat, foto, 'Curiosity.jpg', texto, estilo)}
