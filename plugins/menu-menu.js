d4e92eb628af793e20d06c6d37ead6f43be52222
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-10-04 18:17:22 -0600

    © CuriosityBot-MD

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -316,7 +316,7 @@ let buttonMessage = {
     },
   'caption': menu['trim']()
 }
-await conn.sendFile(m.chat, imagen1, 'error.jpg', buttonMessage)
+await conn.sendMessage(m.chat, buttonMessage, {quoted: fkontak})
 } catch {
 conn.reply(m.chat, '*⚠️ EL MENU TIENE UN ERROR PRUEBE CON EL MENU COMPLETO: (.menucompleto)*', fpoll, m)
 }}
