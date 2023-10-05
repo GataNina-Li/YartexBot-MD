2ad7a519de6ceaf1ab2b4057dc25a30145478e10
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-10-04 19:41:14 -0600

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,66 +1,19 @@
-import { youtubedl, youtubedlv2, youtubeSearch } from '@bochilteam/scraper' 
+import fetch from 'node-fetch' 
   
- var handler = async (m, { conn, command, text, usedPrefix }) => { 
-   try { 
-     if (!text) { 
-       return conn.reply(m.chat, `Gunakan contoh ${usedPrefix}${command} 7!! Orange`, m); 
-     } 
-  
-     conn.reply(m.chat, 'Tunggu sebentar, sedang dicari dan diunduh...', m); 
-  
-     let search = await youtubeSearch(text); 
-  
-     if (!search || !search.video || !search.video[0]) { 
-       throw 'Video Tidak Ditemukan, Coba Judul Lain'; 
-     } 
-  
-     let vid = search.video[0]; 
-     let { authorName, title, thumbnail, duration, viewH, publishedTime, url } = vid; 
-  
-     let caption = `╭──── 〔 Y O U T U B E 〕 ─⬣ 
- ⬡ Judul: ${title} 
- ⬡ Author: ${authorName} 
- ⬡ Durasi: ${duration} 
- ⬡ Views: ${viewH} 
- ⬡ Upload: ${publishedTime} 
- ⬡ Link: ${url} 
- ╰────────⬣`; 
-  
-     conn.reply(m.chat, caption, m, { 
-       contextInfo: { 
-         externalAdReply: { 
-           showAdAttribution: true, 
-           mediaType: 2, 
-           mediaUrl: thumbnail, 
-           body: wm, 
-           thumbnail: await (await conn.getFile(thumbnail)).data, 
-           sourceUrl: url, 
-         }, 
-       }, 
-     }); 
-  
-     const yt = await youtubedl(url).catch(async (_) => await youtubedlv2(url)); 
-     const link = await yt.audio['128kbps'].download(); 
-     let doc = { 
-       audio: { 
-         url: link, 
-       }, 
-       mimetype: 'audio/mp4', 
-       fileName: `${title}`, 
-     }; 
-  
-     return conn.sendMessage(m.chat, doc, { quoted: m }); 
-   } catch (error) { 
-     console.error(error); 
-     conn.reply(m.chat, 'Terjadi kesalahan. Silakan coba lagi nanti.\nNyari yang bener lah...', m); 
-   } 
- }; 
-  
- handler.help = ['play'].map((v) => v + ' <pencarian>') 
- handler.tags = ['downloader'] 
- handler.command = /^playl$/i 
-  
- handler.exp = 0 
- handler.register = true 
+ let handler = async(m, { conn, text }) => { 
+   if (!text) throw `Judulnya?` 
+   let res = await fetch(`https://api.xyroinee.xyz/api/anime/otakudesu-search?q=${text}&apikey=${global.xyro}`) 
+   let otaku = await res.json() 
+ let otakuinfo = `• *Title:* ${otaku.data[0].title} 
+ • *Genre:* ${otaku.data[0].genres} 
+ • *Status*: ${otaku.data[0].status} 
+ • *Rating*: ${otaku.data[0].rating} 
+ • *Link*: ${otaku.data[0].url}` 
+   conn.sendFile(m.chat, otaku.data[0].thumbnail, 'otaku.jpeg', otakuinfo, m) 
+ } 
+ handler.help = ['otakusearch'] 
+ handler.tags = ['anime'] 
+ handler.command = /^(otakusearch)$/i 
+ handler.limit = true 
   
  export default handler
