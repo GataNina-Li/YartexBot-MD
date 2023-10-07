a0b83019961431eb5b447553330b54153691044f
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-10-07 14:33:59 -0600

    © CuriosityBot-MD

--- curiosity.sh
+++ curiosity.sh
@@ -4,129 +4,116 @@
 # Interpretación determinada para la ejecución     
 COMANDOS = "pkg install git -y\npkg install nodejs -y\npkg install ffmpeg -y\npkg install imagemagick -y\npkg install -y yarn\ngit clone https://github.com/AzamiJs/CuriosityBot-MD\ncd CuriosityBot-MD\nyarn install\nnpm install\nnpm start"
 
-echo -e "\e[35m
+echo -e "\e[38;5;183m
 ─█▀▀█ ───░█ ░█─░█ ░█▀▀▀█ ▀▀█▀▀ ░█▀▀▀ 　 ░█─── ─█▀▀█ 　 ░█▀▀█ ─█▀▀█ ░█▄─░█ ▀▀█▀▀ ─█▀▀█ ░█─── ░█─── ─█▀▀█ 
 ░█▄▄█ ─▄─░█ ░█─░█ ─▀▀▀▄▄ ─░█── ░█▀▀▀ 　 ░█─── ░█▄▄█ 　 ░█▄▄█ ░█▄▄█ ░█░█░█ ─░█── ░█▄▄█ ░█─── ░█─── ░█▄▄█ 
-░█─░█ ░█▄▄█ ─▀▄▄▀ ░█▄▄▄█ ─░█── ░█▄▄▄ 　 ░█▄▄█ ░█─░█ 　 ░█─── ░█─░█ ░█──▀█ ─░█── ░█─░█ ░█▄▄█ ░█▄▄█ ░█─░█\n\e[0m" 
+░█─░█ ░█▄▄█ ─▀▄▄▀ ░█▄▄▄█ ─░█── ░█▄▄▄ 　 ░█▄▄█ ░█─░█ 　 ░█─── ░█─░█ ░█──▀█ ─░█── ░█─░█ ░█▄▄█ ░█▄▄█ ░█─░█\n\n\nA J U S T E  L A  P A N T A L L A\n\e[0m" 
 
-echo -e "\033[01;93m⏰ Preparando instalación...\n\033[0m"
-echo -e "\033[01;32m\033[01m🚀 Instalando dependencias!!\n\033[0m" 
-echo -e "\e[36m
+echo -e "\033[38;5;51mPreparando instalación...\n\033[0m"
+echo -e "\033[38;5;188m\033[01mInstalando dependencias!!\n\033[0m" 
+echo -e "\e[38;5;117m
+
 █ █▄░█ █▀ ▀█▀ ▄▀█ █░░ █░░   █▀▀ █ ▀█▀
-█ █░▀█ █  ░█░ █▀█ █▄▄ █▄▄   █▄█ █ ░█░\n\e[0m"
+█ █░▀█ ▄█ ░█░ █▀█ █▄▄ █▄▄   █▄█ █ ░█░\n\e[0m"
 
 if command -v git >/dev/null 2>&1; then
-echo -e "\033[01;33m🟡 Git ya estaba instalado anteriormente.\033[0m"
+echo -e "\033[38;5;229mGit ya estaba instalado anteriormente.\033[0m"
 else
 if pkg install git -y 2>&1 >/dev/null | grep -E -i -q '(command not found|unable to locate package|E: Could not get lock|debconf: delaying package configuration|Package not found|Failed to fetch|404 Not Found|Hash sum mismatch|503 Service Unavailable|504 Gateway Timeout|408 Request Timeout|Connection timed out|Temporary failure resolving)'; then
 error=$(pkg install git -y 2>&1 >/dev/null)
-echo -e "\033[0;31m⚠️ Error: $error\033[0m" 
-echo -e "\033[0;34m⚠️ No se pudo instalar Git. Verifique su conexión a internet e inténtelo de nuevo. Si el error continúa, instale de forma manual!!\033[0m" 
+echo -e "\033[0;31mError: $error\033[0m" 
+echo -e "\033[0;34mNo se pudo instalar Git. Verifique su conexión a internet e inténtelo de nuevo. Si el error continúa, instale de forma manual!!\033[0m" 
 echo -e "\033[01;33m$COMANDOS\033[0m"
 exit 1
 else
-echo -e "\033[01;32m\033[01m✅ Git se ha instalado correctamente.\n\033[0m" 
+echo -e "\033[38;5;229m\033[01mGit se ha instalado correctamente.\n\033[0m" 
 fi
 fi
  
-echo -e "\e[35m
+echo -e "\e[38;5;206m
 █ █▄░█ █▀ ▀█▀ ▄▀█ █░░ █░░   █▄░█ █▀█ █▀▄ █▀▀ ░ ░░█ █▀
 █ █░▀█ ▄█ ░█░ █▀█ █▄▄ █▄▄   █░▀█ █▄█ █▄▀ ██▄ ▄ █▄█ ▄█\n\e[0m"
 
 if command -v node >/dev/null 2>&1; then
-echo -e "\033[01;33m🟡 Nodejs ya estaba instalado anteriormente.\033[0m"
+echo -e "\033[38;5;225mNodejs ya estaba instalado anteriormente.\033[0m"
 else
 if pkg install nodejs -y 2>&1 >/dev/null | grep -E -i -q '(command not found|unable to locate package|E: Could not get lock|debconf: delaying package configuration|Package not found|Failed to fetch|404 Not Found|Hash sum mismatch|503 Service Unavailable|504 Gateway Timeout|408 Request Timeout|Connection timed out|Temporary failure resolving)'; then
 error=$(pkg install nodejs -y 2>&1 >/dev/null)
-echo -e "\033[0;31m⚠️ Error: $error\033[0m" 
-echo -e "\033[0;34m⚠️ No se pudo instalar Node.js. Verifique su conexión a Internet e inténtelo de nuevo. Si el error continúa, instale de forma manual!!\033[0m" 
+echo -e "\033[0;31mError: $error\033[0m" 
+echo -e "\033[0;34mNo se pudo instalar Node.js. Verifique su conexión a Internet e inténtelo de nuevo. Si el error continúa, instale de forma manual!!\033[0m" 
 echo -e "\033[01;33m$COMANDOS\033[0m"
 exit 1
 else
-echo -e "\033[01;32m\033[01m✅ Node.js se ha instalado correctamente.\n\033[0m" 
+echo -e "\033[38;5;224m\033[01mNode.js se ha instalado correctamente.\n\033[0m" 
 fi
 fi
 
-echo -e "\e[36m
+echo -e "\e[38;5;117m
 █ █▄░█ █▀ ▀█▀ ▄▀█ █░░ █░░   █▀▀ █▀▀ █▀▄▀█ █▀█ █▀▀ █▀▀
 █ █░▀█ ▄█ ░█░ █▀█ █▄▄ █▄▄   █▀░ █▀░ █░▀░█ █▀▀ ██▄ █▄█\n\e[0m"
 
 if command -v ffmpeg >/dev/null 2>&1; then
-echo -e "\033[01;33m🟡 Ffmpeg ya estaba instalado anteriormente.\033[0m"
+echo -e "\033[38;5;229mFfmpeg ya estaba instalado anteriormente.\033[0m"
 else
 if pkg install ffmpeg -y 2>&1 >/dev/null | grep -E -i -q '(command not found|unable to locate package|E: Could not get lock|debconf: delaying package configuration|Package not found|Failed to fetch|404 Not Found|Hash sum mismatch|503 Service Unavailable|504 Gateway Timeout|408 Request Timeout|Connection timed out|Temporary failure resolving)'; then
 error=$(pkg install ffmpeg -y 2>&1 >/dev/null)
-echo -e "\033[0;31m⚠️ Error: $error\033[0m" 
-echo -e "\033[0;34m⚠️ No se pudo instalar FFmpeg. Verifique su conexión a Internet e inténtelo de nuevo. Si el error continúa, instale de forma manual!!\033[0m" 
+echo -e "\033[0;31mError: $error\033[0m" 
+echo -e "\033[0;34mNo se pudo instalar FFmpeg. Verifique su conexión a Internet e inténtelo de nuevo. Si el error continúa, instale de forma manual!!\033[0m" 
 echo -e "\033[01;33m$COMANDOS\033[0m"
 exit 1
 else
-echo -e "\033[01;32m\033[01m✅ FFmpeg se ha instalado correctamente.\n\033[0m" 
+echo -e "\033[38;5;229m\033[01mFFmpeg se ha instalado correctamente.\n\033[0m" 
 fi
 fi
 
-echo -e "\e[35m
+echo -e "\e[38;5;206m
 █ █▄░█ █▀ ▀█▀ ▄▀█ █░░ █░░   █ █▀▄▀█ ▄▀█ █▀▀ █▀▀ █▀▄▀█ ▄▀█ █▀▀ █ █▀▀ █▄▀
 █ █░▀█ ▄█ ░█░ █▀█ █▄▄ █▄▄   █ █░▀░█ █▀█ █▄█ ██▄ █░▀░█ █▀█ █▄█ █ █▄▄ █░█\n\e[0m"
 
 if command -v convert >/dev/null 2>&1; then
-echo -e "\033[01;33m🟡 Imagemagick ya estaba instalado anteriormente.\033[0m"
+echo -e "\033[38;5;225mImagemagick ya estaba instalado anteriormente.\033[0m"
 else
 if pkg install imagemagick -y 2>&1 >/dev/null | grep -E -i -q '(command not found|unable to locate package|E: Could not get lock|debconf: delaying package configuration|Package not found|Failed to fetch|404 Not Found|Hash sum mismatch|503 Service Unavailable|504 Gateway Timeout|408 Request Timeout|Connection timed out|Temporary failure resolving)'; then
 error=$(pkg install imagemagick -y 2>&1 >/dev/null)
-echo -e "\033[0;31m⚠️ Error: $error\033[0m" 
-echo -e "\033[0;34m⚠️ No se pudo instalar ImageMagick. Verifique su conexión a Internet e inténtelo de nuevo. Si el error continúa, instale de forma manual!!\033[0m" 
+echo -e "\033[0;31mError: $error\033[0m" 
+echo -e "\033[0;34mNo se pudo instalar ImageMagick. Verifique su conexión a Internet e inténtelo de nuevo. Si el error continúa, instale de forma manual!!\033[0m" 
 echo -e "\033[01;33m$COMANDOS\033[0m"
 exit 1
 else
-echo -e "\033[01;32m\033[01m✅ ImageMagick se ha instalado correctamente.\n\033[0m" 
+echo -e "\033[38;5;224m\033[01mImageMagick se ha instalado correctamente.\n\033[0m" 
 fi
 fi
 
-echo -e "\e[36m
+echo -e "\e[38;5;117m
 █ █▄░█ █▀ ▀█▀ ▄▀█ █░░ █░░   █▄█ ▄▀█ █▀█ █▄░█
 █ █░▀█ ▄█ ░█░ █▀█ █▄▄ █▄▄   ░█░ █▀█ █▀▄ █░▀█\n\e[0m"
 
 if command -v yarn >/dev/null 2>&1; then
-echo -e "\033[01;33m🟡 Yarn ya estaba instalado anteriormente.\033[0m"
+echo -e "\033[38;5;228mYarn ya estaba instalado anteriormente.\033[0m"
 else
 if npm install -g yarn 2>&1 >/dev/null | grep -E -i -q '(command not found|unable to locate package|E: Could not get lock|debconf: delaying package configuration|Package not found|Failed to fetch|404 Not Found|Hash sum mismatch|503 Service Unavailable|504 Gateway Timeout|408 Request Timeout|Connection timed out|Temporary failure resolving)'; then
 error=$(npm install -g yarn 2>&1 >/dev/null)
-echo -e "\033[0;31m⚠️ Error: $error\033[0m" 
-echo -e "\033[0;34m⚠️ No se pudo instalar Yarn. Verifique su conexión a Internet e inténtelo de nuevo. Si el error continúa, instale de forma manual!!\033[0m" 
+echo -e "\033[38;5;228mError: $error\033[0m" 
+echo -e "\033[0;34mNo se pudo instalar Yarn. Verifique su conexión a Internet e inténtelo de nuevo. Si el error continúa, instale de forma manual!!\033[0m" 
 echo -e "\033[01;33m$COMANDOS\033[0m"
 exit 1
 else
-echo -e "\033[01;32m\033[01m✅ Yarn se ha instalado correctamente.\n\033[0m" 
+echo -e "\033[38;5;229m\033[01mYarn se ha instalado correctamente.\n\033[0m" 
 fi
 fi
 
-echo -e "\e[36m
-██╗██╗██╗
-██║██║██║
-██║██║██║
-╚═╝╚═╝╚═╝
-██╗██╗██╗
-╚═╝╚═╝╚═╝ 
-
+echo -e "\033[33m
 ▀▀█▀▀ ▒█▀▀▀█ ▒█▀▀▄ ▒█▀▀▀█ 　 ▒█▀▀█ ▒█▀▀▀█ ▒█▀▀█ ▒█▀▀█ ▒█▀▀▀ ▒█▀▀█ ▀▀█▀▀ ▒█▀▀▀█ 
 ░▒█░░ ▒█░░▒█ ▒█░▒█ ▒█░░▒█ 　 ▒█░░░ ▒█░░▒█ ▒█▄▄▀ ▒█▄▄▀ ▒█▀▀▀ ▒█░░░ ░▒█░░ ▒█░░▒█ 
-░▒█░░ ▒█▄▄▄█ ▒█▄▄▀ ▒█▄▄▄█ 　 ▒█▄▄█ ▒█▄▄▄█ ▒█░▒█ ▒█░▒█ ▒█▄▄▄ ▒█▄▄█ ░▒█░░ ▒█▄▄▄█
+░▒█░░ ▒█▄▄▄█ ▒█▄▄▀ ▒█▄▄▄█ 　 ▒█▄▄█ ▒█▄▄▄█ ▒█░▒█ ▒█░▒█ ▒█▄▄▄ ▒█▄▄█ ░▒█░░ ▒█▄▄▄█\n\n\n\e[0m"
+echo -e "\033[01;32m\033[01m\nTodas las dependencias se han instalado correctamente.\n\033[0m" 
 
-░█▀▀█ ▒█░░░ ▒█░░░ 　 ▒█▀▀█ ▀█▀ ▒█▀▀█ ▒█░▒█ ▀▀█▀▀ 
-▒█▄▄█ ▒█░░░ ▒█░░░ 　 ▒█▄▄▀ ▒█░ ▒█░▄▄ ▒█▀▀█ ░▒█░░ 
-▒█░▒█ ▒█▄▄█ ▒█▄▄█ 　 ▒█░▒█ ▄█▄ ▒█▄▄█ ▒█░▒█ ░▒█░░\n\e[0m"
-echo -e "\033[01;32m\033[01m\n✅ Todas las dependencias se han instalado correctamente.\n\033[0m" 
+echo -e "\e[38;5;183m
 
-echo -e "\e[35m
-┈┈┈╲┈┈┈┈╱
-┈┈┈╱▔▔▔▔╲
-┈┈┃┈▇┈┈▇┈┃
-╭╮┣━━━━━━┫╭╮
-┃┃┃┈┈┈┈┈┈┃┃┃
-╰╯┃┈┈┈┈┈┈┃╰╯
-┈┈╰┓┏━━┓┏╯
-┈┈┈╰╯┈┈╰╯ 
+
+
+
+
 
 ██╗░░██╗░░██╗░░  ██╗███╗░░██╗░██████╗████████╗░█████╗░██╗░░░░░██╗░░░░░
 ╚██╗░╚██╗░╚██╗░  ██║████╗░██║██╔════╝╚══██╔══╝██╔══██╗██║░░░░░██║░░░░░
@@ -135,11 +122,11 @@ echo -e "\e[35m
 ██╔╝░██╔╝░██╔╝░  ██║██║░╚███║██████╔╝░░░██║░░░██║░░██║███████╗███████╗
 ╚═╝░░╚═╝░░╚═╝░░  ╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚══════╝\n\e[0m"
 
-echo -e "\033[1;35m"
+echo -e "\033[38;5;51m"
 git clone https://github.com/AzamiJs/CuriosityBot-MD.git
-echo -e "\033[01;32m\033[01m✅ La clonación se ha descargado e instalado correctamente.\n\033[0m"
+echo -e "\033[38;5;188m\033[01mLa clonación se ha descargado e instalado correctamente.\n\033[0m"
 
-echo -e "\033[01;32m\033[01m🟢 Cambiando al directorio del repositorio!!\n\033[0m" 
+echo -e "\033[38;5;51m\033[01mCambiando al directorio del repositorio!!\n\033[0m" 
 cd CuriosityBot-MD
 
 echo -e "\e[36m
