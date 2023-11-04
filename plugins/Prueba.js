975508b009ad1a1b96f044e620164d70104610b2
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-04 11:23:37 -0600

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,19 +1,22 @@
-import fetch from 'node-fetch' 
-  
- let handler = async(m, { conn, text }) => { 
-   if (!text) throw `Judulnya?` 
-   let res = await fetch(`https://api.xyroinee.xyz/api/anime/otakudesu-search?q=${text}&apikey=${global.xyro}`) 
-   let otaku = await res.json() 
- let otakuinfo = `• *Title:* ${otaku.data[0].title} 
- • *Genre:* ${otaku.data[0].genres} 
- • *Status*: ${otaku.data[0].status} 
- • *Rating*: ${otaku.data[0].rating} 
- • *Link*: ${otaku.data[0].url}` 
-   conn.sendFile(m.chat, otaku.data[0].thumbnail, 'otaku.jpeg', otakuinfo, m) 
- } 
- handler.help = ['otakusearch'] 
- handler.tags = ['anime'] 
- handler.command = /^(otakusearch)$/i 
- handler.limit = true 
-  
- export default handler
+import fs from 'fs'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+await m.reply(`_*🗂️ Enviando base de datos. . .*_`)
+try {
+let d = new Date
+let date = d.toLocaleDateString('es', { day: 'numeric', month: 'long', year: 'numeric' })
+let database = await fs.readFileSync(`./database.json`)
+let creds = await fs.readFileSync(`./sessions/creds.json`)
+await conn.reply(m.sender, `*🗓️ Database:* ${date}`, fkontak)
+await conn.sendMessage(m.sender, {document: database, mimetype: 'application/json', fileName: `database.json`}, { quoted: m })
+await conn.sendMessage(m.sender, {document: creds, mimetype: 'application/json', fileName: `creds.json`}, { quoted: m })
+} catch (e) {
+await m.reply('FALLO')
+console.log(`FALLO`)
+console.log(e)}}
+
+handler.command = /^(backup|respaldo|copia)$/i
+handler.owner = true
+
+export default handler
