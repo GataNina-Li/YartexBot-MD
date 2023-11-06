6d029c5604392d661f63d97bdeca4e6c712a635c
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-06 07:32:07 -0600

    © CuriosityBot-MD

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ global.packname = 'CuriosityBot-MD'
 global.author = ''
 global.wm = '© CuriosityBot-MD'
 global.wm2 = '𝗖𝗎𝗋𝗂𝗈𝗌𝗂𝗍𝗒 : 𝗕𝗈𝗍'
-global.azami = 'Azami'
+global.Azami = 'Azami'
 global.cb = 'CuriosityBot-MD'
 
 global.vs = '1.0.2'
