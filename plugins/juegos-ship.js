c646eba2f2a3d6cf910d556a913d5121dda1c2be
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-08 00:02:38 -0600

    © CuriosityBot-MD

---
+++ plugins/juegos-ship.js
@@ -0,0 +1,19 @@
+var handler = async (m, { conn, command, text }) => {
+
+if (!text) throw `*⚠️ ESCRIBE EL NOMBRE DE DOS PERSONAS PARA CALCULAR SU AMOR*`
+let [text1, ...text2] = text.split(' ')
+
+text2 = (text2 || []).join(' ')
+if (!text2) throw `*⚠️ ESCRIBE EL NOMBRE DE LA SEGUNDA PERSONA*`
+let love = `❤️ *${text1}* tu oportunidad de enamorarte de *${text2}* es de *${Math.floor(Math.random() * 100)}%*👩🏻‍❤️‍👨🏻 
+
+_*by Curiosity*_
+`.trim()
+m.reply(love, null, { mentions: conn.parseMention(love) })
+
+}
+handler.help = ['love']
+handler.tags = ['fun']
+handler.command = /^(ship)$/i
+
+export default handler
