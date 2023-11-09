d7e3d5bd5f7f9e4f7f3553050d2050fb6a423593
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-09 14:16:39 -0600

    © CuriosityBot-MD

--- config.js
+++ config.js
@@ -1,15 +1,7 @@
 import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
-import fs from 'fs'
-import cheerio from 'cheerio'
-import fetch from 'node-fetch'
-import axios from 'axios'
-import path, { join } from 'path'
 import { fileURLToPath, pathToFileURL } from 'url'
 import moment from 'moment-timezone' 
-import { platform } from 'process'
-global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') { return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString() }; global.__dirname = function dirname(pathURL) { return path.dirname(global.__filename(pathURL, true)) }; global.__require = function require(dir = import.meta.url) { return createRequire(dir) }
-const __dirname = global.__dirname(import.meta.url)
 
 global.owner = [ ['5214531287294', 'CuriosityBot-MD 🍧', true], ['5217294888993'], '5492266466080', '593968585383', '51992004117', '5492266613038', '573106040746', '50558124470', '51929972576' ]
 
@@ -37,6 +29,8 @@ global.lenguaje = 'Español'
 global.menudi = ['⛶','❏','⫹⫺']
 global.dev = '5217294888993'
 
+global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') { return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString() }; global.__dirname = function dirname(pathURL) { return path.dirname(global.__filename(pathURL, true)) }; global.__require = function require(dir = import.meta.url) { return createRequire(dir) }
+const __dirname = global.__dirname(import.meta.url)
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => { unwatchFile(file)
 console.log(chalk.yellow('Se actualizo el archivo config.js'))
