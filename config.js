77dfe73b3b4ccc236cc1483325408837a8257d0e
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-09 14:07:04 -0600

    © CuriosityBot-MD

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ import { platform } from 'process'
 global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') { return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString() }; global.__dirname = function dirname(pathURL) { return path.dirname(global.__filename(pathURL, true)) }; global.__require = function require(dir = import.meta.url) { return createRequire(dir) }
 const __dirname = global.__dirname(import.meta.url)
 
-global.owner = [['527294888993', 'Creador', true], ['5492266466080', 'Colaborador', true], ['593968585383'], ['51992004117'], ['5492266613038'], ['573106040746'], ['50576390682'], ['5217294888993'], ['50558124470'], ['51929972576']]
+global.owner = [['527294888993', 'Creador', true], ['5492266466080', 'Colaborador', true], ['5217294888993'], '593968585383', '51992004117', '5492266613038', '573106040746', '50558124470', '51929972576']
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
