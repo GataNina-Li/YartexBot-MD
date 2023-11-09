36c100207c4635afae7f8ae7ceee00d21fb159e5
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-09 14:40:34 -0600

    © CuriosityBot-MD

--- store.js
+++ store.js
@@ -3,6 +3,7 @@ import chalk from 'chalk'
 import fs from 'fs'
 import path, { join } from 'path'
 import { fileURLToPath, pathToFileURL } from 'url'
+global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') { return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString() }; global.__dirname = function dirname(pathURL) { return path.dirname(global.__filename(pathURL, true)) }; global.__require = function require(dir = import.meta.url) { return createRequire(dir) }
 const __dirname = global.__dirname(import.meta.url)
 
 //Imagenes
