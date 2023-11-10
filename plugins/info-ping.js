536c35e3e34b02de570e688e89e71f53a35923f2
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-10 13:11:45 -0600

    © CuriosityBot-MD

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -1,6 +1,12 @@
-
+import { totalmem, freemem } from 'os'
+import os from 'os'
+import util from 'util'
+import osu from 'node-os-utils'
+import { performance } from 'perf_hooks'
+import { sizeFormatter } from 'human-readable'
 import speed from 'performance-now'
 import { spawn, exec, execSync } from 'child_process'
+const format = sizeFormatter({ std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (literal, symbol) => `${literal} ${symbol}B` })
 
 var handler = async (m, { conn }) => {
       
