ab88cf6f750eca125cce19ac516b47ff707eb0ce
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-10 14:53:48 -0600

    © CuriosityBot-MD

--- plugins/info-scrip.js
+++ plugins/info-scrip.js
@@ -1,5 +1,9 @@
-let handler = async (m, { conn, usedPrefix }) => {
-let git = '*乂  B O T  -  S C R I P T*\nhttps://github.com/AzamiJs/CuriosityBot-MD'
+var handler = async (m, { conn, usedPrefix }) => {
+
+ let res = await fetch('https://api.github.com/repos/AzamiJs/CuriosityBot-MD')
+ let json = await res.json()
+ 
+let git = `*乂  B O T  -  S C R I P T*\n\n◦ *Nombre* : ${json.name}\n◦ *Visitantes* : ${json.watchers_count}\n◦ *Peso* : ${(json.size / 1024).toFixed(2)} MB\n◦ *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n◦ *Url* : ${json.html_url}\n\n	   ${json.forks_count} Forks · ${json.stargazers_count} Stars · ${json.open_issues_count} Issues\n\n${azami}`
 await conn.sendUrl(m.chat, git, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen2, thumbnailUrl: imagen1, title: '\t\t\t\t\t\t᭡͡ᩬ🍧✩̣̣̣̣̣ͯ𝑪𝒖𝒓𝒊𝒐𝒔𝒊𝒕𝒚𝑩𝒐𝒕-𝑴𝑫᭡͡ᩬ🍧✩̣̣̣̣̣ͯ', }})
  
 }
