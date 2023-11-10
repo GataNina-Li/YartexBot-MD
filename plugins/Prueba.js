6ddaf64cbd4e414d0cdd6eaa611b49c411bcbc5b
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-09 21:28:41 -0600

    © CuriosityBot-MD

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,15 +1,18 @@
 import fetch from 'node-fetch'
+import axios from 'axios'
 
 var handler = async (m, { conn, text, usedPrefix, command }) => {
 	
-let curiosity = await(await fetch("https://api.myip.com")).html().catch(_ => 'error')
-        
+let curiosity = await(await fetch("https://api.myip.com")).json().catch(_ => 'error')
 let ip = (curiosity == 'error' ? 'Fallo' : curiosity.ip)
 let cr = (curiosity == 'error' ? 'Fallo' : curiosity.country)
 let cc = (curiosity == 'error' ? 'Fallo' : curiosity.cc)
+let bot = await(await fetch("https://whatismyipaddress.com/ip/${ip}")
 await m.reply(`Ip: ${ip}
 País: ${cr}
-Model: ${cc}`)
+Model: ${cc}
+
+${bot}`)
     
 }
 handler.help = ['agedetect']
