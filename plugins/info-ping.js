8a1872edc9fff06526295572d73e2f35bb9f71ce
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-10 17:13:15 -0600

    © CuriosityBot-MD

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -37,3 +37,9 @@ handler.command = ['ping', 'speed']
 handler.register = true
 
 export default handler
+
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
