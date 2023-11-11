7720dcdd4a79ad668136b1c5645cbf3875fb6fd9
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-10 18:30:22 -0600

    © CuriosityBot-MD

---
+++ plugins/info-creditos.js
@@ -0,0 +1,74 @@
+var handler = async (m) => {
+
+let tqto = `*▸ - - - —「 MUCHAS GRACIAS A 」— - - - ◂*
+*Adiwajshing:*
+https://github.com/adiwajshing
+*WhisKeySockets:*
+https://github.com/WhiskeySockets
+*Nurutomo:*
+https://github.com/Nurutomo
+*Istikmal:* 
+https://github.com/BochilGaming
+*Ariffb:*
+https://github.com/Ariffb25
+*Ilman:*
+https://github.com/ilmanhdyt
+*Amirul:*
+https://github.com/amiruldev20
+*Rasel:*
+https://github.com/raselcomel
+*Fatur:*
+https://github.com/Ftwrr
+*Rominaru:*
+https://github.com/Rominaru
+*Kannachann:*
+https://github.com/Kannachann
+*The.sad.boy01:*
+https://github.com/kangsad01
+*Ameliascrf:*
+https://github.com/Ameliascrf
+*Fokus ID:*
+https://github.com/Fokusdotid
+*Johannes:*
+https://github.com/Johannes2803
+*BrunoSobrino:*
+https://github.com/BrunoSobrino
+*GataDios:*
+https://github.com/GataNina-Li
+*Krisna:*
+https://github.com/NevtBotz
+*LitRHap:*
+https://github.com/LitRHap
+*Rlxfly:*
+https://github.com/Rlxfly
+*Aine:*
+https://github.com/Aiinne
+*Papah-Chan:*
+https://github.com/FahriAdison
+
+*▸ - - - —「 Desarollador 」— - - - ◂*
+*Azami:*
+https://github.com/AzamiJs
+
+*▸ - - - —「 Gracias por ayudarme 」— - - - ◂*
+*Mario* (Elrebelde21)
+*GataDios* (GataNina-Li)
+*Endymion* (ReyEndymion)
+*Wotchito* (Aleixon127271)
+*HJ* (HACHEJOTA)
+*Ender* (EnderLB)
+*Ax* (Jxtxn17)
+*Wilmer* (Wilsmac)
+*Jose* (KimdanBot-MD)
+*Katashi* (KatashiFukushima)
+*Daniel* (MoonContentCreator)`
+
+conn.reply(m.chat, tqto, fkontak, m)
+
+
+}
+handler.help = ['tqto']
+handler.tags = ['info']
+handler.command = /^(credits|creditos|credit|thanks|thanksto|tqto)$/i
+
+export default handler
