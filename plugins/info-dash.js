b69a934556007e1e833d13c9c11d7a9cf19298ba
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-15 07:24:39 -0600

    © CuriosityBot-MD

---
+++ plugins/info-dash.js
@@ -0,0 +1,22 @@
+var handler = async (m, {
+ conn
+ }) => {
+  var stats = Object.entries(db.data.stats).map(([key, val]) => {
+    var name = Array.isArray(plugins[key]?.help) ? plugins[key]?.help?.join(' , ') : plugins[key]?.help || key 
+    if (/exec/.test(name)) return
+    return { name, ...val }
+  })
+  stats = stats.sort((a, b) => b.total - a.total)
+  var handlers = stats.slice(0, 10).map(({
+   name, 
+   total, 
+   last
+ }) => {
+    return `乂 *Comando* : *${name}*\n• *Global HIT* : ${total}`
+  }).join`\n\n`
+  m.reply(handlers)   
+};
+handler.command = handler.help = ['dashboard', 'dash', 'views']
+handler.tags = ['main']
+
+export default  handler
