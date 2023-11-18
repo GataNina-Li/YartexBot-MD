ae4a14536fb040eab474ffdc65eff615b2993aa6
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-17 23:19:34 -0600

    © CuriosityBot-MD

--- plugins/A.js
+++ plugins/A.js
@@ -1,16 +1,15 @@
-import fetch from "node-fetch"
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+import fetch from 'node-fetch'
+
+var handler = async (m, { conn, args, usedPrefix, command }) => {
+
 var flag_list = await(await fetch("https://raw.githubusercontent.com/maisans-maid/country-flags/master/countries.json")).json()
 var flag_img = Array.from(Object.keys(flag_list).map((v) => v))
 var flag_name = Array.from(Object.values(flag_list).map((o) => o))
-        let listSections = []
-        Object.keys(flag_img, flag_name).map((v, index) => {
-            listSections.push(["Model [ " + ++index + " ]", [
-                [flag_name[v], usedPrefix + "get https://raw.githubusercontent.com/maisans-maid/country-flags/master/png1000px/" + flag_img[v].toLowerCase() + ".png", "➥"]
-            ]])
-        })
-        return conn.reply(m.chat, listSections, m)
+
+conn.reply(m.chat, flag_list, fkontak, m)
+
 }
-handler.help = ["flags"]
-handler.command = ["flags"]
+handler.help = ['flags']
+handler.command = ['flags']
+
 export default handler
