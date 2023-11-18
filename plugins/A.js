ff8edbc29315d22f12a396ca7df66322937448fe
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-17 23:22:45 -0600

    Update A.js

--- plugins/A.js
+++ plugins/A.js
@@ -3,10 +3,8 @@ import fetch from 'node-fetch'
 var handler = async (m, { conn, args, usedPrefix, command }) => {
 
 var flag_list = await(await fetch("https://raw.githubusercontent.com/maisans-maid/country-flags/master/countries.json")).json()
-var flag_img = Array.from(Object.keys(flag_list).map((v) => v))
-var flag_name = Array.from(Object.values(flag_list).map((o) => o))
 
-conn.reply(m.chat, flag_list, fkontak, m)
+conn.reply(m.chat, flag_list, m)
 
 }
 handler.help = ['flags']
