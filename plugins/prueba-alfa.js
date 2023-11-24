cf0f841f6e91e6effe1d1c530e46cbb115981431
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-11-24 09:26:44 -0500

    Update prueba-alfa.js

--- plugins/prueba-alfa.js
+++ plugins/prueba-alfa.js
@@ -3,7 +3,7 @@ import yaoiImages from 'module-gatadios'
 var handler = async (m, {conn, args, command, usedPrefix}) => {
   
 const result1 = yaoiImages.getRandomImage('yaoi-hard', ['link'])
-m.reply(result1.link)
+m.reply(result1)
 
 }
 handler.command = /^(prueba23)$/i
