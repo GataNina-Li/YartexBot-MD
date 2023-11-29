9a67e0d85359aae9896cc952b05a700f71c85e5c
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-11-29 13:54:54 -0500

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -20,18 +20,18 @@ let chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats
 let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
 
 
-let texto = `*▢ V E L O C I D A D*
-🚄 ${latensi.toFixed(4)}
+let texto = `*🚀 V E L O C I D A D*
+☯︎ ${latensi.toFixed(4)}
 
-*▢ A C T I V I D A D*
-• ${muptime}
+*⏰ A C T I V I D A D*
+☯︎ ${muptime}
 
-*▢ C H A T S*
-• ${chats.length} *Chats privados*
-• ${groups.length} *Grupos*
+*🌱 C H A T S*
+☯︎ ${chats.length} *Chats privados*
+☯︎ ${groups.length} *Grupos*
 
-*▢ S E R V I D O R*
-🛜 *Ram:* ${format(totalmem() - freemem())} / ${format(totalmem())}`.trim()
+*💻 S E R V I D O R*
+☯︎ *Ram:* ${format(totalmem() - freemem())} / ${format(totalmem())}`.trim()
 
 conn.sendMessage(m.chat, { text: texto, contextInfo: { externalAdReply: { title: '', body: 'CURIOSITY - BOT - MD', thumbnailUrl: 'https://telegra.ph/file/6cbf9148b572711e9b000.jpg', sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
 
