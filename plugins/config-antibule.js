4b15e95c0d6f6d580c6f8a7e0a34a19082eb111d
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-01 01:36:02 -0600

    © CuriosityBot-MD

--- plugins/config-antibule.js
+++ plugins/config-antibule.js
@@ -5,7 +5,7 @@ let chat = global.db.data.chats[m.chat]
 let user = global.db.data.users[m.sender]
 let caption = `*⚠️ ANTI EXTRANJEROS ⚠️*\n\n${name} @${m.sender.split("@")[0]}, Adios!`.trim()
 if (chat.antibule) {
-if (!m.sender.startsWith('52' || '00')) {
+if (!m.sender.startsWith('52' || '593')) {
 user.banned = true
 conn.reply(m.chat, caption, null, m, { mentions: this.parseMention(caption) })
 return this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
