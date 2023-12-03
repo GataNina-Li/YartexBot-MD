52f8f72d5cfad739171fadff6d0d1db3ba24c337
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-02 22:01:04 -0600

    © CuriosityBot-MD

--- plugins/bot-menu.js
+++ plugins/bot-menu.js
@@ -1,4 +1,4 @@
-//CÓDIGO ADAPTADO POR https://github.com/GataNina-Li | @gata_dios & https://github.com/Undefined17 | @Azami
+//CÓDIGO ADAPTADO POR https://github.com/GataNina-Li | @gata_dios & https://github.com/AzamiJs | @Azami
 
 import fs from 'fs'
 import fetch from 'node-fetch'
