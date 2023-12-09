6b04eb72321cdd58c217352f8c23337a5235731d
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-09 12:25:37 -0600

    © CuriosityBot-MD

--- plugins/internet-youtube.js
+++ plugins/internet-youtube.js
@@ -1,21 +1,27 @@
 import yts from 'yt-search'
-let handler = async (m, { text, conn, args, command, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `*⚠️ ESCRIBA EL NOMBRE DE UN VIDEO O UN CANAL DE YOUTUBE*`, fakeimg,  m)
+
+var handler = async (m, { text, conn, args, command, usedPrefix }) => {
+
+if (!text) return conn.reply(m.chat, `*⚠️ ESCRIBA EL NOMBRE DE UN VIDEO O UN CANAL DE YOUTUBE*`, m)
 let results = await yts(text)
 let tes = results.all
 let teks = results.all.map(v => {
-switch (v.type) { 
+switch (v.type) {
 case 'video': return `📑 *TÍTULO:* ${v.title}
 🔗 *ENLACE:* ${v.url}
 ⏰ *DURACIÓN:* ${v.timestamp}
 🔎 *SUBIDO:* ${v.ago}
 👀 *VISTAS:* ${v.views}`}}).filter(v => v).join('\n\n••••••••••••••••••••••••••••••••••••\n\n')
+
 conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, fkontak, m)
+
 }
-handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
+handler.help = ['ytsearch']
 handler.tags = ['internet:']
 handler.command = /^playlist|ytbuscar|yts(earch)?$/i
+
 handler.exp = 70
 handler.limit = 1
+
 export default handler
       
