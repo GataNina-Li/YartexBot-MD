9576ef4878d37a3b4235891b8deecbcb9358c12c
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-09 12:27:07 -0600

    © CuriosityBot-MD

--- plugins/bot-script.js
+++ plugins/bot-script.js
@@ -3,15 +3,17 @@ import moment from 'moment-timezone'
 
 var handler = async (m, { conn, usedPrefix }) => {
 
- let res = await fetch('https://api.github.com/repos/AzamiJs/CuriosityBot-MD')
- let json = await res.json()
+let res = await fetch('https://api.github.com/repos/AzamiJs/CuriosityBot-MD')
+let json = await res.json()
  
 let git = `*乂  B O T  -  S C R I P T*\n\n◦ *Nombre* : ${json.name}\n◦ *Visitantes* : ${json.watchers_count}\n◦ *Peso* : ${(json.size / 1024).toFixed(2)} MB\n◦ *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n◦ *Url* : ${json.html_url}\n\n	   ${json.forks_count} Forks · ${json.stargazers_count} Stars · ${json.open_issues_count} Issues\n\n${azami}`
 await conn.sendUrl(m.chat, git, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen2, thumbnailUrl: imagen1, title: '\t\t\t\t\t\t᭡͡ᩬ🍧✩̣̣̣̣̣ͯ𝑪𝒖𝒓𝒊𝒐𝒔𝒊𝒕𝒚𝑩𝒐𝒕-𝑴𝑫᭡͡ᩬ🍧✩̣̣̣̣̣ͯ', }})
  
 }
-handler.tags =['info'] 
+handler.tags =['bot'] 
 handler.help = ['script'] 
-handler.command = ['sc', 'script', 'codigo', 'git', 'github'] 
-handler.register = true 
+handler.command = ['sc', 'script', 'codigo', 'git', 'github']
+
+handler.register = true
+
 export default handler
