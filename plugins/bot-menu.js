888583fca3aa72d6d95c39e0949ae71bd2c490a9
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-12 14:41:06 -0600

    © CuriosityBot-MD

--- plugins/bot-menu.js
+++ plugins/bot-menu.js
@@ -30,7 +30,7 @@ let pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra
 let fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': imagen1, thumbnail: imagen1 ,sendEphemeral: true}}}
 let links = linkSity.getRandom()
 
-let a = {'key': {'participants': '0@s.whatsapp.net' ,'fromMe':![],'id': '0@s.whatsapp.net', 'message': {'locationMessage':{'name': `${name}`,'jpegThumbnail': await (await fetch(pp)).buffer(),'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'participant': '0@s.whatsapp'}}}}
+let a = {'key': {'participants': '0@s.whatsapp.net', 'fromMe': false, 'id': '', 'message': {'locationMessage':{'name': `${name}`, 'jpegThumbnail': await (await fetch(pp)).buffer(),'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'participant': '0@s.whatsapp'}}}}
 //Fakes
 const ftrol = { key : { remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2023, status: 1, surface : 1, message: `${name}!`, orderTitle: `▮Menu ▸`, sellerJid: '0@s.whatsapp.net' }}}
 //const fload = { key : { message: `CuriosityBot-MD 🍧` + `\nAzami`, thumbnail: await (await fetch(pp)).buffer(), sellerJid: '0@s.whatsapp.net' }}
