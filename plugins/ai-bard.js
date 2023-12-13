cc22f2b3c9471b35ff0635ef41a83fe5fa2d3436
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-13 06:38:11 -0600

    © CURIOSITYBOT-MD

---
+++ plugins/ai-bard.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch'
+
+var handler = async (m, { text,  usedPrefix, command }) => {
+
+if (!text) throw `*⚠️ INGRESE UN TEXTO* `
+
+try {
+
+await m.reply('*🚀 C A R G A N D O*')
+var apii = await fetch(`https://api.botcahx.live/api/search/bard-ai?apikey=97Fxy6mj&text=${text}`)
+var res = await apii.json()
+await m.reply(res.message)
+
+} catch (error) {
+console.error(error)
+throw '⚠️ *OCURRIÓ UN ERROR*'
+}
+
+}
+handler.command = ['bard']
+handler.help = ['bard']
+handler.tags = ['ai']
+
+handler.premium = false
+
+export default handler
