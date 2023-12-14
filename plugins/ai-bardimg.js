2357ef768e31c5739a9d046f0c9c3c938213fd21
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-14 14:57:48 -0600

    © CuriosityBot-MD

---
+++ plugins/ai-bardimg.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch'
+import uploader from '../lib/uploadImage'
+
+var handler = async (m, { conn, text, command, usedPrefix }) => {
+
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || q.mediaType || ''
+if (/image/g.test(mime) && !/webp/g.test(mime)) {
+let buffer = await q.download()
+
+await m.reply('*🚀 C A R G A N D O*')
+
+let media = await uploader(buffer)
+let json = await (await fetch(`https://aemt.me/bardimg?url=${media}&text=${text}`)).json()
+
+conn.sendMessage(m.chat, { text: json.result }, { quoted: m })
+
+} else throw `*⚠️ RESPONDE A UNA IMAGEN*`
+  
+}
+handler.help = ['bardimg']
+handler.tags = ['ai']
+handler.command = /^(bardimg|bardimage)$/i
+
+handler.limit = true
+
+export default handler
