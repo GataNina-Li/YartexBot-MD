1eacc5b30bab293b4d463179a612bd36e3c897a6
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-14 08:37:14 -0600

    © CuriosityBot-MD

--- plugins/bot-menu.js
+++ plugins/bot-menu.js
@@ -55,7 +55,7 @@ _(Para ver el menú completo)_`
 
 //await conn.reply(m.chat, menu, a, m)
 
-await conn.sendMessage(m.chat, { text: menu }, {quoted: m})
+await conn.sendMessage(m.chat, { text: menu }, {quoted: a})
 
 } catch {
 conn.reply(m.chat, '⚠️ *FALLO, USE .allmenu*', fkontak, m)
