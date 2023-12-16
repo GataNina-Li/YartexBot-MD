002978a7bda5803a20b657d39ca3c3420b445a3b
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-15 18:58:27 -0600

    © CuriosityBot-MD

---
+++ plugins/imagen-girls.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import fetch from 'node-fetch'
+
+var handler = async (m, {command, conn}) => {
+
+if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*❗ 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*';
+
+if (command == 'yaoi2') {
+let res = await fetch(`https://aemt.me/china`)
+let json = await res.json()
+let url = json.link
+conn.sendMessage(m.chat, {image: res, caption: `_${command}_`.trim()}, {quoted: m});
+}
+
+}
+handler.help = ['china']
+handler.command = ['china']
+handler.tags = ['nsfw']
+
+export default handler
