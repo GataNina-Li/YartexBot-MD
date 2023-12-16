29b99094b6ec60928e2fbabcb9d62346c8ebfffb
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-15 19:51:34 -0600

    © CuriosityBot-MD

--- plugins/imagen-girls.js
+++ plugins/imagen-girls.js
@@ -2,8 +2,6 @@ import fetch from 'node-fetch'
 
 var handler = async (m, {command, conn}) => {
 
-if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*❗ 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*';
-
 if (command == 'china') {
 let china = await fetch(`https://aemt.me/china`)
 conn.sendMessage(m.chat, {image: china, caption: `*🇨🇳 China*`.trim()}, {quoted: m})
@@ -34,9 +32,14 @@ let coreana = await fetch(`https://aemt.me/korea`)
 conn.sendMessage(m.chat, {image: coreana, caption: `*🇰🇷 Coreana*`.trim()}, {quoted: m})
 }
 
+if (command == 'malaya') {
+let malaya = await fetch(`https://aemt.me/malaysia`)
+conn.sendMessage(m.chat, {image: malaya, caption: `*🇲🇾 Malaya*`.trim()}, {quoted: m})
 }
-handler.help = ['china', 'vietnamita', 'tailandes', 'indonesia', 'japones', 'coreana']
-handler.command = ['china', 'vietnamita', 'tailandes', 'indonesia', 'japones', 'coreana']
+
+}
+handler.help = ['china', 'vietnamita', 'tailandes', 'indonesia', 'japones', 'coreana', 'malaya']
+handler.command = ['china', 'vietnamita', 'tailandes', 'indonesia', 'japones', 'coreana', 'malaya']
 handler.tags = ['nsfw']
 
 export default handler
