a33b5b27c6061db8ff6f9f21fd6ce440be40ecc6
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-18 14:14:20 -0600

    © CuriosityBot-MD

--- plugins/a.js
+++ plugins/a.js
@@ -7,10 +7,11 @@ let Prefijo = false
 const prefixRegex = global.prefix
 if (prefixRegex.test(m.text)) Prefijo = true
 
+if (bot.bard && !m.isGroup && !Prefijo && !m.fromMe && m.text !== '') {
 let api = await fetch(`https://aemt.me/bard?text=${text}`)
 let res = await api.json()
 
-await m.reply(res.result)
+await m.reply(res.result)}
 
 }
 export default handler
