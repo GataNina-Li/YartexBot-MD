229c19801f7f38a35fec0af4321f38b863fb3d66
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-18 14:17:47 -0600

    © CuriosityBot-MD

--- plugins/a.js
+++ plugins/a.js
@@ -10,6 +10,7 @@ if (prefixRegex.test(m.text)) Prefijo = true
 if (bot.bard && !m.isGroup && !Prefijo && !m.fromMe && m.text !== '') {
 if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
 
+conn.sendPresenceUpdate('composing', m.chat)
 let api = await fetch(`https://aemt.me/bard?text=${text}`)
 let res = await api.json()
 
