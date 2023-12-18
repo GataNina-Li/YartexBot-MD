65d311174f81c82f9bf6afc16ce0775881c6a6e6
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-18 12:07:55 -0600

    © CuriosityBot-MD

--- plugins/anime-imagenes.js
+++ plugins/anime-imagenes.js
@@ -1,23 +1,61 @@
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, command }) => {
+var handler = async (m, { conn, command }) => {
 
 if (command == 'fanart') {
-let url = await fetch(`https://api.lolhuman.xyz/api/random/art?apikey=${lolkeysapi}`)
-conn.sendMessage(m.chat, {image: url, caption: `*🧧 Fanart*`.trim()}, {quoted: m})
+let fanart = await fetch(`https://api.lolhuman.xyz/api/random/art?apikey=${lolkeysapi}`)
+conn.sendMessage(m.chat, {image: fanart, caption: `*🧧 Fanart*`.trim()}, {quoted: m})
+m.react('🧧')
 }
 
 if (command == 'husbu') {
-let url2 = await fetch(`https://api.lolhuman.xyz/api/random/husbu?apikey=${lolkeysapi}`)
-conn.sendMessage(m.chat, {image: url2, caption: `*Bojone Gepenk 🐦*`.trim()}, {quoted: m})
+let husbu = await fetch(`https://api.lolhuman.xyz/api/random/husbu?apikey=${lolkeysapi}`)
+conn.sendMessage(m.chat, {image: husbu, caption: `*🚩 Husbu*`.trim()}, {quoted: m})
+m.react('🚩')
+}
+
+if (command == 'kanna') {
+let kanna = await fetch(`https://api.lolhuman.xyz/api/random/kanna?apikey=${lolkeysapi}`)
+conn.sendMessage(m.chat, {image: kanna, caption: `*🍧 Kanna*`.trim()}, {quoted: m})
+m.react('🍧')
+}
+
+if (command == 'megumin') {
+let megumin = await fetch(`https://api.lolhuman.xyz/api/random/megumin?apikey=${lolkeysapi}`)
+conn.sendMessage(m.chat, {image: megumin, caption: `*🍂 Megumin*`.trim()}, {quoted: m})
+m.react('🍂')
+}
+
+if (command == 'neko') {
+let neko = await fetch(`https://api.lolhuman.xyz/api/random/neko?apikey=${lolkeysapi}`)
+conn.sendMessage(m.chat, {image: neko, caption: `*😻 Neko*`.trim()}, {quoted: m})
+m.react('😻')
+}
+
+if (command == 'shota') {
+let neko = await fetch(`https://api.lolhuman.xyz/api/random/shota?apikey=${lolkeysapi}`)
+conn.sendMessage(m.chat, {image: neko, caption: `*⚡ Shota*`.trim()}, {quoted: m})
+m.react('⚡')
+}
+
+if (command == 'waifu') {
+let waifu = await fetch(`https://api.lolhuman.xyz/api/random/waifu?apikey=${lolkeysapi}`)
+conn.sendMessage(m.chat, {image: waifu, caption: `*🍭 Waifu*`.trim()}, {quoted: m})
+m.react('🍭')
+}
+
+if (command == 'elaina') {
+let elaina = await fetch(`https://api.lolhuman.xyz/api/random/neko?apikey=${lolkeysapi}`)
+conn.sendMessage(m.chat, {image: elaina, caption: `*🍚 Elaina*`.trim()}, {quoted: m})
+m.react('🍚')
 }
 
 }
-handler.command = /^(fanart')$/i
+handler.command = /^(fanart|husbu|kanna|megumin|neko|shota|waifu|elaina)$/i
 handler.tags = ['anime']
 handler.help = ['fanart']
 
 handler.limit = true
-handler.premium = true
 
 export default handler
+                 
