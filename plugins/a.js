3e0f39e0728dbb98fbd36644eb1545b99449345c
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-18 14:12:36 -0600

    © CuriosityBot-MD

--- plugins/a.js
+++ plugins/a.js
@@ -1,15 +1,17 @@
-import fetch from 'node-fetch';
-const handler = (m, { text }) => m
+import fetch from 'node-fetch'
 
+var handler = (m, { text }) => m
 handler.before = async (m, { text }) => {
-  let Prefijo = false;
-  const prefixRegex = global.prefix;
-  if (prefixRegex.test(m.text)) Prefijo = true
 
-var apii = await fetch(`https://aemt.me/bard?text=${text}`)
-var res = await apii.json()
+let Prefijo = false
+const prefixRegex = global.prefix
+if (prefixRegex.test(m.text)) Prefijo = true
+
+let api = await fetch(`https://aemt.me/bard?text=${text}`)
+let res = await api.json()
+
 await m.reply(res.result)
 
 }
-export default handler;
+export default handler
 
