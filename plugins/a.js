267cf3ea97daa686a30e52af08ad494be82602fd
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-18 14:10:15 -0600

    © CuriosityBot-MD

--- plugins/a.js
+++ plugins/a.js
@@ -1,13 +1,15 @@
-/*import fetch from 'node-fetch';
-const handler = (m) => m
+import fetch from 'node-fetch';
+const handler = (m, { text }) => m
 
 handler.before = async (m) => {
   let Prefijo = false;
   const prefixRegex = global.prefix;
   if (prefixRegex.test(m.text)) Prefijo = true
 
-  m.reply(wm)
+var apii = await fetch(`https://aemt.me/bard?text=${text}`)
+var res = await apii.json()
+await m.reply(res.result)
 
-};
+}
 export default handler;
-*/
+
