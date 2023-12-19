722e9f3a6f2be9b156d57a43f4ed4619114b7b87
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-19 14:15:50 -0600

    © CuriosityBot-MD

--- plugins/bot-bot.js
+++ plugins/bot-bot.js
@@ -3,12 +3,14 @@ import fs from 'fs'
 var handler = m => m
 handler.all = async function (m) {
 
+let img = sityImg.getRandom()
+
 let bot = `*🚀 Hola soy Bot*\nEn que te puedo ayudar?`
-let vn = [imagen1, imagen2, imagen3, imagen4, img1].getRandom()
+
 let chat = global.db.data.chats[m.chat]
 if (/^bot$/i.test(m.text) && !chat.isBanned) { 
 conn.sendPresenceUpdate('recording', m.chat)    
-conn.sendFile(m.chat, vn, 'bot.jpg', bot, m, true, { type: 'conversation', ptt: true, sendEphemeral: true, quoted: estilo })}
+conn.sendFile(m.chat, img, 'bot.jpg', bot, m, true, { type: 'conversation', ptt: true, sendEphemeral: true, quoted: estilo })}
 return !0
 
 }
