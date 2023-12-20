0b17d9d8b8225730e02b85d2f56c214e0bdbe2a0
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-19 23:30:48 -0600

    © CuriosityBot-MD

--- plugins/ai-bard.js
+++ plugins/ai-bard.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { text,  usedPrefix, command }) => {
 
-if (!text) await conn.reply(m.chat, `Ingrese una petición`, m, { contextInfo:{ externalAdReply: {title: saludo, body: dev, sourceUrl: global.ig, thumbnailUrl: fotos }}}, { quoted: m })
+if (!text) await conn.reply(m.chat, `Ingrese una petición`, m, { contextInfo:{ externalAdReply: {title: saludo + nombre, body: dev, sourceUrl: global.ig, thumbnailUrl: fotos }}}, { quoted: m })
 
 try {
 
