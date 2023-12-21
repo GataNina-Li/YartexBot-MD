f3c8ff1e736ffbfd1042ec12d95f77a466f5eef0
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-20 21:06:50 -0600

    © CuriosityBot-MD

--- plugins/ai-bard.js
+++ plugins/ai-bard.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { text,  usedPrefix, command }) => {
 
-if (!text) return conn.reply(m.chat, `Ingrese una petición`, m, fake, )
+if (!text) return conn.reply(m.chat, `🎌 *Ingrese una petición*\n\nEjemplo, .bard Conoces CuriosityBot-MD?`, m, fake, )
 
 try {
 
