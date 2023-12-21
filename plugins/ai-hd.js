fe961eec6b9d765e4acc337b6f12fb55ca3e6b5b
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-20 23:35:36 -0600

    © CuriosityBot-MD

--- plugins/ai-hd.js
+++ plugins/ai-hd.js
@@ -23,7 +23,7 @@ let img = await q.download?.()
 let error
 try {
 const This = await processing(img, "enlace")
-conn.sendFile(m.chat, This, '', '🧃 Toma tu foto\n\n' + cred.toString('utf-8'), m)
+conn.sendFile(m.chat, This, '', '🧃 *Toma tu foto*\n\n' + cred.toString('utf-8'), m)
 } catch (er) {
 error = true
 } finally {
