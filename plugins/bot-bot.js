68b4e64b898b90cd5b59b18dc691e2fb98b82ebb
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-21 10:18:01 -0600

    © CuriosityBot-MD

--- plugins/bot-bot.js
+++ plugins/bot-bot.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (/^bot$/i.test(m.text) && !chat.isBanned) {
 
 conn.sendPresenceUpdate('composing', m.chat)    
-conn.reply(m.chat, bot, m, fake, )
+conn.reply(m.chat, bot, fake, )
 
 return !0
 
