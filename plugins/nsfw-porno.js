39fd2302c970d6ca3bed0fc4d89844dd6d6d7175
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-21 11:28:50 -0600

    © CuriosityBot-MD

--- plugins/nsfw-porno.js
+++ plugins/nsfw-porno.js
@@ -1,13 +1,17 @@
-import fetch from "node-fetch";
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+import fetch from 'node-fetch'
 
+var handler = async (m, { conn, args, usedPrefix, command }) => {
+
+if (!db.data.chats[m.chat].nsfw && m.isGroup) throw '🎌 *Los comandos nsfw no estan permitidos*\n\nUtilice !on nsfw para activar'
   
 let res = await fetch('https://api.zahwazein.xyz/randomasupan/discord18?apikey=zenzkey_048b3b850d51')
 let json = await res.json()
-conn.sendFile(m.chat, json.result, null, `🥵`, null, null, {viewOnce: true}, m)
-};
-handler.tags = ["info"];
-handler.command = handler.help = ["vporno", "xxxvideo"];
+conn.sendFile(m.chat, json.result, null, `🥵`, null, null, { viewOnce: true }, m)
+
+}
+handler.tags = ['nsfw']
+handler.command = ['vporno', 'xxxvideo']
+handler.help = ['vporno', 'xxxvideo']
 handler.register = true
 
-export default handler;
+export default handler
