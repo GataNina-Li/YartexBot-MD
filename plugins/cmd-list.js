c26af1811adeb39abb23d7eda4b312e9c50557df
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-21 22:45:19 -0600

    © CuriosityBot-MD

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -3,7 +3,7 @@ var handler = async (m, {conn}) => {
 conn.reply(m.chat, `🎌 *LISTA - CMD*
 
 ${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}.-\n*Codigo:* ${value.locked ? `_bloqueado_ ${key}` : key}\n*Comando* ${value.text}`).join('\n\n')}
-`.trim(), null, {mentions: Object.values(global.db.data.sticker).map((x) => x.mentionedJid).reduce((a, b) => [...a, ...b], [])}, fake, )
+`.trim(), null, {mentions: Object.values(global.db.data.sticker).map((x) => x.mentionedJid).reduce((a, b) => [...a, ...b], [])})
 
 }
 handler.tags = ['database']
