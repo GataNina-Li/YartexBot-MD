874fef555e5bed69e0ba7319a6c3e963f23dc446
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-21 21:34:23 -0600

    © CuriosityBot-MD

--- plugins/bot-firstchat.js
+++ plugins/bot-firstchat.js
@@ -7,7 +7,7 @@ if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup) return
 let user = global.db.data.users[m.sender]
 
 if (new Date() - user.pc < 86400000) return
-await m.reply(`👋 Hola ${nombre}!!
+await conn.reply(m.chat, `👋 Hola ${nombre}!!
  *${saludo}*
 
 📅 Fecha: ${fecha}
@@ -17,7 +17,7 @@ await m.reply(`👋 Hola ${nombre}!!
 🧃 Escriba *.menu* para mostrar el menú 
   
 📝 ¿Quieres apoyar este proyecto para que siga actualizándose? enviar a través de: 
-*https://paypal.me/azami.19*`, fake, )
+*https://paypal.me/azami.19*`, m, fake, )
 
 
 user.pc = new Date * 1
