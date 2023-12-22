19e5036d56522d5aac21d3a75f860a54bae9ecb9
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-21 22:43:52 -0600

    © CuriosityBot-MD

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -1,9 +1,12 @@
-const handler = async (m, {conn}) => {
-  conn.reply(m.chat, `
-*< 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 / 𝚃𝙴𝚇𝚃𝙾𝚂 𝙰𝚂𝙸𝙶𝙰𝙽𝙳𝙾𝚂 />*
+var handler = async (m, {conn}) => {
 
-${Object.entries(global.db.data.sticker).map(([key, value], index) => `*${index + 1}.-*\n*𝙲𝙾𝙳𝙸𝙶𝙾:* ${value.locked ? `*(𝚋𝚕𝚘𝚚𝚞𝚎𝚊𝚍𝚘)* ${key}` : key}\n*𝙲𝙾𝙼𝙰𝙽𝙳𝙾/𝚃𝙴𝚇𝚃𝙾* ${value.text}`).join('\n\n')}
-`.trim(), null, {mentions: Object.values(global.db.data.sticker).map((x) => x.mentionedJid).reduce((a, b) => [...a, ...b], [])});
-};
+conn.reply(m.chat, `🎌 *LISTA - CMD*
+
+${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}.-\n*Codigo:* ${value.locked ? `_bloqueado_ ${key}` : key}\n*Comando* ${value.text}`).join('\n\n')}
+`.trim(), null, {mentions: Object.values(global.db.data.sticker).map((x) => x.mentionedJid).reduce((a, b) => [...a, ...b], [])}, fake, )
+
+}
+handler.tags = ['database']
+handler.help = ['listcmd']
 handler.command = ['listcmd', 'cmdlist'];
-export default handler;
+export default handler
