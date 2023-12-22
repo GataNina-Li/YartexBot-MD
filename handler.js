353b1dccab284845f50c09a30b6c44024df58d01
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-12-21 22:43:47 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -576,7 +576,7 @@ botAdmin: '*¡Para realizar la función debo ser admin!*',
 unreg: '*¡Para continuar con esta función debera registrarse!*\n\n!reg nombre.edad\n\n*Uso correcto* : !reg Azami.21',
 restrict: '*¡Esta característica esta desactivada!*'
 }[type]
-if (msg) return conn.reply(m.chat, msg, m, { contextInfo:{ externalAdReply: {title: '🎄'  + saludo +  nombre, body: dev, sourceUrl: global.channel, thumbnailUrl: fotos }}})
+if (msg) return conn.reply(m.chat, msg, m, { contextInfo:{ externalAdReply: {title: '🎄'  + saludo + ' ' + nombre, body: dev, sourceUrl: global.channel, thumbnailUrl: fotos }}})
 
 }
 const file = global.__filename(import.meta.url, true);
