08fd0ec2432a415ff4fa5ca1ff01ecbd39cd16e1
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-24 00:14:54 -0600

    © CuriosityBot-MD

--- plugins/bot-ds.js
+++ plugins/bot-ds.js
@@ -28,12 +28,12 @@ if (filesDeleted === 0) {
 await conn.reply(m.chat, '🚩 *No se encontró ningún archivo que incluya la ID del chat*', m, fake, )
 } else {
 await conn.reply(m.chat, `🎌 *Se eliminaron ${filesDeleted} archivos de sesión*`, m, fake, )
+conn.reply(m.chat, `😸 *¡Hola! logras ver me*`, m, fake, )
 }
 } catch (err) {
 console.error('Error al leer la carpeta o los archivos de sesión:', err)
 await conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, fake, )
 }
-await conn.reply(m.chat, `😸 *¡Hola! logras ver me*`, m, fake, )
 
 }
 handler.help = ['ds']
