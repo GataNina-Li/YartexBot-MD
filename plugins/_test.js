d4c1fa6f66227ccc233fe855096793599579224d
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-26 18:56:51 -0600

    © CuriosityBot-MD

---
+++ plugins/_test.js
@@ -0,0 +1,26 @@
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
+
+var handler  = async (m, { conn, usedPrefix }) => {
+
+var info = `Sí, estoy aquí Azami`
+
+const pre = generateWAMessageFromContent(m.chat, { liveLocationMessage:{
+degreesLatitude: 0,
+degreesLongitude: 0,
+accuracyInMeters: 0,
+speedInMps: 0,
+degreesClockwiseFromMagneticNorth: 0,
+caption: info,
+sequenceNumber: 0,
+timeOffset: 8600,
+thumbnailUrl: img, 
+contextInfo: { mentionedJid: [m.sender] }
+}}, { quoted: fake })
+
+return conn.relayMessage(m.chat, pre.message, { messageId: pre.key.id })
+
+}
+handler.customPrefix = /^(tes|bot|curio|test)$/i
+handler.command = new RegExp
+
+export default handler
