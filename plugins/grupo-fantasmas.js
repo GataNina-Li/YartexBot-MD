0f8d7ce1b400d448f4b6b2ce0c443ffbfb50249c
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-26 20:38:09 -0600

    © CuriosityBot-MD

--- plugins/grupo-fantasmas.js
+++ plugins/grupo-fantasmas.js
@@ -9,29 +9,32 @@ var sum = member.length
 var sum = text} 
 var total = 0
 var sider = []
-for(let i = 0; i < sum; i++) {
+for (let i = 0; i < sum; i++) {
 let users = m.isGroup ? participants.find(u => u.id == member[i]) : {}
-if((typeof global.db.data.users[member[i]] == 'undefined' || global.db.data.users[member[i]].chat == 0) && !users.isAdmin && !users.isSuperAdmin) { 
+if ((typeof global.db.data.users[member[i]] == 'undefined' || global.db.data.users[member[i]].chat == 0) && !users.isAdmin && !users.isSuperAdmin) { 
 if (typeof global.db.data.users[member[i]] !== 'undefined'){
-if(global.db.data.users[member[i]].whitelist == false){
+if (global.db.data.users[member[i]].whitelist == false){
 total++
 sider.push(member[i])}
 }else {
 total++
 sider.push(member[i])}}}
 const delay = time => new Promise(res=>setTimeout(res,time))
+
 switch (command) {
+
 case 'fantasmas': 
-if(total == 0) return conn.reply(m.chat, `*🌟 ESTE GRUPO ES ACTIVO, NO TIENE FANTASMAS*`, m) 
-m.reply(`*⚠️ REVISIÓN DE INACTIVOS ⚠️*\n\n*❕ Miembros del grupo:* ${sum}\n\n*👻 Lista de fantasmas 👻*\n${sider.map(v => '@' + v.replace(/@.+/, '')).join('\n')}\n\n*📝 NOTA:* Esto no es al 100% acertado, el bot inicia el conteo de mensajes a partir de que se active en este número`, null, { mentions: sider }) 
-break   
+if(total == 0) return conn.reply(m.chat, `🎌 *Este grupo es activo, no tiene fantasmas*`, m, fake, ) 
+m.reply(`🚩 *Revisión de inactivos*\n\n⚠️ *Lista de fantasmas*\n${sider.map(v => '@' + v.replace(/@.+/, '')).join('\n')}\n\n*📝 NOTA:*\nEsto no es al 100% acertado, el bot inicia el conteo de mensajes a partir de que se active en este número`, null, { mentions: sider }) 
+break
+
 case 'kickfantasmas':  
-if(total == 0) return conn.reply(m.chat, `*🌟 ESTE GRUPO ES ACTIVO NO TIENE FANTASMAS :D*`, m) 
-await m.reply(`*⚠️ ELIMINACIÓN DE INACTIVOS ⚠️*\n\n*Participantes: ${sum}*\n\n*👻 Fantasmas 👻*\n${sider.map(v => '@' + v.replace(/@.+/, '')).join('\n')}\n\n*❗ ᴇʟ ʙᴏᴛ ᴇʟɪᴍɪɴᴀʀᴀ ʟᴀ ʟɪsᴛᴀ ᴍᴇɴᴄɪᴏɴᴀᴅᴀ, ᴇᴍᴘᴇᴢᴀɴᴅᴏ ᴇɴ 20 sᴇɢᴜɴᴅᴏs, ʏ ᴄᴀᴅᴀ 10 sᴇɢᴜɴᴅᴏs ᴇʟɪᴍɪɴᴀʀᴀ ᴜɴ ɴᴜᴍᴇʀᴏ*`, null, { mentions: sider }) 
+if(total == 0) return conn.reply(m.chat, `🎌 *Este grupo es activo no tiene fantasmas*`, m, fake, ) 
+await m.reply(`🚩 *Eliminación de inactivos*\n\n⚠️ *Lista de fantasmas*\n${sider.map(v => '@' + v.replace(/@.+/, '')).join('\n')}\n\n❗ _El bot eliminara a los usuarios de la lista mencionada cada 10 segundos_`, null, { mentions: sider }) 
 await delay(1 * 10000)
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-try{
+try {
        
 let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
 let kickedGhost = sider.map(v => v.id).filter(v => v !== conn.user.jid)
