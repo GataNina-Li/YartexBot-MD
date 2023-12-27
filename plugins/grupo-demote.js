606dcc0e6338f0a82bfddf0c8d5980a2a9b87d16
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-26 20:20:35 -0600

    © CuriosityBot-MD

--- plugins/grupo-demote.js
+++ plugins/grupo-demote.js
@@ -1,33 +1,34 @@
 var handler = async (m, { conn,usedPrefix, command, text }) => {
 
-if(isNaN(text) && !text.match(/@/g)){
+if (isNaN(text) && !text.match(/@/g)){
 	
-}else if(isNaN(text)) {
+} else if (isNaN(text)) {
 var number = text.split`@`[1]
-}else if(!isNaN(text)) {
+} else if (!isNaN(text)) {
 var number = text
 }
-if(!text && !m.quoted) return m.reply(`*⚠️ EJEMPLO DE USO*\n\n*${usedPrefix}demote @tag*\n*${usedPrefix}demote responder a un mensaje*`)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return m.reply(`⚠️ NUMERO INCORRECTO`)
+if (!text && !m.quoted) return conn.reply(m.chat, `🎌 *Etiqueta al admin. que desea degradar*\nEjemplo, !demote *@tag*\n!demote *responder a un mensaje*`, m, fake, )
+if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🚩 *Número erróneo*`, m, fake, )
   
 try {
-if(text) {
+if (text) {
 var user = number + '@s.whatsapp.net'
-} else if(m.quoted.sender) {
+} else if (m.quoted.sender) {
 var user = m.quoted.sender
-} else if(m.mentionedJid) {
+} else if (m.mentionedJid) {
 var user = number + '@s.whatsapp.net'
 } 
 } catch (e) {
 } finally {
 conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-m.reply(`*✅ EXITO, YA NO TIENE PODER EN ESTE GRUPO*`)
+conn.reply(m.chat, `✅ *Se degrado de admin con éxito*`, m, fake, )
 }
 
 }
 handler.help = ['demote']
 handler.tags = ['grupo']
-handler.command = ['demote', 'degradar'] 
+handler.command = ['demote', 'degradar']
+
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
