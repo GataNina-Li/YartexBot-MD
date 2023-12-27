41fcbf055064b8206b1929032e7ae57714dd1fe4
MD <122848691+WorkCwp@users.noreply.github.com>
2023-12-26 21:26:17 -0500

    © CuriosityBot-MD

--- plugins/bot-ping.js
+++ plugins/bot-ping.js
@@ -1,4 +1,4 @@
-/*import { totalmem, freemem } from 'os'
+import { totalmem, freemem } from 'os'
 import os from 'os'
 import util from 'util'
 import osu from 'node-os-utils'
@@ -49,4 +49,3 @@ let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
 return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
-*/
