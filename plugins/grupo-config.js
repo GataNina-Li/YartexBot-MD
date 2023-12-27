df1c1e008a2dcd2b3adaf4bd0d9d882f037bde96
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-26 19:59:41 -0600

    © CuriosityBot-MD

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -2,7 +2,7 @@ var handler = async (m, {conn, args, usedPrefix, command}) => {
 
 const isClose = { 'open': 'not_announcement', 'close': 'announcement', 'abierto': 'not_announcement', 'cerrado': 'announcement', 'abrir': 'not_announcement', 'cerrar': 'announcement', 'desbloquear': 'unlocked', 'bloquear': 'locked' }[(args[0] || '')]
 
-if (isClose === undefined) { return conn.reply(m.chat, `*Elija una opción para configurar el grupo*\n\nEjemplo:\n*○ !${command} abrir*\n*○ ${command} cerrar*\n*○ ${command} bloquear*\n*○ ${command} desbloquear*`, m, fake, )
+if (isClose === undefined) { return conn.reply(m.chat, `*Elija una opción para configurar el grupo*\n\nEjemplo:\n*○ !${command} abrir*\n*○ !${command} cerrar*\n*○ !${command} bloquear*\n*○ !${command} desbloquear*`, m, fake, )
 
 }
 await conn.groupSettingUpdate(m.chat, isClose)
