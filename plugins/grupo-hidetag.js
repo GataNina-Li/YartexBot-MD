0ae184c45dc047865fe84173299720c936ea6478
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-30 13:02:14 -0600

    © CuriosityBot-MD

--- plugins/grupo-hidetag.js
+++ plugins/grupo-hidetag.js
@@ -3,7 +3,7 @@ import * as fs from 'fs'
 
 var handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
 
-if (!m.quoted || !text) return conn.reply(m.chat, `🎌 *Ingrese un texto*`, m, fake, )
+if (!m.quoted && !text) return conn.reply(m.chat, `🎌 *Ingrese un texto*`, m, fake, )
 
 try { 
 
