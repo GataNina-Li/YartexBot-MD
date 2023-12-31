5b19d15a01b5659fc6075caa321b7baf352ef2f2
ArizzVal <137434511+ArizzVal@users.noreply.github.com>
2023-12-30 23:31:21 -0600

    Update rpg-_afk.js
    Anterior

--- plugins/rpg-_afk.js
+++ plugins/rpg-_afk.js
@@ -1,35 +1,28 @@
-import db from '../lib/database.js';
-
+import db from '../lib/database.js'
 export function before(m) {
-    let user = global.db.data.users[m.sender];
-
+    let user = global.db.data.users[m.sender]
     if (user.afk > -1) {
         m.reply(`
-*⏰ 「DEJASTE DE ESTAR AFK」 ⏰*
-${user.afkReason ? '👀 *Razón:* ' + user.afkReason : ''}
+*「⏰ DEJASTE DE ESTAR AFK ⏰」*\n ${user.afkReason ? ' \n👀 Razón: ' + user.afkReason : ''}
 ⏱️ *Durante:* ${(new Date - user.afk).toTimeString()}
-`.trim());
-        user.afk = -1;
-        user.afkReason = '';
+  `.trim())
+        user.afk = -1
+        user.afkReason = ''
     }
-    let jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])];
+    let jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])]
     for (let jid of jids) {
-        let user = global.db.data.users[jid];
-        if (!user) continue;
-        let afkTime = user.afk;
-        if (!afkTime || afkTime < 0) continue;
-        let reason = user.afkReason || '';
+        let user = global.db.data.users[jid]
+        if (!user)
+            continue
+        let afkTime = user.afk
+        if (!afkTime || afkTime < 0)
+            continue
+        let reason = user.afkReason || ''
         m.reply(`
-⚠️ *EL USUARIO QUE MENCIONASTE ESTÁ AFK*
-${reason ? '👀 *Razón:* ' + reason : '*Sin razón*'}
-⏱️ *Durante:* ${clockString(new Date - afkTime)}
-`.trim());
+️⚠️ *EL USUARIO QUE MENCIONASTE ESTA AFK*
+${reason ? '👀 *Razón* : ' + reason : '*Sin razón*'}
+⏱️ *Durante* : ${clockString(new Date - afkTime)}
+  `.trim())
     }
-    return true;
-}
-function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':');
+    return true
 }
