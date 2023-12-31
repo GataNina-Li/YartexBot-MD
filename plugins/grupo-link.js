b231ccc05652b5529fce78ccb21ccfbec2073bb1
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-31 14:14:33 -0600

    © CuriosityBot-MD

--- plugins/grupo-link.js
+++ plugins/grupo-link.js
@@ -2,12 +2,14 @@ var handler = async (m, { conn, args }) => {
 
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, link, fproducto2, m, { detectLink: true })
+conn.reply(m.chat, '🚩' + link, fkontak, m, { detectLink: true })
 
 }
 handler.help = ['link']
 handler.tags = ['grupo']
 handler.command = /^link(gro?up)?$/i
+
 handler.group = true
 handler.botAdmin = true
+
 export default handler
