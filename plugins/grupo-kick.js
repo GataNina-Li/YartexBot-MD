4d1e51d64607f38e633db0f52841ffe561f20f85
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-30 22:42:55 -0600

    © CuriosityBot-MD

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,15 +1,15 @@
 var handler = async (m, { conn, participants, usedPrefix, command }) => {
 
-let kicktext = `*⚠️ ETIQUETE A LA PERSONA O RESPONDA AL MENSAJE DE LA PERSONA QUE QUIERE ELIMINAR*\n\n❕ EJEMPLO:\n*${usedPrefix + command} @${global.owner[0][0]}*`
-if (!m.mentionedJid[0] && !m.quoted) return m.reply(kicktext, m.chat, { mentions: conn.parseMention(kicktext)}) 
+if (!m.mentionedJid[0] && !m.quoted) return conn.reply(m.chat, '🎌 *Etiquete o responda al mensaje de la persona que quiere eliminar*', m, fake, ) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-let owr = m.chat.split`-`[0]
+
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
 
 }
 handler.help = ['kick']
 handler.tags = ['grupo']
 handler.command = /^(kick|echar|hechar|sacar|ban)$/i
+
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
