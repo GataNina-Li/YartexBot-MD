525b82567d57fdc9479221face0003857c85c112
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-01-01 12:12:45 -0600

    © CuriosityBot-MD

--- plugins/grupo-setdesc.js
+++ plugins/grupo-setdesc.js
@@ -2,13 +2,14 @@ var handler = async (m, { conn, text, isROwner, isOwner }) => {
 
 if (text) {
 await conn.groupUpdateDescription(m.chat, text)
-m.reply(`*✅ SE A CAMBIADO LA DESCRIPCIÓN*`)
-} else throw '*⚠️ QUE DESCRIPCIÓN PONGO?*'
+conn.reply(m.chat, `✅ *Se actualizo la descripción con éxito*`, m, fake, )
+} else return conn.reply(m.chat, '🎌 *Ingrese la nueva descripción para el grupo*\n\nEjmplo, !setdesc Welcome to the group', m, fake, )
 
 }
 handler.help = ['setdesc']
 handler.tags = ['grupo']
 handler.command = /^set(desk)?(desc)$/i
+
 handler.botAdmin = true
 handler.group = true
 handler.admin = true
