80ca9d29d56e422ae72e70a59ac58037b46c1fa2
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-01-02 15:21:36 -0600

    © CuriosityBot-MD

--- plugins/grupo-setname.js
+++ plugins/grupo-setname.js
@@ -2,12 +2,13 @@ var handler = async (m, { conn, text, isROwner, isOwner }) => {
 
 if (!text) return conn.reply(m.chat, '🎌 *Ingrese el nuevo nombre del grupo*\n\nEjmplo, !setname Curiosity', m, fake, )
 await conn.groupUpdateSubject(m.chat, text)
-conn.reply(m.chat, `🚩 ${text ? `${text} es el nuevo nombre de este grupo` : 'No pusieron un nombre'}`, m, fake, )
+conn.reply(m.chat, `🚩 ${text ? `${text} *es el nuevo nombre de este grupo*` : '*No pusieron un nombre*'}`, m, fake, )
  
 }
 handler.help = ['setname']
 handler.tags = ['grupo']
 handler.command = /^setname$/i
+
 handler.botAdmin = true
 handler.group = true
 handler.admin = true
