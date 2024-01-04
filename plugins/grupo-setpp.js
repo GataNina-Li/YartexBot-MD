e906bffdd3343a0074fcbb4e7d30e811f29df436
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-01-03 20:23:04 -0600

    © CuriosityBot-MD

--- plugins/grupo-setpp.js
+++ plugins/grupo-setpp.js
@@ -4,12 +4,15 @@ let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/image/.test(mime)) {
 let img = await q.download()
-if (!img) throw '*⚠️️ RESPONDE A UNA IMAGEN*'
-await conn.updateProfilePicture(m.chat, img).then(_ => m.reply('❕ *_Imagen actualizada con éxito_*'))
-} else throw '*⚠️️ RESPONDE A UNA IMAGEN*'
+if (!img) return conn.reply(m.chat, '🎌 *Debera etiquetar una imagen*', m, fake, )
+await conn.updateProfilePicture(m.chat, img).then(_ => conn.reply(m.chat, '✅ *Imagen actualizada con éxito*', m, fake, ))
+} else return conn.reply(m.chat, '🎌 *Debera etiquetar una imagen*', m, fake, )
 
 }
+handler.help = ['setpp', 'setppgc', 'setppgroup']
+handler.tags = ['grupo']
 handler.command = /^setpp(group|grup|gc)?$/i
+
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
