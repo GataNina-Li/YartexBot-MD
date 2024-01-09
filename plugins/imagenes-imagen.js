53527af4d941cb0a554f35dfe883dfad69f79d46
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-01-08 19:06:03 -0600

    © CuriosityBot-MD

--- plugins/imagenes-imagen.js
+++ plugins/imagenes-imagen.js
@@ -1,20 +1,25 @@
 import { googleImage } from '@bochilteam/scraper'
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+var handler = async (m, { conn, text, usedPrefix, command }) => {
+
 const prohibited = ['caca', 'polla', 'porno', 'porn', 'gore', 'cum', 'semen', 'puta', 'puto', 'culo', 'putita', 'putito','pussy', 'hentai', 'pene', 'coño', 'asesinato', 'zoofilia', 'mia khalifa', 'desnudo', 'desnuda', 'cuca', 'chocha', 'muertos', 'pornhub', 'xnxx', 'xvideos', 'teta', 'vagina', 'marsha may', 'misha cross', 'sexmex', 'furry', 'furro', 'furra', 'xxx', 'rule34', 'panocha', 'pedofilia', 'necrofilia', 'pinga', 'horny', 'ass', 'nude', 'popo', 'nsfw', 'femdom', 'futanari', 'erofeet', 'sexo', 'sex', 'yuri', 'ero', 'ecchi', 'blowjob', 'anal', 'ahegao', 'pija', 'verga', 'trasero', 'violation', 'violacion', 'bdsm', 'cachonda', '+18', 'cp', 'mia marin', 'lana rhoades', 'cepesito', 'hot', 'buceta', 'xxx']
 if (prohibited.some(word => m.text.toLowerCase().includes(word))) return conn.reply(m.chat, '🚩 *No dare resultado a tu solicitud*', m, fake, )
 
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-if (!text) throw `*⚠️ INGRESE UN TEXTO*\n\n💡 EJEMPLO\n*${usedPrefix + command} Curiosity*`
+if (!text) return conn.reply(m.chat, `🎌 *Bebera citar un texto a buscar*\n\nEjemplo, !${command} WhatsApp Bot*`, m, fake, )
 m.react(done)
+
 const res = await googleImage(text)
 let image = res.getRandom()
 let link = image
-conn.sendFile(m.chat, link, 'error.jpg', `📍 *Resultado de:* ${text}
-🔎 *Buscado en:* Google`, fliveLoc, m) 
+
+conn.sendFile(m.chat, link, 'error.jpg', `🚩 *Resultado de:* ${text}`, m, fake, )
+
 }
-handler.help = ['gimage <query>', 'imagen <query>']
-handler.tags = ['dl']
+handler.help = ['gimage', 'imagen']
+handler.tags = ['imagenes']
 handler.command = /^(gimage|image|imagen)$/i
-handler.exp = 20
+
+handler.limit = true
 handler.register = true
+
 export default handler
