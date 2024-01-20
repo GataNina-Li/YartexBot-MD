f1af71ff6fce0c0de43f831c254dcc6684b538db
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-01-20 08:23:20 -0600

    © CuriosityBot-MD

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -6,7 +6,7 @@ import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 
 var handler = async (m, { conn, command, args, text, usedPrefix }) => {
 
-if (!text) return conn.reply(m.chat, `🎌 *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} Banners - Someone To You`,  m, fake, )
+if (!text) return conn.reply(m.chat, `🎌 *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} New West - Those Eyes`,  m, fake, )
 m.react(rwait)
 
 try {
