b60073ebf381128e4de59e940cd5439ebd184c38
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-01-28 13:42:44 -0600

    © CuriosityBot-MD

--- main.js
+++ main.js
@@ -135,7 +135,7 @@ const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : false,
 mobile: MethodMobile, 
-browser: ['CuriosityBot-MD', 'Chrome', '20.0.04'],
+browser: ['CuriosityBot', 'Chrome', '20.0.04'],
 auth: {
 creds: state.creds,
 keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
