6e7a584d113d0fe44944b4041c380be9202edfe5
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-01-28 15:32:36 -0600

    © CuriosityBot-MD

--- plugins/tools-mirar.js
+++ plugins/tools-mirar.js
@@ -1,6 +1,6 @@
 var handler = async (m, { conn }) => {
 
-if (!/viewOnce/.test(m.quoted?.mtype)) throw '*⚠️ RESPONDE A UN VÍDEO O FOTO QUE ESTÁ PARA VER SOLO UNA VES*'
+if (!/viewOnce/.test(m.quoted?.mtype)) return conn.reply(m.chat, '🚩 *Responda a un archivo multimedia con ver una vez*', m, fake, )
 let mtype = Object.keys(m.quoted.message)[0]
 let buffer = await m.quoted.download()
 let caption = m.quoted.message[mtype].caption || ''
@@ -8,7 +8,10 @@ conn.sendMessage(m.chat, { [mtype.replace(/Message/, '')]: buffer, caption }, { 
 	
 }
 handler.help = ['readviewonce']
-handler.tags = ['implementos']
+handler.tags = ['tools']
 handler.command = /^retrieve|readviewonce|mirar/i
+
+handler.limit = true
+handler.register = true
 
 export default handler
