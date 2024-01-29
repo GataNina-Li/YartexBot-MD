c6309b082d91d3991cce04c229ce1c4fa4d8f345
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-01-28 21:36:19 -0600

    © CuriosityBot-MD

--- plugins/implementos-nowa.js
+++ plugins/implementos-nowa.js
@@ -2,8 +2,8 @@ var handler = async (m, {conn, text, usedPrefix, command}) => {
 
 const regex = /x/g
   
-if (!text) throw '*⚠️ FALTO EL NÚMERO*'
-if (!text.match(regex)) throw `*❕ EJEMPLO DE USO:*\n${usedPrefix + command} 521999340434x*`
+if (!text) return conn.reply(m.chat, '🚩 *Número faltante*', m, fake, )
+if (!text.match(regex)) return conn.reply(m.chat, `🚩 *Ejemplo de uso*\n${usedPrefix + command} 5217299999x*`, m, fake, )
 let random = text.match(regex).length
 let total = Math.pow(10, random)
 let array = []
@@ -15,8 +15,8 @@ let info = await conn.fetchStatus(result).catch((_) => {})
 array.push({exists: true, jid: result, ...info})
 } else {
 array.push({exists: false, jid: result})}}
-let txt = 'Registrados\n\n' + array.filter((v) => v.exists).map((v) => `• Nro: wa.me/${v.jid.split('@')[0]}\n*• Bio:* ${v.status || 'Sin descripcion'}\n*• Fecha:* ${formatDate(v.setAt)}`).join('\n\n') + '\n\n*No registrados*\n\n' + array.filter((v) => !v.exists).map((v) => v.jid.split('@')[0]).join('\n');
-m.reply(txt)
+let txt = '🚩 *Registrados*\n\n' + array.filter((v) => v.exists).map((v) => `${readMore}• Nro: wa.me/${v.jid.split('@')[0]}\n*• Bio:* ${v.status || 'Sin descripcion'}\n*• Fecha:* ${formatDate(v.setAt)}`).join('\n\n') + '\n\n*No registrados*\n\n' + array.filter((v) => !v.exists).map((v) => v.jid.split('@')[0]).join('\n')
+conn.reply(m.chat, txt, m, fake, )
 
 }
 handler.help = ['nowa']
@@ -27,5 +27,5 @@ export default handler
 
 function formatDate(n, locale = 'id') {
 var d = new Date(n)
-return d.toLocaleDateString(locale, {timeZone: 'Asia/Jakarta'})
+return d.toLocaleDateString(locale, {timeZone: 'America/Los_Angeles'})
 }
