d1015e293c096e4ffdf47202c60db0cab1bca9ab
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-01 16:47:26 -0600

    © CuriosityBot-MD

--- plugins/tools-readqr.js
+++ plugins/tools-readqr.js
@@ -5,17 +5,20 @@ var handler = async (m, {conn, text, usedPrefix, command}) => {
 
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw '*⚠️ RESPONDA A UNA IMAGEN*'
+if (!mime) conn.reply(m.chat, '🚩 *Responda a un código QR*', m, fake, )
 let img = await q.download?.()
 let url = await uploadImage(img)
 let anu = await fetch(`https://api.lolhuman.xyz/api/read-qr?apikey=${lolkeysapi}&img=${url}`)
 let json = await anu.json()
 
-await m.reply(`*🧃 El Texto del Codigo QR Es:* ${json.result}`)
+await conn.reply(m.chat, `🎌 *El Texto del Codigo QR Es:* ${json.result}`, m, fake, )
   
 }
 handler.help = ['readqr']
-handler.tags = ['implementos']
+handler.tags = ['tools']
 handler.command = /^(readqr)$/i
+
+handler.limit = true
+handler.register = true
 
 export default handler
