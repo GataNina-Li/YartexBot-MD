b15d7a74f0d210b40c308fbeab0cfdc4e49d2eb0
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-01 17:25:43 -0600

    © CuriosityBot-MD

--- plugins/tools-translate.js
+++ plugins/tools-translate.js
@@ -3,6 +3,7 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { args, usedPrefix, command }) => {
 
+if (command == 'translate' || command == 'traducir') {
 let msg = `🚩 *Uso correcto*\n!${command} (idioma) (texto)\n\nEjemplo, !${command} es Hello`
 if (!args || !args[0]) return conn.reply(m.chat, msg, m, fake, )
 let lang = args[0]
@@ -23,13 +24,16 @@ let result2 = loll.result.translated
 await conn.reply(m.chat, '*Traducción:* ' + result2, m, fake, )
 } catch {
 await conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, fake, )
-}}
-
+}}}
+if (command == 'codesidioma') {
 let codesidioma = '*Español:* es\n*Welsh:* cy\n*Vietnamese:* vi\n*Turkish:* tr\n*Thai:* th\n*Tamil:* ta\n*Swedish:* sv\n*Swahili:* sw\n*Slovak:* sk\n*Serbian:* sr\n*Russian:* ru\n*Romanian:* ro\n*Portuguese:* pt\n*Polish:* pl\n*Norwegian:* no\n*Macedonian:* mk\n*Latvian:* lv\n*Latin:* la\n*Korean:* ko\n*Japanese:* ja\n*Italian:* it\n*Indonesian:* id\n*Icelandic:* is\n*Hungarian:* hu\n*Hindi:* hi\n*Haitian Creole:* ht\n*Greek:* el\n*German:* de\n*French:* fr\n*Finnish:* fi\n*Esperanto:* eo\n*English:* en\n*Dutch:* nl\n*Danish:* da\n*Czech:* cs\n*Croatian:* hr\n*Chinese:* zh\n*Catalan:* ca\n*Armenian:* hy\n*Arabic:* ar\n*Albanian:* sq\n*Afrikaans:* af'
+m.reply(codesidioma)
+}
+
 }
 handler.help = ['traducir']
 handler.tags = ['tools']
-handler.command = /^(translate|traducir|trad)$/i
+handler.command = /^(translate|traducir|codesidioma)$/i
 
 handler.register = true
 handler.limit = true
