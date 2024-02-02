8799d7cb41f42995714a039e0077fcb600e6dee8
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-01 18:15:15 -0600

    © CuriosityBot-MD

--- plugins/tw.js
+++ plugins/tw.js
@@ -3,8 +3,10 @@ const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/b
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
+ let text = wm
 const perfil_sc = await conn.profilePictureUrl(m.sender, "image").catch(_ => "https://i.pinimg.com/564x/da/4b/9b/da4b9b9a26146c50c951410d9d2f1037.jpg")
-let msg = await generateWAMessageFromContent(m.chat, { locationMessage: {
+
+ let msg = await generateWAMessageFromContent(m.chat, { locationMessage: {
   degreesLatitude: 0,
   degreesLongitude: 0,
   name: 'Github del bot',
@@ -19,6 +21,8 @@ let msg = await generateWAMessageFromContent(m.chat, { locationMessage: {
   jpegThumbnail: imagen1
 }}, { quoted: m })
 conn.relayMessage(m.chat, msg.message, {})
+ conn.sendMessage(m.chat, {text: text.trim(), mentions: [...text.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: { mentionedJid: [...text.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": wm, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen2, "mediaUrl": global.nn, "sourceUrl": global.nn}}}, {quoted: fproducto});
+   
 }
 
 //let str = '*🍧 CuriosityBot-MD*'
