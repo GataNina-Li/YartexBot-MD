6c054e024304932303df9fa10cdacfa3809c2ea2
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-01 19:49:36 -0600

    © CuriosityBot-MD

--- plugins/bot-faketest.js
+++ plugins/bot-faketest.js
@@ -3,21 +3,25 @@ const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/b
 
 var handler = async(m, { conn, text, usedPrefix, command }) => {
 
+if (command == 'fakeloc') {
 const perfil_sc = await conn.profilePictureUrl(m.sender, "image").catch(_ => "https://i.pinimg.com/564x/da/4b/9b/da4b9b9a26146c50c951410d9d2f1037.jpg")
 let msg = await generateWAMessageFromContent(m.chat, { locationMessage: { degreesLatitude: 0, degreesLongitude: 0, name: 'Github del bot', address: 'By Azami', url: md, isLive: true, accuracyInMeters: 0, speedInMps: 0, degreesClockwiseFromMagneticNorth: 2, comment: 'Hola', jpegThumbnail: imagen3 }}, { quoted: m })
 
 conn.relayMessage(m.chat, msg.message, {})
- 
+}
+if (command == 'fake2img') {
 conn.sendMessage(m.chat, {image: imagen1, caption: wm, mentions: [...text.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: { mentionedJid: [...text.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": wm, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen2, "mediaUrl": global.nn, "sourceUrl": global.nn}}}, {quoted: fproducto})
-   
+}
+ 
+if (command == 'sendurl') {
+let str = '*🍧 CuriosityBot-MD*'
+await conn.sendUrl(m.chat, str, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen1, thumbnailUrl: imagen1, title: '🍓 Azami', }})
 }
 
-//let str = '*🍧 CuriosityBot-MD*'
-//await conn.sendUrl(m.chat, str, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen1, thumbnailUrl: imagen1, title: '🍓 Azami', }})
-
+}
 handler.help = ['listafakes']
 handler.tags = ['bot']
-handler.command = /^(fake2img|fakeloc)$/i
+handler.command = /^(fake2img|fakeloc|sendurl)$/i
 
 handler.rowner = true
 
