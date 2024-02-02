777ae1d363a962e35b385f00b14418320e58cacb
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-01 21:22:53 -0600

    © CuriosityBot-MD

--- plugins/bot-faketest.js
+++ plugins/bot-faketest.js
@@ -40,15 +40,15 @@ conn.reply(m.chat, wm, ftextt)
 }
 
 if (command == 'fliveLoc') {
-conn.reply(m.chat, wm, m, fliveLoc)
+conn.reply(m.chat, wm, fliveLoc)
 }
 
 if (command == 'liveLoc') {
-conn.reply(m.chat, wm, m, fliveLoc2)
+conn.reply(m.chat, wm, fliveLoc2)
 }
 
 if (command == 'fLoc') {
-conn.reply(m.chat, wm, m, fliveLoc3)
+conn.reply(m.chat, wm, fliveLoc3)
 }
 
 /*if (command == 'fliveLoc') {
