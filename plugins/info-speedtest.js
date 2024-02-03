15de8183890460fe7832b14cca6d8f5f240149c3
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-03 11:01:01 -0600

    © CuriosityBot-MD

--- plugins/info-speedtest.js
+++ plugins/info-speedtest.js
@@ -2,6 +2,14 @@ import cp from 'child_process';
 import {promisify} from 'util';
 const exec = promisify(cp.exec).bind(cp);
 const handler = async (m) => {
+  const { key } = await conn.sendMessage(m.chat, {text: `🚀 Espere un momento`}, {quoted: m})
+await delay(1000 * 1)
+await conn.sendMessage(m.chat, {text: `📑 Recuperando la lista de servidores speedtest.net...`, edit: key})
+await delay(2000 * 1)
+await conn.sendMessage(m.chat, {text: `🔎 Seleccionando el mejor servidor según el ping...`, edit: key})
+await delay(2000 * 1)
+await conn.sendMessage(m.chat, {text: `✅ *Hecho*`, edit: key})
+
   await conn.reply(m.chat, global.wait, m);
   let o;
   try {
@@ -10,6 +18,8 @@ const handler = async (m) => {
     o = e;
   } finally {
     const {stdout, stderr} = o;
+
+    await delay(5000 * 1)
     if (stdout.trim()) m.reply(stdout);
     if (stderr.trim()) m.reply(stderr);
   }
@@ -19,3 +29,5 @@ handler.tags = ['info'];
 handler.command = /^(speedtest?|test?speed)$/i;
 
 export default handler;
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
