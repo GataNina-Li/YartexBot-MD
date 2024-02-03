2cc98eb0d43c56a2cd326470aec0a5040552d2ec
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-02 18:18:54 -0600

    © CuriosityBot-MD

--- plugins/info-grupos-lista.js
+++ plugins/info-grupos-lista.js
@@ -3,7 +3,7 @@ var handler = async (m, { conn }) => {
 let txt = ''
 for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `${await conn.getName(jid)}\n🪪${jid} [${chat?.metadata?.read_only ? 'Salido' : 'Unido'}]\n\n`
     
-m.reply(m.chat, `🚩 *Lista De Grupos*:
+conn.reply(m.chat, `🚩 *Lista De Grupos*:
 
 ${txt}`, m, fake, )
 
