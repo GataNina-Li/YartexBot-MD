c4bd62ac9d606b9cfed8af3c8f8f7d5a97cff338
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-03 11:14:00 -0600

    © CuriosityBot-MD

--- speed.py
+++ speed.py
@@ -1914,7 +1914,7 @@ def shell():
             quiet)
 
     if not args.mini:
-        printer('', quiet)
+
         try:
             speedtest.get_servers(servers=args.server, exclude=args.exclude)
         except NoMatchedServers:
@@ -1932,9 +1932,9 @@ def shell():
             )
 
         if args.server and len(args.server) == 1:
-            printer('', quiet)
+            
         else:
-            printer('', quiet)
+            
         speedtest.get_best_server()
     elif args.mini:
         speedtest.get_best_server(speedtest.set_mini_server(args.mini))
@@ -1945,7 +1945,7 @@ def shell():
             '\n⚡ *Ping :* %(latency)s ms' % results.server, quiet)
 
     if args.download:
-        printer('', quiet,
+        
                 end=('', '\n')[bool(debug)])
         speedtest.download(
             callback=callback,
@@ -1960,7 +1960,7 @@ def shell():
 
     if args.upload:
         speedtest.upload()
-        printer('*🚀 Subir:* %0.2f M%s/s' %
+        printer('*🚀 Subida:* %0.2f M%s/s' %
                 ((results.upload / 1000.0 / 1000.0) / args.units[1],
                  args.units[0]),
                 quiet)
