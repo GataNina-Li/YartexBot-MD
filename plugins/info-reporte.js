5337748c557989fce69b486930a323d4d29f4533
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-03 09:37:09 -0600

    © CuriosityBot-MD

--- plugins/info-reporte.js
+++ plugins/info-reporte.js
@@ -4,14 +4,17 @@ if (!text) return conn.reply(m.chat, `🎌 *Escriba su reporte*\n\nEjemplo, !${c
 if (text.length < 10) return conn.reply(m.chat, `🚩 *Mínimo 10 caracteres para hacer el reporte*`, m, fake, )
 if (text.length > 1000) return conn.reply(m.chat, `🚩 *Máximo 1000 caracteres para hacer el reporte.*`, m, fake, )
 
-let teks = `⚠️ *Reporte* ⚠️\n\n *\nWa.me/${m.sender.split`@`[0]}\n*📝 MENSAJE*\n${text}`
+let teks = `⚠️ *Reporte* ⚠️\n\n⬡ *Numero*\nWa.me/${m.sender.split`@`[0]}\n\n⬡ *Mensaje*\n${text}`
 conn.reply('5217294888993@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, { contextInfo: { mentionedJid: [m.sender] }})
-m.reply(`*[⚠️] Exito el reporte ha sido enviado a mí Creadora. Tendrá una respuesta pronto. Si se detecta que el reporte es Falso será Ignorado el reporte.*`)
+
+conn.reply(m.chat, `🚩 *El reporte se envió a mi creador, entra una respuesta más tarde*`, m, fake, )
 
 }
 
-handler.help = ['reporte', 'request'].map(v => v + ' <teks>')
+handler.help = ['reporte', 'request']
 handler.tags = ['owner']
-handler.exp = 100 
-handler.command = /^(report|request|reporte|bugs|bug|report-owner|reportes|reportar)$/i 
+handler.command = /^(report|request|reporte|bugs|bug|report-owner|reportes|reportar)$/i
+
+handler.register = true
+
 export default handler
