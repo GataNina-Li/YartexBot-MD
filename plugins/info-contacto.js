0e7b759e78928ee7c55aa2ab21fef96ca02fe207
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-03 01:18:30 -0600

    © CuriosityBot-MD

--- plugins/info-contacto.js
+++ plugins/info-contacto.js
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch'
-let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+
+var handler = async (m, { conn, usedPrefix, text, args, command }) => {
+
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let pp = await conn.profilePictureUrl(who).catch(_ => '')
 let name = await conn.getName(who)
@@ -7,10 +9,15 @@ let biografia = await conn.fetchStatus('5217294888993' +'@s.whatsapp.net').catch
 let biografiaBot = await conn.fetchStatus('5214531287294' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
 
 await conn.sendContactArray(m.chat, [
-[devnum, `${await conn.getName('5217294888993'+'@s.whatsapp.net')}`, `🍭 Creador`, dev, `No correo`, `🇲🇽 México`, `https://www.youtube.com/@Azami_19`, biografia],
+[devnum, `${await conn.getName('5217294888993'+'@s.whatsapp.net')}`, `🍭 Creador`, dev, `No correo`, `🇲🇽 México`, `https://www.youtube.com/@Azami_YT`, biografia],
 [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 CuriosityBot-MD`, `📵 No hacer spam`, `No correo`, `🇲🇽 México`, `https://github.com/AzamiJs/CuriosityBot-MD`, biografiaBot]
 ], m)
-} 
+  
+}
+handler.help = ['owner', 'contacto', 'creador', 'contactos']
+handler.tags = ['info']
+handler.command = /^(owner|contacto|creador|contactos)/i
 
-handler.command = /^(sentcontact)/i
+handler.register = true
+
 export default handler
