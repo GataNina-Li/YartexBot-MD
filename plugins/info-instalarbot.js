94908b9cfdf85d9f7919755cd8d89239c78b37da
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-02 19:01:52 -0600

    © CuriosityBot-MD

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -36,15 +36,15 @@ handler.before = async m => {
 
 if (/^comandos$/i.test(m.text) ) {
 m.reply('termux-setup-storage')
-
+await delay(1000 * 1)
 m.reply('apt-get update -y && apt-get upgrade -y')
-
+await delay(1000 * 1)
 m.reply('pkg install -y git nodejs ffmpeg imagemagick && pkg install yarn')
-
+await delay(1000 * 1)
 m.reply('git clone https://github.com/AzamiJs/CuriosityBot-MD && cd CuriosityBot-MD && yarn install && npm install ')
-
+await delay(1000 * 1)
 m.reply('ls')
-
+await delay(1000 * 1)
 m.reply('npm start')
 }}
 
@@ -54,3 +54,5 @@ handler.tags = ['main']
 handler.command = /^(instalarbot)/i
 
 export default handler
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
