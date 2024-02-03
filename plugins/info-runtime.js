d745663b1bc10c0f70784657769cb8dbf2960a2d
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-03 10:15:59 -0600

    © CuriosityBot-MD

--- plugins/info-runtime.js
+++ plugins/info-runtime.js
@@ -1,157 +1,35 @@
-/**
-POR FAVOR TENGAN LA AMABILIDAD Y BONDAD DE NO CAMBIAR MÍNIMAMENTE LOS CRÉDITOS DE GATABOT-MD, 
-SI VAS A AÑADIR TUS DATOS O CRÉDITOS, ESTA BIEN. PERO NO QUITEN LOS QUE YA ESTAN DE GATABOT-MD, GRACIAS 
-**/
-
-/** PLEASE BE KIND AND KINDNESS NOT TO MINIMALLY CHANGE GATABOT-MD CREDITS, 
-IF YOU ARE GOING TO ADD YOUR DATA OR CREDITS, IT'S OK. BUT DO NOT REMOVE THOSE THAT ARE ALREADY FROM GATABOT-MD, THANK YOU **/
-/*import { cpus as _cpus, totalmem, freemem } from 'os'
-import util from 'util'
-import os from 'os'
-import fetch from 'node-fetch'
-import osu from 'node-os-utils'
 import { performance } from 'perf_hooks'
-import { sizeFormatter } from 'human-readable'
-let format = sizeFormatter({
-std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
-decimalPlaces: 2,
-keepTrailingZeroes: false,
-render: (literal, symbol) => `${literal} ${symbol}B`,
-})
-let handler = async (m, { conn, command, usedPrefix }) => {
-let picture = './storage/logos/Menu1.jpg'
-let name = await conn.getName(m.sender)
-let _uptime = process.uptime() * 1000
-let _muptime
-if (process.send) { process.send('uptime')
-_muptime = await new Promise(resolve => { process.once('message', resolve) 
-setTimeout(resolve, 1000) }) * 1000}
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let pp = './src/fg_logo.jpg'
-let uptime = clockString(_uptime)
-let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let muptime = clockString(_muptime)
-const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
-const used = process.memoryUsage()
-const cpus = _cpus().map(cpu => {
-cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
-return cpu
-})
+import osu from 'node-os-utils'
 
-const cpu = cpus.reduce((last, cpu, _, { length }) => {
-last.total += cpu.total
-last.speed += cpu.speed / length
-last.times.user += cpu.times.user
-last.times.nice += cpu.times.nice
-last.times.sys += cpu.times.sys
-last.times.idle += cpu.times.idle
-last.times.irq += cpu.times.irq
-return last
-  
-}, {
-speed: 0,
-total: 0,
-times: {
-user: 0,
-nice: 0,
-sys: 0,
-idle: 0,
-irq: 0
-}})
+var handler = async(m, { conn, command, usedPrefix}) => {
 
+try {
+
+let NotDetect = 'Not Detect'
 let old = performance.now()
+let cpu = osu.cpu
+let cpuCore = cpu.count()
+let drive = osu.drive
+let mem = osu.mem
+let netstat = osu.netstat
+let OS = osu.os.platform()
+let cpuModel = cpu.model()
+let cpuPer
+let p1 = cpu.usage().then(cpuPercentage => { cpuPer = cpuPercentage }).catch(() => { cpuPer = NotDetect })
+let driveTotal, driveUsed, drivePer
+let p2 = drive.info().then(info => { driveTotal = (info.totalGb + ' GB'), driveUsed = info.usedGb, drivePer = (info.usedPercentage + '%') }).catch(() => { driveTotal = NotDetect, driveUsed = NotDetect, drivePer = NotDetect })
+let ramTotal, ramUsed
+let p3 = mem.info().then(info => { ramTotal = info.totalMemMb, ramUsed = info.usedMemMb }).catch(() => { ramTotal = NotDetect, ramUsed = NotDetect })
+let netsIn, netsOut
+let p4 = netstat.inOut().then(info => { netsIn = (info.total.inputMb + ' MB'), netsOut = (info.total.outputMb + ' MB') }).catch(() => { netsIn = NotDetect, netsOut = NotDetect })
+await Promise.all([p1, p2, p3, p4])
+await conn.reply(m.chat, `_Probando ${command }..._`, m)
+let _ramTotal = (ramTotal + ' MB')
 let neww = performance.now()
-let speed = neww - old
-let info = `┏━━━━❰･𝐄𝐒𝐓𝐀𝐃𝐎 𝐃𝐄𝐋 𝐁𝐎𝐓･❱━━━━
-┃
-┃웃 Hola ${taguser}
-┃
-┃╍╍╍╍╍╍╍╍╍╍╍╍╍
-┃
-┃➢  𝚅𝙴𝙻𝙾𝙲𝙸𝙳𝙰𝙳 : *${speed}* ms
-┃➢  𝚁𝙰𝙼 𝙳𝙴𝙻 𝚂𝙴𝚁𝚅𝙸𝙳𝙾𝚁:  ${format(totalmem() - freemem())} / ${format(totalmem())}
-┃➢  𝙿𝙻𝙰𝚃𝙰𝙵𝙾𝚁𝙼𝙰 : ${os.platform()}
-┃➢  𝙰𝙲𝚃𝙸𝚅𝙾:  ${uptime}
-┃➢  𝙼𝙾𝙳𝙾: ${global.opts['self'] ? '𝙿𝚁𝙸𝚅𝙰𝙳𝙾 - 𝙿𝚁𝙸𝚅𝙰𝚃𝙴' : '𝙿𝚄𝙱𝙻𝙸𝙲𝙾 - 𝙿𝚄𝙱𝙻𝙸𝙲'}
-┃➢  𝙶𝚁𝚄𝙿𝙾𝚂 𝙱𝙰𝙽𝙴𝙰𝙳𝙾𝚂: ${Object.entries(global.db.data.users).filter(user => user[1].banned).length} 
-┃➢ 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂 𝙱𝙰𝙽𝙴𝙰𝙳𝙾𝚂: ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length} 
-┃➢ 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂 𝚃𝙾𝚃𝙰𝙻𝙴𝚂: ${Object.keys(global.db.data.users).length} 
-┃
-┗━━━━━━━━━━━━━`.trim()
- // conn.sendMessage(m.chat, {image: picture, caption: info, { quoted: fkontak})
-conn.sendFile(m.chat, picture, 'curiosity.jpg', info, fkontak, m)
-//conn.sendButton(m.chat, info, `${wm}`, pp, [['MENU', '.menu']], fkontak, m)
-}
-handler.help = ['estado']
-handler.tags = ['main']
-handler.command = ['runtime', 'uptime', 'estado']
-handler.register = true
-export default handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-
-function clockString(ms) {
-let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
-*/
-
-import { performance } from 'perf_hooks'
-import osu from 'node-os-utils'
-let handler = async(m, { conn, command, usedPrefix, DevMode }) => {
-    try {
-        let NotDetect = 'Not Detect'
-        let old = performance.now()
-        let cpu = osu.cpu
-        let cpuCore = cpu.count()
-        let drive = osu.drive
-        let mem = osu.mem
-        let netstat = osu.netstat
-        let OS = osu.os.platform()
-        let cpuModel = cpu.model()
-        let cpuPer
-        let p1 = cpu.usage().then(cpuPercentage => {
-            cpuPer = cpuPercentage
-        }).catch(() => {
-            cpuPer = NotDetect
-        })
-        let driveTotal, driveUsed, drivePer
-        let p2 = drive.info().then(info => {
-            driveTotal = (info.totalGb + ' GB'),
-                driveUsed = info.usedGb,
-                drivePer = (info.usedPercentage + '%')
-        }).catch(() => {
-            driveTotal = NotDetect,
-                driveUsed = NotDetect,
-                drivePer = NotDetect
-        })
-        let ramTotal, ramUsed
-        let p3 = mem.info().then(info => {
-            ramTotal = info.totalMemMb,
-                ramUsed = info.usedMemMb
-        }).catch(() => {
-            ramTotal = NotDetect,
-                ramUsed = NotDetect
-        })
-        let netsIn, netsOut
-        let p4 = netstat.inOut().then(info => {
-            netsIn = (info.total.inputMb + ' MB'),
-                netsOut = (info.total.outputMb + ' MB')
-        }).catch(() => {
-            netsIn = NotDetect,
-                netsOut = NotDetect
-        })
-        await Promise.all([p1, p2, p3, p4])
-        await conn.reply(m.chat, `_Testing ${command }..._`, m)
-        let _ramTotal = (ramTotal + ' MB')
-        let neww = performance.now()
         
 
-var txt = `
-*「 Status 」*
+var txt = `*「 Estado 」*
+
 OS : *${OS}*
 CPU Model : *${cpuModel}*
 CPU Core : *${cpuCore} Core*
@@ -160,37 +38,21 @@ Ram : *${ramUsed} / ${_ramTotal}(${/[0-9.+/]/g.test(ramUsed) &&  /[0-9.+/]/g.tes
 Drive : *${driveUsed} / ${driveTotal} (${drivePer})*
 Ping : *${Math.round(neww - old)} ms*
 Internet IN : *${netsIn}*
-Internet OUT : *${netsOut}*
-`
+Internet OUT : *${netsOut}*`
 
-conn.relayMessage(m.chat, {
-extendedTextMessage:{
-                text: txt, 
-                contextInfo: {
-                     externalAdReply: {
-                        title: "",
-                        mediaType: 1,
-                        previewType: 0,
-                        renderLargerThumbnail: true,
-                        thumbnailUrl: 'https://telegra.ph/file/ec8cf04e3a2890d3dce9c.jpg',
-                        sourceUrl: ''
-                    }
-                }, mentions: [m.sender]
-}}, {})
-        console.log(OS)
-    } catch (e) {
-        console.log(e)
-        conn.reply(m.chat, eror, m)
-        if (DevMode) {
-            for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) {
-                conn.reply(jid, 'Status.js error\nNo: *' + m.sender.split `@` [0] + '*\nCommand: *' + m.text + '*\n\n*' + e + '*', m)
-            }
-        }
-    }
+conn.relayMessage(m.chat, { extendedTextMessage:{ text: txt, contextInfo: { externalAdReply: { title: '', mediaType: 1, previewType: 0, renderLargerThumbnail: true, thumbnailUrl: 'https://telegra.ph/file/ec8cf04e3a2890d3dce9c.jpg', sourceUrl: '' }}, mentions: [m.sender] }}, {})
+
+console.log(OS)
+} catch (e) {
+console.log(e)
+conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, fake, )}
+
 }
-handler.help = ['', 'bot'].map(v => 'status' + v)
-handler.tags = ['info']
-handler.command = /^(bot)?stat(us)?(bot)?$/i
+handler.help = ['estado', 'status']
+handler.tags = ['bot']
+handler.command = /^(estado)?stat(us)?(bot)?$/i
+
+handler.register = true
 
 export default handler
 
