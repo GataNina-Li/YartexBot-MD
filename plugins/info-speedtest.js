9e821c3e9665a0a249259a013c7ce0e0f578adee
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-03 11:07:12 -0600

    © CuriosityBot-MD

--- plugins/info-speedtest.js
+++ plugins/info-speedtest.js
@@ -1,8 +1,11 @@
-import cp from 'child_process';
-import {promisify} from 'util';
-const exec = promisify(cp.exec).bind(cp);
-const handler = async (m) => {
-  const { key } = await conn.sendMessage(m.chat, {text: `🚀 Espere un momento`}, {quoted: m})
+import cp from 'child_process'
+import {promisify} from 'util'
+
+const exec = promisify(cp.exec).bind(cp)
+
+var handler = async (m) => {
+  
+const { key } = await conn.sendMessage(m.chat, {text: `🚀 Espere un momento`}, {quoted: m})
 await delay(1000 * 1)
 await conn.sendMessage(m.chat, {text: `📑 Recuperando la lista de servidores speedtest.net...`, edit: key})
 await delay(2000 * 1)
@@ -10,24 +13,28 @@ await conn.sendMessage(m.chat, {text: `🔎 Seleccionando el mejor servidor seg�
 await delay(2000 * 1)
 await conn.sendMessage(m.chat, {text: `✅ *Hecho*`, edit: key})
 
-  await conn.reply(m.chat, global.wait, m);
-  let o;
-  try {
-    o = await exec('python3 speed.py');
-  } catch (e) {
-    o = e;
-  } finally {
-    const {stdout, stderr} = o;
+let o
+  
+try {
 
-    await delay(5000 * 1)
-    if (stdout.trim()) m.reply(stdout);
-    if (stderr.trim()) m.reply(stderr);
-  }
-};
-handler.help = ['speedtest'];
-handler.tags = ['info'];
-handler.command = /^(speedtest?|test?speed)$/i;
+o = await exec('python3 speed.py')
+} catch (e) {
+o = e
+} finally {
+let {stdout, stderr} = o
 
-export default handler;
+
+if (stdout.trim()) m.reply(stdout)
+if (stderr.trim()) m.reply(stderr)
+}
+
+}
+handler.help = ['speedtest']
+handler.tags = ['bot']
+handler.command = /^(speedtest?|test?speed)$/i
+
+handler.register = true
+
+export default handler
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
