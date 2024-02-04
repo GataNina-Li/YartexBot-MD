6f71fa3eb512dd21912103155d33211e5182f335
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-03 18:56:53 -0600

    Test

--- plugins/internet-google.js
+++ plugins/internet-google.js
@@ -1,85 +1,60 @@
-/*import { googleIt } from '@bochilteam/scraper'
+import {googleIt} from '@bochilteam/scraper';
+import google from 'google-it';
+import axios from 'axios';
 let handler = async (m, { conn, command, args, usedPrefix }) => {
-const fetch = (await import('node-fetch')).default
-let full = /f$/i.test(command)
-let text = args.join` `
-if (!text) return conn.reply(m.chat, `*⚠️ ESCRIBA LO QUE QUIERE BUSCAR*\n\n❕ EJEMPLO:\n*${usedPrefix + command}* Curiosity`, m)
-let pp = './storage/menus/Menu1.jpg'
-let url = 'https://google.com/search?q=' + encodeURIComponent(text)
-let search = await googleIt(text)
-let msg = search.articles.map(({
-// header,
-title,
-url,
-description
-}) => {
-return `*🔍 ${title}*\n\n💻 ${url}\n\n📡 _${description}_\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈`
-}).join('\n\n')
+  const fetch = (await import('node-fetch')).default;
+  const text = args.join` `;
+  if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝙾 𝚃𝙴𝙼𝙰 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*', m);
+const url = 'https://google.com/search?q=' + encodeURIComponent(text);
+google({'query': text}).then(res => {
+let teks = `*RESULTADOS DE : _${text}_*\n\n${url}\n\n`
+for (let g of res) {
+teks += `_*${g.title}*_\n_${g.link}_\n_${g.snippet}_\n\n`
+} 
+const ss = `https://image.thum.io/get/fullpage/${url}`
+conn.sendFile(m.chat, ss, 'error.png', teks, m)
+//m.reply(teks)
+})
+} 
+handler.help = ['google', 'googlef'].map((v) => v + ' <pencarian>');
+handler.tags = ['internet'];
+handler.command = /^googlef?$/i;
+export default handler;
 
-
-try {
-let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).arrayBuffer()
-if (/<!DOCTYPE html>/i.test(ss.toBuffer().toString())) throw ''
-await conn.sendFile(m.chat, ss, 'error.png', url + '\n\n' + msg, m)
-} catch (e) {
-m.reply(msg)
-}}
-handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
-handler.tags = ['internet']
-handler.command = /^googlef?$/i
-handler.register = true
-
-export default handler
-
-*/
-
-/*import fetch from 'node-fetch'
-import googleIt from 'google-it'
-let handler = async (m, { conn, command, args }) => {
-  let full = /f$/i.test(command)
-  let text = args.join` `
-  if (!text) return conn.reply(m.chat, 'Tidak ada teks untuk di cari', m)
-  let url = 'https://google.com/search?q=' + encodeURIComponent(text)
-  let search = await googleIt({ query: text })
-  let msg = search.map(({ title, link, snippet}) => {
-    return `*${title}*\n_${link}_\n_${snippet}_`
-  }).join`\n\n`
+/*import {googleIt} from '@bochilteam/scraper';
+import axios from 'axios';
+const handler = async (m, {conn, command, args}) => {
+  const fetch = (await import('node-fetch')).default;
+  const text = args.join` `;
+  if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝙾 𝚃𝙴𝙼𝙰 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*', m);
+  const url = 'https://google.com/search?q=' + encodeURIComponent(text);
+  const search = await googleIt(text);
+  const msg = search.articles.map(({title, url, description}) => {
+    return `*${title}*\n_${url}_\n_${description}_`;
+  }).join('\n\n');
   try {
-    var logos = 'https://telegra.ph/file/cf62f2b8648a352548978.jpg'
-	conn.sendFile(m.chat, logos, 'logos.jpg', url + '\n\n' + msg, m)
-  } catch (e) {
-    m.reply(msg)
+    const ss = `https://image.thum.io/get/fullpage/${url}`;
+    await conn.sendFile(m.chat, ss, 'error.png', url + '\n\n' + msg, m);
+  } catch {
+    m.reply(msg);
   }
-}
-handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
-handler.tags = ['internet']
-handler.command = /^googlef?$/i
-handler.fail = null
+};
+handler.help = ['google', 'googlef'].map((v) => v + ' <pencarian>');
+handler.tags = ['internet'];
+handler.command = /^googlef?$/i;
+export default handler;
 
-export default handler*/
-
-import { googleIt } from '@bochilteam/scraper'
-let handler = async (m, { conn, command, args }) => {
-    const fetch = (await import('node-fetch')).default
-    let full = /f$/i.test(command)
-    let text = args.join` `
-    if (!text) return conn.reply(m.chat, 'Tidak ada teks untuk di cari', m)
-    let url = 'https://google.com/search?q=' + encodeURIComponent(text)
-    let search = await googleIt(text)
-    let msg = search.articles.map(({
-        // header,
-        title,
-        url,
-        description
-    }) => {
-        return `*${title}*\n_${url}_\n_${description}_`
-    }).join('\n\n')
-    m.reply(msg)
-}
-
-handler.help = ['google'].map(v => v + ' <search>')
-handler.tags = ['internet']
-handler.command = /^google?$/i
-
-
-export default handler
+ let ss2 = await ssweb(url, 'desktop')
+let dataa = ss2.result
+async function ssweb(url, device = 'desktop'){
+return new Promise((resolve, reject) => {
+const base = 'https://www.screenshotmachine.com'
+const param = { url: url, device: device, cacheLimit: 0 }
+axios({url: base + '/capture.php', method: 'POST', data: new URLSearchParams(Object.entries(param)), headers: { 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8' }}).then((data) => {
+const cookies = data.headers['set-cookie']
+if (data.data.status == 'success') {
+axios.get(base + '/' + data.data.link, { headers: { 'cookie': cookies.join('') }, responseType: 'arraybuffer' }).then(({ data }) => {
+let result = { status: 200, author: '@BrunoSobrino', result: data }
+resolve(result)})
+} else {
+reject({ status: 404, author: 'Ryzn', message: data.data })}}).catch(reject)})}*/
