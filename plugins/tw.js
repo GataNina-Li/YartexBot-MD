7e882fa868470d2510e4e1db1aaf1b1f3a183535
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-04 11:16:58 -0600

    © CuriosityBot-MD

--- plugins/tw.js
+++ plugins/tw.js
@@ -2,7 +2,7 @@ let { MessageType } = (await import('@adiwajshing/baileys')).default
 let { is } = (await import('cheerio/lib/api/traversing')).default
 import qrcode from 'qrcode'
 import fs from 'fs'
-import path form 'path'
+import path from 'path'
 
 if (global.conns instanceof Array) console.log()// for (let i of global.conns) global.conns[i] && global.conns[i].user ? global.conns[i].close().then(() => delete global.conns[id] && global.conns.splice(i, 1)).catch(global.conn.logger.error) : delete global.conns[i] && global.conns.splice(i, 1)
 else global.conns = []
