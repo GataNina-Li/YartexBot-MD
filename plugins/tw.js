1db1795bace0f8cf3a8277bb80d472e8a609db31
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-04 12:31:36 -0600

    Update tw.js

--- plugins/tw.js
+++ plugins/tw.js
@@ -3,14 +3,32 @@ import qrcode from 'qrcode'
 import fs from 'fs'
 import path from 'path'
 
+if (global.conns instanceof Array) console.log()
+else global.conns = []
 
-let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
+var handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
 
+let conn = new global.conn.constructor()
 let parent = args[0] && args[0] == 'plz' ? conn : global.conn
 
 conn.on('qr', async qr => {
 await parent.sendFile(m.chat, await qrcode.toDataURL(qr, { scale: 8 }), 'qrcode.png', wm, m)
 })
+conn.welcome = global.conn.welcome + ''
+conn.bye = global.conn.bye + ''
+conn.spromote = global.conn.spromote + ''
+conn.sdemote = global.conn.sdemote + ''
+conn.handler = global.conn.handler.bind(conn)
+conn.onDelete = global.conn.onDelete.bind(conn)
+conn.onParticipantsUpdate = global.conn.onParticipantsUpdate.bind(conn)
+conn.onGroupUpdate = global.conn.onGroupUpdate.bind(conn)
+conn.onCall = global.conn.onCall.bind(conn)
+conn.on('chat-update', conn.handler)
+conn.on('message-delete', conn.onDelete)
+conn.on('group-participants-update', conn.onParticipantsUpdate)
+conn.on('group-update', conn.onGroupUpdate)
+conn.on('CB:action,,call', conn.onCall)
+conn.regenerateQRIntervalMs = null
  
 m.reply(wm)
 
