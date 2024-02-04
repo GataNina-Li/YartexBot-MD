36469c2303f9f909a7a090f1ccc85ded631bfc13
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-03 19:44:38 -0600

    © CuriosityBot-MD

--- plugins/internet-npm.js
+++ plugins/internet-npm.js
@@ -1,18 +1,21 @@
 import fetch from 'node-fetch'
 
-let handler = async (m, { text }) => {
-	if (!text) throw 'Input Query'
-	let res = await fetch(`http://registry.npmjs.com/-/v1/search?text=${text}`)
-	let { objects } = await res.json()
-	if (!objects.length) throw `Query "${text}" not found :/`
-	let txt = objects.map(({ package: pkg }) => {
-		return `*${pkg.name}* (v${pkg.version})\n_${pkg.links.npm}_\n_${pkg.description}_`
-	}).join`\n\n`
-	m.reply(txt)
+var handler = async (m, { text }) => {
+
+if (!text) return conn.reply(m.chat, '🎌 *Ingrese lo que esta buscando*\n\nEjemplo, !npm module', m, fake, )
+let res = await fetch(`http://registry.npmjs.com/-/v1/search?text=${text}`)
+let { objects } = await res.json()
+if (!objects.length) return conn.reply(m.chat, `🚩 *La solicitud "${text}" no funciona*`, m, fake, )
+
+let txt = objects.map(({ package: pkg }) => {
+return `*${pkg.name}* (v${pkg.version})\n_${pkg.links.npm}_\n_${pkg.description}_` }).join`\n\n`
+conn.reply(m.chat, txt, m, fake, )
+
 }
 handler.help = ['npmsearch']
-handler.tags = ['tools']
+handler.tags = ['internet']
 handler.command = /^npmjs|npmsearch?$/i
 
-export default handler
+handler.register = true
 
+export default handler
