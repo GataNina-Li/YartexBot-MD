d3e9cd7cb7c0d9ef1a8643ee450f0841f9baf2e8
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-04 10:56:34 -0600

    © CuriosityBot-MD

--- plugins/jadibot-token.js
+++ plugins/jadibot-token.js
@@ -1,16 +1,23 @@
-import fs from "fs"
+import fs from 'fs'
+
 async function handler(m, {usedPrefix}) {
-    const user = m.sender.split("@")[0]
-    if (fs.existsSync("./jadibts/" + user + "/creds.json")) {
-        let token = Buffer.from(fs.readFileSync("./jadibts/" + user + "/creds.json"), "utf-8").toString("base64")
-        await m.reply(`El token te permite iniciar sesion en otros bots, recomendamos no compartirlo con nadie.\n\n*Tu token es:*`)
-        await m.reply(token)
-    } else {
-        await m.reply(`*No tienes ningun token activo, usa ${usedPrefix}jadibot para crear uno.*`)
-    }
-  }
-  handler.command = handler.help = ['token', 'gettoken', 'serbottoken'];
-  handler.tags = ['jadibot'];
-  handler.private = true
-  export default handler;
-  
+
+const user = m.sender.split('@')[0]
+if (fs.existsSync('./jadibts/' + user + '/creds.json')) {
+let token = Buffer.from(fs.readFileSync('./jadibts/' + user + '/creds.json'), 'utf-8').toString('base64')
+
+await conn.reply(m.chat, `🎌 *El token te permite iniciar sesion en otros bots, recomendamos no compartirlo con nadie*\n\nTu token es:`, m, fake, )
+await conn.reply(m.chat token, m, fake, )
+} else {
+await conn.reply(m.chat, `🚩 *No tienes ningun token activo, usa !jadibot para crear uno*`, m, fake, )
+}
+
+}
+handler.help = ['token', 'gettoken', 'serbottoken']
+handler.command = ['token', 'gettoken', 'serbottoken']
+handler.tags = ['jadibot']
+
+handler.private = true
+handler.register = true
+
+export default handler 
