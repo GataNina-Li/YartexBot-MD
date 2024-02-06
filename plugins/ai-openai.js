954aea84875f002f60f1cd4aae9802e94f1568cb
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-02-05 22:18:21 -0300

    Update ai-openai.js

--- plugins/ai-openai.js
+++ plugins/ai-openai.js
@@ -1,46 +1,44 @@
-import fetch from 'node-fetch'
-import axios from 'axios'
-import translate from '@vitalets/google-translate-api'
-import { Configuration, OpenAIApi } from 'openai'
-
-const configuration = new Configuration({ organization: global.openai_org_id, apiKey: global.openai_key })
-const openaiii = new OpenAIApi(configuration)
-
-var handler = async (m, { conn, text, usedPrefix, command }) => {
-
-if (usedPrefix == 'a' || usedPrefix == 'A') return
-if (!text) return conn.reply(m.chat, `*🎌 Ingrese una petición*\n\nEjemplo: !ia Pasos para crear una página`, m, fake)
-
+import fetch from 'node-fetch';
+import axios from 'axios';
+import translate from '@vitalets/google-translate-api';
+import {Configuration, OpenAIApi} from 'openai';
+const configuration = new Configuration({organization: global.openai_org_id, apiKey: global.openai_key});
+const openaiii = new OpenAIApi(configuration);
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+if (usedPrefix == 'a' || usedPrefix == 'A') return;
+if (!text) throw `[ ⚠️ ] 𝐈𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉 𝙊 𝙐𝙉𝘼 𝙊𝙍𝘿𝙀𝙉 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝘾𝙃𝘼𝙏𝙂𝙋𝙏\n\n❏ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊 𝘿𝙀 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉𝙀𝙎 𝙔 𝙊𝙍𝘿𝙀𝙉𝙀𝙎\n❏ ${usedPrefix + command} Recomienda un top 10 de películas de acción\n❏ ${usedPrefix + command} Codigo en JS para un juego de cartas`    
 try {
-conn.sendPresenceUpdate('composing', m.chat)
-
-// Traducir de indonesio a español
-const translation = await translate(text, { from: 'id', to: 'es' })
-const indonesianText = translation.text
-let syms = `Eres un asistente y tu nombre es CuriosityBot-MD, el nombre de tu dueño es Azami`
-let res = await openaiii.ChatGpt(indonesianText, syms)
-
-await m.reply(res.text)
-
-} catch (err) {
+conn.sendPresenceUpdate('composing', m.chat);
+let sistema1 = `Actuaras como un Bot de WhatsApp el cual fue creado por Alexis, tu seras CuriosityBug`;
+async function getOpenAIChatCompletion(texto) {
+const openaiAPIKey = global.openai_key;
+let chgptdb = global.chatgpt.data.users[m.sender];
+chgptdb.push({ role: 'user', content: texto });
+const url = "https://api.openai.com/v1/chat/completions";
+const headers = { "Content-Type": "application/json", "Authorization": `Bearer ${openaiAPIKey}` };
+const data = { "model": "gpt-3.5-turbo", "messages": [{ "role": "system", "content": sistema1 }, ...chgptdb, ]};
+const response = await fetch(url, {method: "POST", headers: headers, body: JSON.stringify(data)});
+const result = await response.json();
+const finalResponse = result.choices[0].message.content;
+return finalResponse;
+};
+let respuesta = await getOpenAIChatCompletion(text);
+if (respuesta == 'error' || respuesta == '' || !respuesta) return XD; // causar error undefined para usar otra api
+m.reply(`${respuesta}`.trim());
+} catch {
 try {
-let ia2 = await fetch(`https://aemt.me/openai?text=${text}`)
-let resu2 = await ia2.json()
-m.reply(resu2.result.trim())
-} catch (err) {
+const botIA222 = await openaiii.createCompletion({model: 'text-davinci-003', prompt: text, temperature: 0.3, max_tokens: 4097, stop: ['Ai:', 'Human:'], top_p: 1, frequency_penalty: 0.2, presence_penalty: 0});
+if (botIA222.data.choices[0].text == 'error' || botIA222.data.choices[0].text == '' || !botIA222.data.choices[0].text) return XD; // causar error undefined para usar otra api
+m.reply(botIA222.data.choices[0].text.trim());
+} catch {
 try {
-let tioress = await fetch(`https://api.lolhuman.xyz/api/openai-turbo?apikey=${lolkeysapi}&text=${text}`)
-let hasill = await tioress.json()
-conn.reply(m.chat, `${hasill.result}`, m, fake, )
-} catch (err) {
-console.error(err)
-conn.reply(m.chat, '🚩 *Error al procesar la solicitud*', m, fake, )
-}
-}}
+const syms1 = `Actuaras como un Bot de WhatsApp el cual fue creado por Alexis, tu seras CuriosityBug`
+const Empireapi1 = await fetch(`https://api.cafirexos.com/api/chatgpt?text=${text}&name=${m.name}&prompt=${syms1}`);
+const empireApijson1 = await Empireapi1.json();
+if (empireApijson1.resultado == 'error' || empireApijson1.resultado == '' || !empireApijson1.resultado) return XD; // causar error undefined para lanzar msg de error
+m.reply(`${empireApijson1.resultado}`.trim());
+} catch {
+}}}};
+handler.command = /^(openai|chatgpt|ia|openai2|chatgpt2|ia2|Gata)$/i;
+export default handler;
 
-}
-handler.help = ['ia']
-handler.tags = ['ai']
-handler.command = ['openai', 'chatgpt', 'ia', 'robot']
-
-export default handler
