ea144fc9da915eaee0591d2923ad73430671a2a9
ArizzVal <137434511+ArizzVal@users.noreply.github.com>
2024-02-05 20:11:06 -0600

    ©CuriosityBot-MD

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['524531287294', 'CuriosityBot-MD 🍧', true], ['573183650526'], ['5217294888993'], ['573155428601'], ['5214531287294'], ['51906662557'], ['5492266466080'], ['593968585383'], ['573026191480'], ['5492266613038'], ['573106040746'], ['5215610314499'], ['5217441298510'], ['5492964650915']]
+global.owner = [['524531287294', 'CuriosityBot-MD 🍧', true], ['573183650526'], ['5217294888993'], ['573155428601'], ['5214531287294'], ['51906662557'], ['5492266466080'], ['593968585383'], ['573026191480'], ['5492266613038'], ['573106040746'], ['5215610314499'], ['5713107133321'], ['5217441298510'], ['5492964650915']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
