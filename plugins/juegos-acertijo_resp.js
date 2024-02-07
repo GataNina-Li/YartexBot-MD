a9f8f3fbddec42571809ee5bf01fd101580ac26d
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-06 21:42:16 -0600

    © CuriosityBot-MD

--- plugins/juegos-acertijo_resp.js
+++ plugins/juegos-acertijo_resp.js
@@ -7,17 +7,17 @@ handler.before = async function (m) {
 let id = m.chat
 if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0
 this.tekateki = this.tekateki ? this.tekateki : {}
-if (!(id in this.tekateki)) return m.reply('*⚠️ ESE ACERTIJO YA HA TERMINADO!*')
+if (!(id in this.tekateki)) return conn.reply(m.chat, '🚩 *El acertijo ha terminado*', m, fake, )
 if (m.quoted.id == this.tekateki[id][0].id) {
 let json = JSON.parse(JSON.stringify(this.tekateki[id][1]))
 if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
 global.db.data.users[m.sender].exp += this.tekateki[id][2]
-m.reply(`*✅ RESPUESTA CORRECTA!*\n+${this.tekateki[id][2]} Exp`)
+conn.reply(m.chat, `✅ *Respuesta correcta*\n+${this.tekateki[id][2]} Exp`, m, fake, )
 m.react(done)
 clearTimeout(this.tekateki[id][3])
 delete this.tekateki[id]
 } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`*⚠️ CASI LO LOGRAS!*`)
-else m.reply('*⚠️ RESPUESTA INCORRECTA!*')}
+else conn.reply(m.chat, '🚩 *Respuesta incorrecta*', m, fake, )}
 m.react(error)
 return !0
 
