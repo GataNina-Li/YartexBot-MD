b58ca035460336d78cc19ce2bfb36f958379b629
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-07 17:46:59 -0600

    © CuriosityBot-MD

--- plugins/juegos-delttt.js
+++ plugins/juegos-delttt.js
@@ -1,9 +1,9 @@
 var handler = async (m, { conn, text }) => {
 
 let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
-if (room == undefined) return conn.reply(m.chat,`*⚠️ NO ESTAS EN UNA PARTIDA DE TICTACTOE*`, m)
+if (room == undefined) return conn.reply(m.chat,`🚩 *No estas registrado en una partida tic tac toe*`, m, fake, )
 delete conn.game[room.id]
-await conn.reply(m.chat, `*✅ SE ELIMINÓ LA SESIÓN DE TICTACTOE*`, m)
+await conn.reply(m.chat, `✅ *Se eliminó la sesión de tic tac toe*`, m, fake, )
 
 }
 handler.help = ['delttt', 'delttc', 'delxo']
