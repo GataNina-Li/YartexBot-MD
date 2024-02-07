48b493023dfd18ad2b2ee63655715f210bddceb4
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-02-07 08:46:31 -0300

    Add files via upload

---
+++ plugins/_prueba.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw `*[⚠️] ESCRIBE EL REPORTE*\n\n💡 EJEMPLO\n*${usedPrefix + command} el comando ${usedPrefix}infobot no funciona.*`
+let teks = `- PRUEBA - *\nUsuario\nWa.me/${m.sender.split`@`[0]}\n${text}`
+conn.reply('5217821153974@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {
+contextInfo: {
+mentionedJid: [m.sender]
+}})
+  m.reply(` ❕ `)
+
+}
+
+handler.help = ['reporte', 'request'].map(v => v + ' <teks>')
+handler.tags = ['owner']
+handler.exp = 100 
+handler.command = /^(prueba)$/i 
+export default handler
