23d628e73935bf575cb274a9279cbc7c51b2dc1d
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-08 15:49:43 -0600

    © CuriosityBot-MD

--- plugins/juegos-ppt.js
+++ plugins/juegos-ppt.js
@@ -1,6 +1,7 @@
 var handler = async (m, { conn, text, usedPrefix, command }) => {
 
 let poin = 500
+let poin2 = 20
 
 if (!text) return conn.reply(m.chat, `🎌 *Seleccione piedra/papel/tijera*\n\nEjemplo, !${command} papel`, m, fake, )
 
@@ -16,31 +17,46 @@ astro = 'papel'
 
 if (text == astro) {
 global.db.data.users[m.sender].exp += 100
-conn.reply(m.chat, `🚩 *Empate*\n\nTú: ${text}\nEl Bot: ${astro}\n\n🎁 Puntos (±)100 XP ✨`, m, fake, )
+global.db.data.users[m.sender].diamond += 5
+
+conn.reply(m.chat, `🚩 *Empate*\n\nTú: ${text}\nEl Bot: ${astro}\n\n(±)100 XP ✨\n(±)5 💎`, m, fake, )
 } else if (text == 'piedra') {
 if (astro == 'tijera') {
+  
 global.db.data.users[m.sender].exp += 300
-conn.reply(m.chat, `🎌 *Ganaste*\n\nTú: ${text}\nEl Bot: ${astro}\n\n🎁 Puntos +${poin} XP ✨`, m, fake, )
+global.db.data.users[m.sender].diamond += 10
+
+conn.reply(m.chat, `🎌 *Ganaste*\n\nTú: ${text}\nEl Bot: ${astro}\n\n+${poin} XP ✨\n+${poin2} 💎`, m, fake, )
 } else {
+global.db.data.users[m.sender].diamond -= 10
 global.db.data.users[m.sender].exp -= 300
-conn.reply(m.chat, `🚩 *Perdiste*\n\nTú: ${text}\nEl Bot: ${astro}\n\n Puntos -${poin} XP ✨`, m, fake, )
+
+conn.reply(m.chat, `🚩 *Perdiste*\n\nTú: ${text}\nEl Bot: ${astro}\n\n-${poin} XP ✨\n-${poin2} 💎`, m, fake, )
 }
 } else if (text == 'tijera') {
 if (astro == 'papel') {
+global.db.data.users[m.sender].diamond += 20
 global.db.data.users[m.sender].exp += 500
-conn.reply(m.chat, `🎌 *Ganaste*\n\nTú: ${text}\nEl Bot: ${astro}\n\n🎁 Puntos +${poin} XP ✨`, m, fake, )
+
+conn.reply(m.chat, `🎌 *Ganaste*\n\nTú: ${text}\nEl Bot: ${astro}\n\n+${poin} XP ✨\n+${poin2} 💎`, m, fake, )
 } else {
+global.db.data.users[m.sender].diamond -= 15
 global.db.data.users[m.sender].exp -= 150
-conn.reply(m.chat, `🚩 *Perdiste*\n\nTú: ${text}\nEl Bot: ${astro}\n\n Puntos -${poin} XP ✨`, m, fake, )
+
+conn.reply(m.chat, `🚩 *Perdiste*\n\nTú: ${text}\nEl Bot: ${astro}\n\n-${poin} XP ✨\n-${poin2} 💎`, m, fake, )
 }
 } else if (text == 'papel') {
 if (astro == 'piedra') {
+global.db.data.users[m.sender].diamond += 25
 global.db.data.users[m.sender].exp += 600
-conn.reply(m.chat, `🚩 *Perdiste*\n\nTú: ${text}\nEl Bot: ${astro}\n\n Puntos -${poin} XP ✨`, m, fake, )
+
+conn.reply(m.chat, `🎌 *Ganaste*\n\nTú: ${text}\nEl Bot: ${astro}\n\n+${poin} XP ✨\n+${poin2} 💎`, m, fake, )
 } else {
+global.db.data.users[m.sender].diamond -= 10
 global.db.data.users[m.sender].exp -= 300
-conn.reply(m.chat, `🚩 *Perdiste*\n\nTú: ${text}\nEl Bot: ${astro}\n\n Puntos -${poin} XP ✨`, m, fake, )
+conn.reply(m.chat, `🚩 *Perdiste*\n\nTú: ${text}\nEl Bot: ${astro}\n\n-${poin} XP ✨\n-${poin2} 💎`, m, fake, )
 }
+
 } else {
 return conn.reply(m.chat, `🎌 *Seleccione piedra/papel/tijera*\n\nEjemplo, !${command} papel`, m, fake, )
 }
