1e513a435efce2b708998d01ea4c291511b5b115
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-08 17:47:46 -0600

    © CuriosityBot-MD

--- plugins/azami.js
+++ plugins/azami.js
@@ -2,13 +2,13 @@
 Comando Actualizado Por AzamiJs*/
 
 let handler = (m) => m
-handler.all = async function (m) {
+handler.before = async function (m) {
 
 let chat = global.db.data.chats[m.chat]
 
 if (/^Azami|azami|Zam|zam$/i.test(m.text) && !chat.isBanned) { 
 
-var GB = `🚩 *Parece que se ha mencionado el nombre de mi creado (@${devnum})*`
+var GB = `🚩 *Parece que se ha mencionado el nombre de mi creador* ( @${devnum} )`
 
 m.reply(GB, m.chat, { mentions: conn.parseMention(GB)})
 
