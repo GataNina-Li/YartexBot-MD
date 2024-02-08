1286bd4ddd7b476c95888b2672235178d09a42d4
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-08 18:29:49 -0500

    Actualizar azami.js

--- plugins/azami.js
+++ plugins/azami.js
@@ -2,7 +2,7 @@ let handler = (m) => m;
 handler.all = async function (m) {
 
 
-  if (/^diego|Diego$/i.test(m.text)) {
+  if (/^Azami|azami|Zam|zam$/i.test(m.text)) {
     var GB = `𝙊𝙬𝙣𝙚𝙧 @${global.suittag} 𝙏𝙚 𝘼𝙣 𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙙𝙤 🎌`;
 
     m.reply(GB, m.chat, { mentions: conn.parseMention(GB)});
