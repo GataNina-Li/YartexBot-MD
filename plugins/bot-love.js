0811efc2361b2342cc46658ee5d1c2bbc1951613
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-07 19:21:08 -0500

    Actualizar bot-love.js

--- plugins/bot-love.js
+++ plugins/bot-love.js
@@ -3,12 +3,13 @@ let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.m
 let name = conn.getName(who)
 const pp = './storage/logos/Menu1.jpg'
 
-let love = `\t\t🚩 𝗨𝗡 𝗣𝗢𝗥𝗖𝗘𝗡𝗧𝗔𝗝𝗘 𝗗𝗘 𝗔𝗠𝗢𝗥
+let love = `🚩 𝗨𝗡 𝗣𝗢𝗥𝗖𝗘𝗡𝗧𝗔𝗝𝗘 𝗗𝗘 𝗔𝗠𝗢𝗥
 
 𝖤𝗅 𝖠𝗆𝗈𝗋 𝖣𝖾: 
 ${name}
 𝖲𝗂𝖾𝗇𝗍𝖾 𝖯𝗈𝗋 𝖳𝗂 𝖴𝗇: 
-*${Math.floor(Math.random() * 100)}%* 𝖣𝖾 𝖴𝗇 *100%%*
+*${Math.floor(Math.random() * 100)}%* 𝖣𝖾 𝖴𝗇 *100%*
+
 🎌 𝐇𝐚𝐲 𝐀𝐦𝐨𝐫,𝐃𝐞𝐛𝐞𝐬 𝐏𝐞𝐝𝐢𝐫𝐥𝐞 𝐌𝐚𝐭𝐫𝐢𝐦𝐨𝐧𝐢𝐨?`
 
 conn.sendMessage(m.chat,{image: {url: pp}, caption: love, mentions: [m.sender]}, {quoted: m}) 
