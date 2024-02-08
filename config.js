c5cf6338a6abad8c5ea84279c7f10717d085616d
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-07 23:05:02 -0600

    © CuriosityBot-MD

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['524531287294', 'CuriosityBot-MD 🍧', true], ['5217294888993'], ['5216566014078'], ['573155428601'], ['5214531287294'], ['51906662557'], ['5492266466080'], ['593968585383'], ['573026191480'], ['5492266613038'], ['573106040746'], ['5215610314499'], ['573107133321'], ['5217441298510'], ['5492964650915']]
+global.owner = [['524531287294', 'CuriosityBot-MD 🍧', true], ['5217294888993'], ['59162714296'], ['573155428601'], ['5214531287294'], ['51906662557'], ['5492266466080'], ['593968585383'], ['573026191480'], ['5492266613038'], ['573106040746'], ['5215610314499'], ['573107133321'], ['5217441298510'], ['5492964650915']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
@@ -25,8 +25,8 @@ global.library = 'Baileys'
 global.baileys = '@whiskeysockets/baileys'
 global.lenguaje = 'Español'
 global.menudi = ['⛶','❏','⫹⫺']
-global.dev = 'Azami ~ ¿Zam?'
-global.devnum = '+52 1 656 601 4078'
+global.dev = '¿Azami ~ Zam?'
+global.devnum = '+591 62714296'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => { unwatchFile(file)
