eebdc246ad201f2ae07ac84cc5654ffc9d97b2c6
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-09 14:14:19 -0600

    © CuriosityBot-MD

--- plugins/descargas-ytv.js
+++ plugins/descargas-ytv.js
@@ -1,42 +1,36 @@
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
-import fetch from 'node-fetch';
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
 
-const handler = async (m, { conn, args, command }) => {
-  if (!args[0]) {
-    return conn.reply(
-      m.chat,
-      `🎌 *Ingrese un enlace de YouTube*\n\nEjemplo: !${command} https://youtu.be/85xI8WFMIUY`,
-      m
-    );
-  }
-  m.react('⌛');
+var handler = async (m, { conn, args, command }) => {
 
-  try {
-    let qu = args[1] || '360';
-    let q = qu + 'p';
-    let v = args[0];
-    const yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v));
-    const dl_url = await yt.video[q].download();
-    const ttl = await yt.title;
-    const size = await yt.video[q].fileSizeH;
-    m.react('✅');
-    await conn.sendMessage(m.chat, {
-      video: { url: dl_url },
-      fileName: `${ttl}.mp4`,
-      mimetype: 'video/mp4',
-      caption: `*Título*\n${ttl}\n\n*Peso*\n${size}`,
-      thumbnail: await fetch(yt.thumbnail),
-    }, { quoted: m });
-  } catch {
-    await conn.reply(m.chat, `🚩 *Ocurrió un error*`, m);
-    m.react('❌');
-  }
-};
+if (!args[0]) {
+return conn.reply( m.chat, `🎌 *Ingrese un enlace de YouTube*\n\nEjemplo: !${command} https://youtu.be/85xI8WFMIUY`, m, fake, )
+}
+m.react('⏰')
 
-handler.help = ['ytv'];
-handler.tags = ['descargas'];
-handler.command = /^fgmp4|dlmp4|getvid|yt(v|mp4)?$/i;
-handler.register = true;
-handler.limit = true;
+try {
+let qu = args[1] || '360'
+let q = qu + 'p'
+let v = args[0]
+let yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v))
+let dl_url = await yt.video[q].download()
+let ttl = await yt.title
+let size = await yt.video[q].fileSizeH
+m.react('✅')
 
-export default handler;
+  
+await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `*Título*\n${ttl}\n\n*Peso*\n${size}`, thumbnail: await fetch(yt.thumbnail),}, { quoted: m })
+} catch {
+await conn.reply(m.chat, `🚩 *Ocurrió un error*`, m, fake, )
+m.react('❌')
+}
+
+}
+handler.help = ['ytv']
+handler.tags = ['descargas']
+handler.command = /^fgmp4|dlmp4|getvid|yt(v|mp4)?$/i
+
+handler.register = true
+handler.limit = true
+
+export default handler
