f0782655936892b68e8a82126c1b09a5dc029772
ArizzVal <137434511+ArizzVal@users.noreply.github.com>
2024-02-08 23:16:17 -0600

    ©CuriosityBot-MD

---
+++ plugins/botsi-Aruu.js
@@ -0,0 +1,19 @@
+/*Codigo Actualizado Y Editado Para El Bot CuriosityBot-MD
+Comando Actualizado Por AzamiJs*/
+
+let handler = (m) => m
+handler.before = async function (m) {
+
+let chat = global.db.data.chats[m.chat]
+
+if (/^kitsune|arumi|ari|aruu$/i.test(m.text) && !chat.isBanned) { 
+
+var GB = `📍 *Oh no, Te has equivocado has mencionado a un colaborador* ( @5215610314499 )`
+
+m.reply(GB, m.chat, { mentions: conn.parseMention(GB)})
+
+}
+return !0
+
+}
+export default handler
