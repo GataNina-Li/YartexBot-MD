e54bc0cf714ce0e074c78c04fd11a8dacfbe0f28
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-09 18:26:28 -0500

    Actualizar bot-autobio.js

--- plugins/bot-autobio.js
+++ plugins/bot-autobio.js
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `${wm} ║ ⏳ Uptime:  ${uptime}`
+let bio = `${wm} ║ ⏰️ Actividad: ${uptime}`
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 } 
