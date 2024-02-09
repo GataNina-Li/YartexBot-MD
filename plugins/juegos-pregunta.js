19d2aad1edc1cc3c456574e05e43cae6ed8bdd31
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-08 18:05:30 -0600

    © CuriosityBot-MD

--- plugins/juegos-pregunta.js
+++ plugins/juegos-pregunta.js
@@ -1,8 +1,12 @@
 var handler = async (m, { conn, text, usedPrefix, command }) => {
 
 if (!text) return conn.reply(m.chat, `*Ingrese un texto a preguntar*\n\nEjemplo, !${command} Hoy va llover?`
-m.react('❔'); m.react(❓); m.react(❔)
-
+m.react('❔')
+await delay(1000 * 1)
+m.react(❓)
+await delay(1000 * 1)
+m.react(❔)
+  
 conn.reply(m.chat, `> *Pregunta:* ${text}
 > *Respuesta:* ${['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible','Por que haces estas preguntas','Por eso te dejo','Para que quieres saber','No te dire la respuesta'].getRandom()}
 `, m, fake, )
@@ -11,5 +15,7 @@ conn.reply(m.chat, `> *Pregunta:* ${text}
 handler.help = ['pregunta']
 handler.tags = ['juegos']
 handler.command = /^pregunta|preguntas|apakah$/i
+
+handler.register = true
 
 export default handler
