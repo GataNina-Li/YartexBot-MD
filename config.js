8300b595a94292ede91ab776bc2fb28f5d0a4d55
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-02-09 11:52:03 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['524531287294', 'CuriosityBot-MD 🍧', true], ['5217294888993'], ['59162714296'], ['5214531287294'], ['5492964650915'], ['573013482814'], ['5215610314499']]
+global.owner = [['524531287294', 'CuriosityBot-MD 🍧', true], ['5492266466080', 'Owner 🍧', true], ['5217294888993'], ['59162714296'], ['5214531287294'], ['5492964650915'], ['573013482814'], ['5215610314499']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
