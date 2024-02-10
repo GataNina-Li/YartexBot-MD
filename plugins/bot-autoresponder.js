c5a740c9497a57cddd44d3112067ca58acc84795
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-10 13:06:21 -0600

    © CuriosityBot-MD

--- plugins/bot-autoresponder.js
+++ plugins/bot-autoresponder.js
@@ -1,12 +1,13 @@
-import { sticker } from '../lib/sticker.js'
-let handler = m => m
+var handler = m => m
 
 handler.all = async function (m, {conn}) {
 let chat = global.db.data.chats[m.chat]
 
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
-await this.sendMessage(m.chat, {text: `🎌 𝙀𝙣 𝙌𝙪𝙚 𝙏𝙚 𝙋𝙪𝙚𝙙𝙤 𝘼𝙮𝙪𝙙𝙖𝙧?`}, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})}
+await conn.reply(m.chat, '🎌 *Hola*', m, fake, )
+}
 
+return !0 
+}
 
-return !0 }
 export default handler
