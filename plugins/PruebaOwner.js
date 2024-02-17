9bc4101f30da1707ea575f693229534e99e9bba7
ArizzVal <137434511+ArizzVal@users.noreply.github.com>
2024-02-17 15:01:13 -0600

    Update PruebaOwner.js

--- plugins/PruebaOwner.js
+++ plugins/PruebaOwner.js
@@ -1,1 +1,24 @@
+//Lleva una llave "}" sino va a mandar error.
+var handler = async(m, { conn, isOwner }) => {
 
+if (m.fromMe) return
+if (isOwner) return conn.reply(m.chat, '🚧*Soy un texto de Prueba de Owner*', m, fake, )
+
+try {
+
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote')
+
+} catch {
+
+await conn.reply(m.chat, '🪛 *Paso un Error* ⚙️, m, fake, )}
+
+}
+handler.help = ['Esto es prueba']
+handler.tags = ['owner']
+handler.command = /^Prueba|Aprueba/i
+
+handler.rowner = true
+handler.group = true
+handler.botAdmin = true
+
+export default handler
