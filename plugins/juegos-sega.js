a96ec97955e150e88d73030885a29727cc99d2ce
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-19 23:13:48 -0600

    © CuriosityBot-MD

--- plugins/juegos-sega.js
+++ plugins/juegos-sega.js
@@ -1,17 +1,18 @@
-let handler = async (m, { conn, usedPrefix, text }) => {
-  let { key } = await conn.sendMessage(m.chat, { text: "Ahora te voy a hacer una paja " }, { quoted: m });
-  const array = [
-    "8==👊==D", "8===👊=D", "8=👊===D", "8=👊===D", "8==👊==D", "8===👊=D", "8=👊===D", "8==👊==D", "8===👊=D", "8=👊===D","8==👊==D","8===👊=D","8===👊=D💦"
-  ];
+var handler = async (m, { conn, usedPrefix, text }) => {
 
-  for (let item of array) {
-    await conn.sendMessage(m.chat, { text: `${item}`, edit: key }, { quoted: m });
-    await new Promise(resolve => setTimeout(resolve, 20)); // Delay 5 seconds
-  }
-  return conn.sendMessage(m.chat, { text: `Oh ${text} se corrió!😋💦`.trim() , edit: key, mentions: [m.sender] }, { quoted: m });
-};
+let { key } = await conn.sendMessage(m.chat, { text: 'Ahora te voy a hacer una paja' }, { quoted: m })
+const array = [
+'8==👊==D', '8===👊=D', '8=👊===D', '8=👊===D', '8==👊==D', '8===👊=D', '8=👊===D', '8==👊==D', '8===👊=D', '8=👊===D', '8==👊==D', '8===👊=D', '8===👊=D💦'
+]
 
-handler.help = ['fun', 'fap'];
+for (let item of array) {
+await conn.sendMessage(m.chat, { text: `${item}`, edit: key }, { quoted: m })
+await new Promise(resolve => setTimeout(resolve, 20)) // Delay 5 seconds
+}
+return conn.sendMessage(m.chat, { text: `Oh ${text} se corrió! 😋💦`.trim() , edit: key, mentions: [m.sender] }, { quoted: m })
+
+}
+handler.help = ['fap']
 handler.tags = ['juegos']
 handler.command = /^(sega|sierra|fap)$/i;
 
