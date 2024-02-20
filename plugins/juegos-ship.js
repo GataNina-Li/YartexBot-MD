b1fcf194b481cbaffde54b16e0834cd256bfbc17
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-19 23:17:49 -0600

    © CuriosityBot-MD

--- plugins/juegos-ship.js
+++ plugins/juegos-ship.js
@@ -1,10 +1,10 @@
 var handler = async (m, { conn, command, text }) => {
 
-if (!text) conn.reply(m.chat, `🎌 *Escribe el nombre de dos personas para calcular su amor*`, m, fake, )
+if (!text) return conn.reply(m.chat, `🎌 *Escribe el nombre de dos personas para calcular su amor*`, m, fake, )
 let [text1, ...text2] = text.split(' ')
 
 text2 = (text2 || []).join(' ')
-if (!text2) conn.reply(m.chat, `🚩 *Escribe el nombre de la segunda persona*`, m, fake, )
+if (!text2) return conn.reply(m.chat, `🚩 *Escribe el nombre de la segunda persona*`, m, fake, )
 let love = `❤️ *${text1}* tu oportunidad de enamorarte de *${text2}* es de *${Math.floor(Math.random() * 100)}%* 👩🏻‍❤️‍👨🏻`
 
 m.reply(love, null, { mentions: conn.parseMention(love) })
