d8a5e4341bb190947019c5c826ef40334ac969d8
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-19 23:53:36 -0600

    © CuriosityBot-MD

--- plugins/juegos-slot.js
+++ plugins/juegos-slot.js
@@ -2,22 +2,21 @@ let reg = 60
 
 var handler = async (m, { conn, args, usedPrefix, command }) => {
 
-let fa = `*⚠️ CUANTO QUIERES APOSTAR?*
+let fa = `🎌 *¿Cuánto quieres apostar?*
 
-❕ EJEMPLO:
-*${usedPrefix + command}* 100`.trim()
-if (!args[0]) throw fa
-if (isNaN(args[0])) throw fa
+Ejemplo, !${command} 100`
+if (!args[0]) return conn.reply(m.chat, fa, m, fake, )
+if (isNaN(args[0])) return conn.reply(m.chat, fa, m, fake, )
 let apuesta = parseInt(args[0])
 let users = global.db.data.users[m.sender]
 let time = users.lastslot + 10000
 if (new Date - users.lastslot < 10000) throw `*⏰ ESPERE ${msToTime(time - new Date())} PARA USAR DE NUEVO*`
-if (apuesta < 50) throw '*⚠️ LO MÍNIMO PARA APOSTAR ES 100 XP*'
+if (apuesta < 50) return conn.reply(m.chat, '🚩 *Lo mínimo para aportar es 100 XP*', m, fake, )
 if (users.exp < apuesta) {
-throw `*⚠️ NO TIENES SUFICIENTE XP*`
+return conn.reply(m.chat, '🚩 *No tienes suficiente XP*', m, fake, )
 }
 
-let emojis = ["🍩", "🍧", "🧃"]
+let emojis = ['🪐', '🌜', '🌟']
 let a = Math.floor(Math.random() * emojis.length)
 let b = Math.floor(Math.random() * emojis.length)
 let c = Math.floor(Math.random() * emojis.length)
@@ -41,13 +40,13 @@ if (c == emojis.length) c = 0
 }
 let end
 if (a == b && b == c) {
-end = `🎁 *GANASTE* 🎁\n*+${apuesta + apuesta + apuesta + apuesta + apuesta} XP*`
+end = `🎁 *Genial, Ganaste la apuesta*\n*+${apuesta + apuesta + apuesta + apuesta + apuesta} XP*`
 users.exp += apuesta + apuesta
 } else if (a == b || a == c || b == c) {
 end = `🌟 *Casi lo logras sigue intentando*\n*Te regalo ${reg} XP*`
 users.exp += reg
 } else {
-end = `🤯 *PERDISTE* 🤯\n*Te e quitado ${apuesta} XP*`
+end = `⚠️ *Perdiste la apuesta*\n*Te e quitado ${apuesta} XP*`
 users.exp -= apuesta
 }
 users.lastslot = new Date * 1
@@ -60,11 +59,13 @@ ${x[2]} : ${y[2]} : ${z[2]}
 ───────────
 🎰┃🎰┃ 🎰
        
-${end}`) 
+${end}`)
+
 }
 handler.help = ['slot']
 handler.tags = ['juegos']
 handler.command = ['slot']
+
 handler.register = true
 
 export default handler
@@ -75,9 +76,9 @@ seconds = Math.floor((duration / 1000) % 60),
 minutes = Math.floor((duration / (1000 * 60)) % 60),
 hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
 
-hours = (hours < 10) ? "0" + hours : hours
-minutes = (minutes < 10) ? "0" + minutes : minutes
-seconds = (seconds < 10) ? "0" + seconds : seconds
+hours = (hours < 10) ? '0' + hours : hours
+minutes = (minutes < 10) ? '0' + minutes : minutes
+seconds = (seconds < 10) ? '0' + seconds : seconds
 
-return seconds + " Segundo(s)"
+return seconds + ' Segundo(s)'
 }
