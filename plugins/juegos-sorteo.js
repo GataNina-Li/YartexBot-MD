e3e0af924db4fc6c7e42c3d42edc028b717f4f25
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-20 16:25:50 -0600

    © CuriosityBot-MD

--- plugins/juegos-sorteo.js
+++ plugins/juegos-sorteo.js
@@ -1,6 +1,3 @@
-import util from 'util'
-import path from 'path'
-
 async function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
 
 let user = a => '@' + a.split('@')[0]
@@ -9,19 +6,11 @@ let ps = groupMetadata.participants.map(v => v.id)
 let a = ps.getRandom()
 let k = Math.floor(Math.random() * 70)
 let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
-let top = `*😼 ${user(a)} Acaba de ganar el sorteo felicitaciones 🎉*`
+let top = `*${user(a)}* Acaba de ganar el sorteo, felicitaciones`
 let txt = ''
 let count = 0
-for (const c of top) {
-await new Promise(resolve => setTimeout(resolve, 15))
-txt += c
-count++
 
-if (count % 10 === 0) {
-conn.sendPresenceUpdate('composing' , m.chat);
-}
-}
-await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100} )
+await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m} )
 
 }
 handler.help = ['sorteo']
