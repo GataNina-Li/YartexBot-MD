9cbad889489ab2d6c60a76ce021a6ea0bb96cc04
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-21 19:43:33 -0600

    © CuriosityBot-MD

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,13 +1,20 @@
-let handler = async (m, { conn, participants, usedPrefix, command }) => {
-let BANtext = `*⚠️ ETIQUETE A UNA PERSONA*\n\n💡 EJEMPLO\n*${usedPrefix + command} @${global.suittag}*`
+var handler = async (m, { conn, participants, usedPrefix, command }) => {
+
+let BANtext = `🎌 *Etiquete a una persona*\n\nEjemplo, !${command} @${global.suittag}`
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(BANtext, m.chat, { mentions: conn.parseMention(BANtext)})
 let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 else who = m.chat
 let users = global.db.data.users
+
 users[who].banned = true
-m.reply('*⏤͟͟͞͞🍓ESTE USUARIO FUE BANEADO CON ÉXITO*\n\n*[🛑] YA NO PUEDE USAR CURIOSITY BOT-MD*')    }
-handler.tags = ['owner']
+m.reply('✅ *Usuario baneado con éxito*')
+
+}
+handler.tags = ['own']
+handler.commnad = ['banuser']
 handler.command = /^banuser$/i
+
 handler.rowner = true
+
 export default handler
