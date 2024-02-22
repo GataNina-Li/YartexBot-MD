dda6500eb3abdc66903dff034088f837650a508a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-22 10:55:48 -0500

    Actualizar info-creditos.js

--- plugins/info-creditos.js
+++ plugins/info-creditos.js
@@ -20,7 +20,6 @@ https://github.com/AzamiJs
 ⬡ *Wilson*
 ⬡ *Cpw*
 ⬡ *Diego*
-⬡ *Piter*
 
 _Envie "colaboradores" para obtener el enlace del perfil de GitHub de los colaboradores_`
 
@@ -46,7 +45,6 @@ let texto = `🚩 *GitHub - Colaboradores*
 ⬡ https://github.com/WilsonWaoz
 ⬡ https://github.com/WorkCwp
 ⬡ https://github.com/diegojadibot
-⬡ https://github.com/Principiante12344
 
 _Envie "cc" Para Obtener El Contacto De Los Colaboradores_`
 
@@ -96,10 +94,7 @@ let contacto = `🎌 *Contacto - Colaboradores*
 @573003025991
 
 ⬡ *Diego*
-@573013482814
-
-⬡ *Piter*
-@51902247905`
+@573013482814`
 
 m.reply(contacto, m.chat, { mentions: conn.parseMention(contacto)})}
 }
