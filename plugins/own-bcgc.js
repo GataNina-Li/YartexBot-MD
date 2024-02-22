2d657bcaeee15b31ed0b4652eb137fe949ce32d9
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-22 14:34:14 -0600

    © CuriosityBot-MD

--- plugins/own-bcgc.js
+++ plugins/own-bcgc.js
@@ -8,7 +8,7 @@ if(!pesan) return conn.reply(m.chat, '🚩 *Ingresé el texto que quiere que tr�
 
 for (let i of anu) {
 await delay(9000 * 1)
-conn.relayMessage(i, { liveLocationMessage: { degreesLatitude: 35.685506276233525, degreesLongitude: 139.75270667105852, accuracyInMeters: 0, degreesClockwiseFromMagneticNorth: 2, caption: '『 *BROADCAST* 』\n\n' + pesan + '\n\n*💌 Esto Es Un Comunicado Oficial*', sequenceNumber: 2, timeOffset: 3, contextInfo: m, }}, {}).catch(_ => _)
+conn.relayMessage(i, { liveLocationMessage: { degreesLatitude: 35.685506276233525, degreesLongitude: 139.75270667105852, accuracyInMeters: 0, degreesClockwiseFromMagneticNorth: 2, caption: '🚩 *Broadcast*\n\n' + pesan + ' ', sequenceNumber: 2, timeOffset: 3, contextInfo: m, }}, {}).catch(_ => _)
 }
 conn.reply(m.chat, `📑 *Mensaje enviado a ${anu.length} grupo/s*`, m, fake, )
 
