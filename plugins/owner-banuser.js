5d92d671185bc914b1a1320b23bdfc443d413a2f
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-21 19:59:23 -0600

    © CuriosityBot-MD

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,24 +1,3 @@
-/*var handler = async (m, { conn, participants, usedPrefix, command }) => {
-
-let BANtext = `🎌 *Etiquete a una persona*\n\nEjemplo, !${command} @${global.suittag}`
-if (!m.mentionedJid[0] && !m.quoted) return m.reply(BANtext, m.chat, { mentions: conn.parseMention(BANtext)})
-let who
-if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-else who = m.chat
-let users = global.db.data.users
-
-users[who].banned = true
-m.reply('✅ *Usuario baneado con éxito*')
-
-}
-handler.tags = ['own']
-handler.commnad = ['banuser']
-handler.command = /^banuser$/i
-
-handler.rowner = true
-
-export default handler*/
-
 var handler = async (m, { conn, text, usedPrefix, command}) => {
 
 let user, number, bot, bant, ownerNumber, aa, users, usr, q, mime, img
@@ -46,7 +25,7 @@ user = number + '@s.whatsapp.net'
 }} catch (e) {
 } finally {
 number = user.split('@')[0]
-if(user === conn.user.jid) return conn.reply(m.chat, lenguajeGB.smsPropban2(bot), null, { mentions: [user] })   
+if(user === conn.user.jid) return conn.reply(m.chat, `🚩 @${bot} *No puede ser baneado con este comando*`, null, { mentions: [user] })   
 for (let i = 0; i < global.owner.length; i++) {
 ownerNumber = global.owner[i][0];
 if (user.replace(/@s\.whatsapp\.net$/, '') === ownerNumber) {
@@ -63,7 +42,13 @@ await conn.reply(m.chat, '✅ *Usuario baneado con éxito*', null, { mentions: [
 }} catch (e) {
 await conn.reply(m.chat, '🚩 *Ocurrió un fallo*', null, m, fake, )
 console.log(e) 
-}}
+}
+
+}
+handler.tags = ['own']
+handler.commnad = ['banuser']
 handler.command = /^banuser$/i
+
 handler.rowner = true
+
 export default handler
