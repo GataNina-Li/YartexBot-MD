c61817c913214d82591210d7278921f743cc4e0e
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-22 19:07:58 -0600

    © CuriosityBot-MD

--- plugins/owner-block-unblock.js
+++ plugins/owner-block-unblock.js
@@ -4,13 +4,14 @@ let why = `🚩 *Ejemplo de uso*\n!${command} @${m.sender.split('@')[0]}`
 let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false
 if (!who) return conn.reply(m.chat, why, m, { mentions: [m.sender] })
 let res = []
+if (!((command === 'unblock') && (isOwner || isROwner))) {
 for (let i = 0; i < global.owner.length; i++) {
 let ownerNumber = global.owner[i][0]
 if (who.replace(/@s\.whatsapp\.net$/, '') === ownerNumber) {
 let aa = ownerNumber + '@s.whatsapp.net'
 await conn.reply(m.chat, `🚩 *No puedo realizar esta función con el propietario @${ownerNumber} de ${cb}*`, m, { mentions: [aa] })
 return
-}}
+}}}
 switch (command) {
 
 case 'blok': case 'block':
