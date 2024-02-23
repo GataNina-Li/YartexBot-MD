714acad574d6f5ace941fe83f24d5b1157777d53
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-23 12:35:29 -0600

    © CuriosityBot-MD

--- plugins/own-cleartmp.js
+++ plugins/own-cleartmp.js
@@ -1,21 +1,27 @@
+///////////////////////////////////////////
+// ¡NO USAR ESTE COMANDO EN HEROKU! //////
+/////////////////////////////////////////
+
 import { tmpdir } from 'os'
 import path, { join } from 'path'
 import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch } from 'fs'
 
 var handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => { 
 
-conn.reply(m.chat, `*⚠️ ARCHIVOS DE LA CARPETA TMP FUERON ELIMINADOS*`, m)
+conn.reply(m.chat, `✅ *Archivos de la carpeta tmp fueron eliminados*`, m, fake, )
 
 const tmp = [tmpdir(), join(__dirname, '../tmp')]
 const filename = []
 tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
 return filename.map(file => {
 const stats = statSync(file)
-unlinkSync(file)
-})} //NO USAR ESTE COMANDO EN HEROKU | DO NOT USE THIS COMMAND ON HEROKU
+unlinkSync(file) })
+
+}
 handler.help = ['cleartmp']
-handler.tags = ['owner']
+handler.tags = ['own']
 handler.command = /^(cleartmp|cleartemp|borrartmp|eliminartmp|borrartemp|borrartemp)$/i
-handler.exp = 500
+
 handler.rowner = true
+
 export default handler
