68e52a6acb169d132db9d34b9f2f5e2a8e43ef91
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-23 00:30:57 -0600

    © CuriosityBot-MD

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -1,25 +1,33 @@
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+import db from '../lib/database.js'
 
-let handler = async (m, { conn, text, isMods, isOwner }) => {
-let link = (m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text
-let [_, code] = link.match(linkRegex) || []
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
-if (!code) throw ` *Ingrese el enlace de un grupo.*\n*Enter the link of a group.*\n\n*𝙀𝙅𝙀𝙈𝙋𝙇𝙊 | 𝙀𝙓𝘼𝙈𝙋𝙇𝙀*\n*#unete ${nn}*\n\n*#join ${nnn}*`
+let handler = async (m, { conn, text, isOwner, args }) => {
 
-if ( isMods || isOwner || m.fromMe) {
-m.reply(` 𝙎𝙀 𝙃𝘼 𝙐𝙉𝙄𝘿𝙊 𝘼𝙇 𝙂𝙍𝙐𝙋𝙊!!✅*\n*𝙎𝙐𝘾𝘾𝙀𝙎𝙎𝙁𝙐𝙇𝙇𝙔 𝙅𝙊𝙄𝙉𝙀𝘿 ✅*`)
-await delay(5 * 5000)
+let users = '500'
+if (!args[0]) return conn.reply(m.chat, `🎌 *Ingrese el enlace de un grupo*\n\nEjemplo, !unete ${nn}`, m, fake, )
+let [_, code, expired] = text.match(linkRegex) || []
+if (!code) return conn.reply(m.chat, '🚩 Error, El enlace no funciona o es inválido', m, fake, )
+let gpData = await conn.groupGetInviteInfo(code).catch(e => {})
+try {
+if (gpData.size <= users) return m.reply(m.chat, `🚩 *La cantidad mínima para unirme al grupo debe ser de ${users} participantes*`, m, fake, )
 let res = await conn.groupAcceptInvite(code)
-} else {
-const data = global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)
+expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
+conn.reply(m.chat, `${cb} se unió al grupo ${res} con éxito \n${expired ? `Durante ${expired} Hora(s)` : ''}`, m, fake, )
+let chats = db.data.chats[res]
+if (!chats) chats = db.data.chats[res] = {}
+if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60
+} catch (e) {
+m.reply(m.chat, '🚩 *Ocurrió un error*', m, fake, )
+}
 
-await delay(1 * 1000)
-for (let jid of data.map(([id]) => [id] + '@s.whatsapp.net').filter(v => v != conn.user.jid)) m.reply(`╭══•───────────────•══╮\n┃ 📧 *𝙎𝙊𝙇𝙄𝘾𝙄𝙏𝙐𝘿 𝙋𝘼𝙍𝘼 𝙐𝙉 𝙂𝙍𝙐𝙋𝙊*\n┃ 📧 *𝙂𝙍𝙊𝙐𝙋 𝙍𝙀𝙌𝙐𝙀𝙎𝙏*\n╰══•───────────────•══╯\n\n*👤 𝙎𝙊𝙇𝙄𝘾𝙄𝙏𝘼𝙉𝙏𝙀 | 𝘼𝙋𝙋𝙇𝙄𝘾𝘼𝙉𝙏*\n` + ' wa.me/' + m.sender.split('@')[0] + '\n\n*🔮 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊 | 𝙂𝙍𝙊𝙐𝙋 𝙇𝙄𝙉𝙆*\n ' + link, jid)
+}
+handler.help = ['unete']
+handler.tags = ['bot']
+handler.command = /^(unete|entrabot|join)$/i
 
-m.reply(`*✅ Su enlace se envió a Mí Propietario(a).*\n*Your link was sent to My Owner.*\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n⚠️ *Su Grupo será Evaluado y Quedará a decisión de Mí Propietario(a) si  se une o no al Grupo.*\n*Your Group will be Evaluated and it will be up to My Owner if joins the Group or not.*\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n❕ *Es posible que su Solicitud sea Rechazada por las siguientes Causas:*\n*Your Application may be Rejected for the following Reasons:*\n*1️⃣ El Bot está Saturado.*\n*The Bot is Saturated.*\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n2️⃣ *El Bot fue eliminado del Grupo.*\n*The Bot was removed from the Group.*\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n3️⃣ *El Grupo no cumple con las Normativas de\n*The Group does not comply with the Regulations of\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n4️⃣ *El enlace del grupo se restableció.*\n*The group link was restored.*\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n5️⃣ *No se agrega a Grupos según Mi Propietario(a).*\n*Not added to Groups by My Owner*\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n💌 *La solicitud puede tardar Horas en ser Respondida. Por favor Tener Paciencia. Gracias*\n*The request may take hours to be answered. Please be patient. Thank you*`)}}
+handler.limit = 10
 
-handler.help = ['join [chat.whatsapp.com]']
-handler.tags = ['owner']
-handler.command = /^unete|join|nuevogrupo|unir|unite|unirse|entra|entrar$/i 
 export default handler
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const isNumber = (x) => (x = parseInt(x), typeof x === 'number' && !isNaN(x))
