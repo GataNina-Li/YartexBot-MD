e094dcde620c5e4c7a9360a13b147964ec4d27cd
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-23 12:39:51 -0600

    © CuriosityBot-MD

--- plugins/own-restart.js
+++ plugins/own-restart.js
@@ -4,13 +4,13 @@ var handler = async (m, { conn, isROwner, text }) => {
 
 if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
 if (conn.user.jid == conn.user.jid) {
-await conn.reply(m.chat, '', m, fake, )
+await conn.reply(m.chat, '☁️ *Reiniciando*', m, fake, )
 process.send('reset')
 } else throw 'eh'
 
 }
 handler.help = ['restart']
-handler.tags = ['owner']
+handler.tags = ['own']
 handler.command = ['restart','reiniciar'] 
 
 handler.rowner = true
