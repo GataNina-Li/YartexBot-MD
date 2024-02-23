32484e57b4a4aec7f9a30a3389c193c29503fc00
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-23 12:37:32 -0600

    Update propietario(a)-restart.js

--- plugins/propietario(a)-restart.js
+++ plugins/propietario(a)-restart.js
@@ -1,12 +1,14 @@
 import { spawn } from 'child_process'
-let handler = async (m, { conn, isROwner, text }) => {
-    if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
-    if (conn.user.jid == conn.user.jid) {
-    await m.reply('🔄 ＲＥＩＮＩＣＩＡＤＯ...')
-    process.send('reset')
-  } else throw 'eh'
-}
 
+var handler = async (m, { conn, isROwner, text }) => {
+
+if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+if (conn.user.jid == conn.user.jid) {
+await conn.reply(m.chat, '', m, fake, )
+process.send('reset')
+} else throw 'eh'
+
+}
 handler.help = ['restart']
 handler.tags = ['owner']
 handler.command = ['restart','reiniciar'] 
