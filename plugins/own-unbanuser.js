31e9ff5ece5ce764a718b0155e77f9af7a8a576c
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-23 11:18:07 -0600

    Update own-unbanuser.js

--- plugins/own-unbanuser.js
+++ plugins/own-unbanuser.js
@@ -1,18 +1,16 @@
 var handler = async (m, {conn, text}) => {
 
-let user, bant
-
-user = conn.user.jid.split`@`[0] + '@s.whatsapp.net'
-bant = `🎌 *Etiquete a una persona*\n\nEjemplo, !${command} @${global.suittag}`
+let user = conn.user.jid.split`@`[0] + '@s.whatsapp.net'
+let bant = `🎌 *Etiquete a una persona*\n\nEjemplo, !${command} @${global.suittag}`
 if (!text) return conn.reply(m.chat, bant, m, { mentions: [user] })
 let who
 if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
-if (!who) throw "*⚠️ INGRESA EL @tag DE ALGUN USUARIO*"
+if (!who) return conn.reply(m.chat, bant, m, { mentions: [user] })
 let users = global.db.data.users
 
 users[who].banned = false
-conn.reply(m.chat, `*Se desbaneo al usuario ${text} con éxito*`, m, { mentions: [text] })
+conn.reply(m.chat, `✅ *Se desbaneo al usuario ${text} con éxito*`, m, { mentions: [user] })
 
 }
 handler.help = ['unbanuser']
