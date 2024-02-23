af8c4a799db804651048f765c25991c3d370ab29
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-23 12:56:03 -0600

    © CuriosityBot-MD

--- plugins/imagenes-memes.js
+++ plugins/imagenes-memes.js
@@ -1,17 +1,21 @@
 /*
      ig : https://www.instagram.com/fg98._/
 */
+
 import hispamemes from 'hispamemes'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
 	
-	const meme = hispamemes.meme()
-    conn.sendFile(m.chat, meme, '', '', m)
-    m.react('🤣') 
+const meme = hispamemes.meme()
+conn.sendFile(m.chat, meme, '', '', m)
+m.react('🤣')
+
 }
 handler.help = ['meme']
-handler.tags = ['anime']
-handler.command = ['meme', 'memes'] 
+handler.tags = ['imagenes']
+handler.command = ['meme', 'memes']
+
 handler.diamond = true
 handler.register = true
+
 export default handler
