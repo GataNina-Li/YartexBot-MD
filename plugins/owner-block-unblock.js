893a525b0302b3a7160f41c14d9ade0b43d221cb
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-22 19:00:24 -0600

    Update owner-block-unblock.js

--- plugins/owner-block-unblock.js
+++ plugins/owner-block-unblock.js
@@ -1,17 +1,17 @@
 var handler = async (m, { text, conn, usedPrefix, command }) => {
 
 let why = `🚩 *Ejemplo de uso*\n!${command} @${m.sender.split('@')[0]}`
-let user = conn.user.jid.split`@`[0] + '@s.whatsapp.net'
 let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false
 if (!who) return conn.reply(m.chat, why, m, { mentions: [m.sender] })
 let res = []
-switch (command) {
 for (let i = 0; i < global.owner.length; i++) {
 let ownerNumber = global.owner[i][0]
-if (user.replace(/@s\.whatsapp\.net$/, '') === ownerNumber) {
+if (who.replace(/@s\.whatsapp\.net$/, '') === ownerNumber) {
 let aa = ownerNumber + '@s.whatsapp.net'
 await conn.reply(m.chat, `🚩 *No puedo banear al propietario @${ownerNumber} de ${cb}*`, m, { mentions: [aa] })
 return
+}}
+switch (command) {
 }}
 case 'blok': case 'block':
 if (who) await conn.updateBlockStatus(who, 'block').then(() => { res.push(who); })
