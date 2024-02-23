2fb30bfd95e456fe50a039af5c5e404db1a496ad
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-22 23:12:55 -0600

    © CuriosityBot-MD

--- plugins/owner-exec2.js
+++ plugins/owner-exec2.js
@@ -1,23 +1,28 @@
 import cp, { exec as _exec } from 'child_process'
 import { promisify } from 'util'
+
 let exec = promisify(_exec).bind(cp)
-let handler = async (m, { conn, isOwner, command, text }) => {
-  if (conn.user.jid != conn.user.jid) return
-  await m.reply(wait)
-  let o
-  try {
-    o = await exec(command.trimStart()  + ' ' + text.trimEnd())
-  } catch (e) {
-    o = e
-  } finally {
-    let { stdout, stderr } = o
-    if (stdout.trim()) m.reply(stdout)
-    if (stderr.trim()) m.reply(stderr)
-  }
+
+var handler = async (m, { conn, isOwner, command, text }) => {
+
+if (conn.user.jid != conn.user.jid) return
+
+let o
+try {
+o = await exec(command.trimStart()  + ' ' + text.trimEnd())
+} catch (e) {
+o = e
+} finally {
+let { stdout, stderr } = o
+if (stderr.trim()) conn.reply(m.chat, stderr, m, fake, )
+}
+  
 }
 handler.help = ['$']
-handler.tags = ['owner']
+handler.tags = ['own']
 handler.customPrefix = /^[$] /
 handler.command = new RegExp
+
 handler.rowner = true
+
 export default handler
