47a8124df1b9e42970947e5fefe0b33c85e18031
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-23 11:28:43 -0600

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,9 +1,14 @@
-let handler = async (m, { conn, text, command }) => {
+var handler = async (m, { conn, text, command }) => {
+
 let id = text ? text : m.chat  
-await conn.reply(id, '*Adios a todos, el Bot se despide! (≧ω≦)ゞ*') 
+await conn.reply(id, '**', m, fake, ) 
 await conn.groupLeave(id)}
-handler.tags = ['owner']
+
+handler.help = ['out', 'leavegc', 'salir']
+handler.tags = ['own']
 handler.command = /^(out|leavegc|leave|salirdelgrupo)$/i
+
 handler.group = true
 handler.rowner = true
+
 export default handler
