1a37ecf9583c24e0057dec94ca68608f0c38c839
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-26 20:42:24 -0600

    © CuriosityBot-MD

--- plugins/rpg-balance.js
+++ plugins/rpg-balance.js
@@ -5,18 +5,14 @@ if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
 else who = m.sender
 let name = conn.getName(who)
 
-conn.reply(m.chat, `╭─────── *Balance* ───────╮
-├🛡️ *Nombre:* ${name}
-├💎 *Diamante:* ${global.db.data.users[who].diamond}
-├✨ *Exp:* ${global.db.data.users[who].exp}
-╰───────────🎰───────────╯
+conn.reply(m.chat, `🌱 *Balance*
+*Nombre:* ${name}
+*Diamantes:* ${global.db.data.users[who].diamond}
 
-╭─── *Nota* ───╮
-├💵 *Puedes comprar más diamantes*
-├📨 *Utilizando los comandos:* 👇
-├💲 *${usedPrefix}buy <cantidad>*
-├☑️ *${usedPrefix}buyall*
-╰─────────────`, m, fake, )
+📄 *Nota*
+Puedes comprar más diamantes tilizando los comandos:* 👇
+${usedPrefix}buy <cantidad>
+${usedPrefix}buyall`, m, fake, )
 
 }
 handler.help = ['bal']
