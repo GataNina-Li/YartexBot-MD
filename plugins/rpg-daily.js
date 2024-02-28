c785cb6ce06e473c46015b5d9af46191a6a267ee
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-28 00:47:35 -0600

    © CuriosityBot-MD

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -3,7 +3,7 @@ const prem = 15
 
 var handler = async (m, {conn, isPrems }) => {
 
-let coin = `${pickRandom([5, 6, 7, 9, 10, 15, 20, 25, 30])}` * 1
+let coin = `${pickRandom([5, 6, 7, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 99, 100, 110, 120, 130, 600, 1000, 1500])}` * 1
 let exp = `${pickRandom([500, 600, 700, 800, 900, 999, 1000, 1300, 1500, 1800])}` * 1
 let exppremium = `${pickRandom([1000, 1500, 1800, 2100, 2500, 2900, 3300, 3600, 4000, 4500])}` * 1
 let d = Math.floor(Math.random() * 30)
