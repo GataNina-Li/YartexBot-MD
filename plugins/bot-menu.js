88d190ef4e5e76eb7aa84991542568cd9053d0be
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-27 21:49:07 -0600

    Update bot-menu.js

--- plugins/bot-menu.js
+++ plugins/bot-menu.js
@@ -6,6 +6,7 @@ import { xpRange } from '../lib/levelling.js'
 const { levelling } = '../lib/levelling.js'
 import PhoneNumber from 'awesome-phonenumber'
 import { promises } from 'fs'
+import moment from 'moment-timezone'
 import { join } from 'path'
 const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
 
@@ -19,6 +20,29 @@ let pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra
 let fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': imagen1, thumbnail: imagen1 ,sendEphemeral: true}}}
 let links = linkSity.getRandom()
 
+let fechaMoment, formatDate, nombreLugar, ciudad = null
+const phoneNumber = '+' + m.sender
+const parsedPhoneNumber = parsePhoneNumber(phoneNumber)
+const countryCode = parsedPhoneNumber.country
+const countryData = ct.getCountry(countryCode)
+const timezones = countryData.timezones
+const zonaHoraria = timezones.length > 0 ? timezones[0] : 'UTC'
+moment.locale(mid.idioma_code)
+let lugarMoment = moment().tz(zonaHoraria)
+if (lugarMoment) {
+fechaMoment = lugarMoment.format('llll [(]a[)]')
+formatDate = fechaMoment.charAt(0).toUpperCase() + fechaMoment.slice(1) 
+nombreLugar = countryData.name
+const partes = zonaHoraria.split('/')
+ciudad = partes[partes.length - 1].replace(/_/g, ' ')
+}else{
+lugarMoment = moment().tz('America/Lima')
+fechaMoment = lugarMoment.format('llll [(]a[)]')
+formatDate = fechaMoment.charAt(0).toUpperCase() + fechaMoment.slice(1) 
+nombreLugar = 'America'
+ciudad = 'Lima'
+}	
+  
 //FAKES
 let a = {'key': {'participants': '0@s.whatsapp.net', 'fromMe': false, 'id': '3B64558B07848BD81108C1D14712018E'}, 'message': {'locationMessage': {'name': `${name}`, 'jpegThumbnail': await (await fetch(pp)).buffer(), 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'}
 const ftrol = { key : { remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2023, status: 1, surface : 1, message: `${name}!`, orderTitle: `▮Menu ▸`, sellerJid: '0@s.whatsapp.net' }}}
