ed0fe73935ff712acb061a5cdfadf3d2dee19b0a
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-27 21:55:30 -0600

    © CuriosityBot-MD

--- plugins/bot-menu.js
+++ plugins/bot-menu.js
@@ -28,7 +28,7 @@ const countryCode = parsedPhoneNumber.country
 const countryData = ct.getCountry(countryCode)
 const timezones = countryData.timezones
 const zonaHoraria = timezones.length > 0 ? timezones[0] : 'UTC'
-moment.locale('es-mx')
+moment.locale('es-MX')
 let lugarMoment = moment().tz(zonaHoraria)
 if (lugarMoment) {
 fechaMoment = lugarMoment.format('llll [(]a[)]')
