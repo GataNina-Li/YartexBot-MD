322da36bca12875acf2ad509e3cd730364f891c4
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-27 21:08:39 -0600

    Update rpg-levelup.js

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -15,7 +15,7 @@ let name = conn.getName(m.sender)
 let whoPP = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let ppBot = await conn.profilePictureUrl(whoPP, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
 
-let image = await new can.Rank().setAvatar(ppBot).setUsername(name ? name.replaceAll('\n','') : '-').setBg('https://i.ibb.co/1s8T3sY/48f7ce63c7aa.jpg').setNeedxp(wm).setCurrxp(`${user.exp}`).setLevel(`${user.level}`).setRank('https://i.ibb.co/Wn9cvnv/FABLED.png').toAttachment()
+let image = await new can.Rank().setAvatar(ppBot).setUsername(name ? name.replaceAll('\n','') : '-').setBg('https://telegra.ph/file/8baf1e9e430c196b832c7.jpg').setNeedxp(wm).setCurrxp(`${user.exp}`).setLevel(`${user.level}`).setRank('https://i.ibb.co/Wn9cvnv/FABLED.png').toAttachment()
 		let data = image.toBuffer()
 			await conn.sendMessage(m.chat, { image: data, caption: wm }, { quoted: m })
 /*let { role } = global.db.data.users[m.sender]
