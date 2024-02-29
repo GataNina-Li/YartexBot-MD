632ab32968aa643118dd30b009ef72ca63709bfe
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-02-28 20:21:41 -0600

    Update rpg-reparar.js

--- plugins/rpg-reparar.js
+++ plugins/rpg-reparar.js
@@ -1,33 +1,31 @@
-
-
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
-  let type = (args[0] || '').toLowerCase()
-  let _type = (args[0] || '').toLowerCase()
-  let user = global.db.data.users[m.sender]
-  global.db.data.users[m.sender].pickaxe = global.db.data.users[m.sender].pickaxe || 0
-  global.db.data.users[m.sender].pedang = global.db.data.users[m.sender].pedang || 0
-  global.db.data.users[m.sender].fishingrod = global.db.data.users[m.sender].fishingrod || 0
-  let botol = global.botwm
 
-let lgocraft = `
-*「 R E P A I R」*`
+let type = (args[0] || '').toLowerCase()
+let _type = (args[0] || '').toLowerCase()
+let user = global.db.data.users[m.sender]
+global.db.data.users[m.sender].pickaxe = global.db.data.users[m.sender].pickaxe || 0
+global.db.data.users[m.sender].pedang = global.db.data.users[m.sender].pedang || 0
+//global.db.data.users[m.sender].fishingrod = global.db.data.users[m.sender].fishingrod || 0
 
-let caption = `
-▧ Pico ⛏️`
+let caption = `📍 Lista de herramientas: 
+
+- pico
+
+> Ejemplo, !repair pickaxe`
 
 try {
 if (/repair/i.test(command)) {
 const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
 switch (type) {
 case 'pickaxe':
-if (user.pickaxedurability > 99) return m.reply('Tools ini belum memiliki kerusakan')
-if (user.pickaxe == 0) return m.reply('Kamu belum memilik ini')
-if (user.diamond < 1 || user.stone < 3 || user.iron < 3 ) return m.reply(`Barang tidak cukup!`)
+if (user.pickaxedurability > 99) return conn.reply(m.chat, '✨ *Esta herramienta aún no tiene daños*', m, fake, )
+if (user.pickaxe == 0) return conn.reply(m.chat, '🚩 *Aún no tienes esta herramienta*', m, fake, )
+if (user.diamond < 1 || user.stone < 3 || user.iron < 3 ) return conn.reply(m.chat, `🚩 *No tienes suficientes recursos!*`, m, fake, )
 user.stone -= 3
 user.iron -= 3
 user.diamond -= 1
 user.pickaxedurability = 100
-m.reply("Sukses memperbaiki!")
+conn.reply(m.chat, '✨ *Herramienta reparada*', m, fake, )
 break
 default: 
 return await conn.reply(m.chat, caption, m, fake, )
@@ -45,11 +43,11 @@ return conn.reply(m.chat, caption, m, fake, )
 }
 }
 } catch (err) {
-m.reply("Error\n\n\n" + err.stack)}
+conn.reply(m.chat, '🚩 *Ocurrió un fallo*\n\n\n' + err.stack, m, fake, )}
  
 }
 handler.help = ['repair']
-handler.tags = ['rpg']
+handler.tags = ['rg']
 handler.command = /^(repair)/i
 
 export default handler
