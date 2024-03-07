8b72a4e7765e7081c73275cbe4a78bcea76fbe21
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-03-06 19:58:12 -0600

    Update rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -4,13 +4,13 @@ let handler = async function (m, { args, command }) {
 if (command == 'unregister' || command == 'unreg') {
 if (!args[0]) throw `*⚠️ INGRESE SU NÚMERO DR SÉRIE, SI NO SABE CUAL ES USE #myns*`
 let user = global.db.data.users[m.sender]
-let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
+var sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
 if (args[0] !== sn) throw `*⚠️ VERIFIQUE QUE SU NÚMERO DE SÉRIE SEA CORRECTO*`
 user.registered = false
 m.reply(`*📇 USTED YA NO ESTÁ REGISTRADO*`)
 }
 
-if (command == 'sn' || commad == 'myns') {
+if (command == 'sn' || command == 'myns') {
 conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', '⬇️ *Este es su número de serie* ⬇️', 'status@broadcast')
 }
 }
