72c928099874defa4ebe0a5710e232801df247cc
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-03-11 18:16:16 -0600

    Update PruebaOwner.js

--- plugins/PruebaOwner.js
+++ plugins/PruebaOwner.js
@@ -9,13 +9,14 @@ try {
 conn.sendPresenceUpdate('composing', m.chat)
 var apii = await fetch(`https://lookup.binlist.net/${text}`)
 var res = await apii.json()
-await m.reply(`${res.scheme} 
+/*await m.reply(`${res.scheme} 
 ${res.type}
 ${res.brand} 
 ${res.alpha2} 
 ${res.numeric.name}
 ${res.emoji} 
-${res.bank}`)
+${res.bank}`)*/
+m.reply(res)
 
 } catch (error) {
 console.error(error)
