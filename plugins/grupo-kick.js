cbc75b419a620d7c41147676db8ba2c9426c506e
MoonContentCreator <136917052+MoonContentCreator@users.noreply.github.com>
2024-03-13 12:53:31 +0100

    Update grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,17 +1,25 @@
 var handler = async (m, { conn, participants, usedPrefix, command }) => {
+    if (!m.mentionedJid[0] && !m.quoted) {
+        return conn.reply(m.chat, '🎌 *Etiqueta o responde al mensaje de la persona que quieres eliminar*', m);
+    }
 
-if (!m.mentionedJid[0] && !m.quoted) return conn.reply(m.chat, '🎌 *Etiquete o responda al mensaje de la persona que quiere eliminar*', m, fake, ) 
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+    let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
 
-await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+    const groupInfo = await conn.groupMetadata(m.chat);
+    const owner = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net';
 
-}
-handler.help = ['kick']
-handler.tags = ['grupo']
-handler.command = /^(kick|echar|hechar|sacar|ban)$/i
+    if (user === conn.user.jid || user === owner) {
+        return conn.reply(m.chat, '🚩 No puedo eliminar el bot ni el creador del grupo ', m);
+    }
 
-handler.admin = true
-handler.group = true
-handler.botAdmin = true
+    await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
+};
 
-export default handler
+handler.help = ['kick'];
+handler.tags = ['grupo'];
+handler.command = /^(kick|echar|hechar|sacar|ban)$/i;
+handler.admin = true;
+handler.group = true;
+handler.botAdmin = true;
+
+export default handler;
