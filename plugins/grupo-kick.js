3f01db9d22d61781ea2d258111ce8019dc7ce573
MoonContentCreator <136917052+MoonContentCreator@users.noreply.github.com>
2024-03-19 01:27:15 +0100

    Update grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,34 +1,44 @@
-var handler = async (m, { conn, participants, usedPrefix, command }) => {
-    if (!m.mentionedJid[0] && !m.quoted) {
-        return conn.reply(m.chat, '🎌 *Etiqueta o responde al mensaje de la persona que quieres eliminar*', m);
-    }
-
-    let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
-
+const handler = async (m, { conn, text, command, usedPrefix }) => {
     const groupInfo = await conn.groupMetadata(m.chat);
     const ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net';
     const ownerBot = global.owner[0][0] + '@s.whatsapp.net';
 
-    if (user === conn.user.jid) {
-        return conn.reply(m.chat, '🚩 No puedo eliminar el bot del grupo', m);
+    let who;
+    if (m.isGroup) {
+        who = m.mentionedJid[0] ?
+            m.mentionedJid[0] :
+            m.quoted ?
+            m.quoted.sender :
+            text;
+    } else who = m.chat;
+
+    const user = global.db.data.users[who];
+
+    if (who === conn.user.jid) {
+        return conn.reply(m.chat, '🚩 No puedo advertir al bot', m);
+    } else if (who === ownerBot) {
+        return conn.reply(m.chat, '🚩 No puedo advertir al propietario del bot', m);
     }
 
-    if (user === ownerGroup) {
-        return conn.reply(m.chat, '🚩 No puedo eliminar al creador del grupo', m);
+    user.warn += 1;
+
+    const msgtext = text || 'Sin motivo';
+    const sdms = msgtext.replace(/@\d+-?\d* /g, '');
+    const warntext = `*❌ 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴 𝙰 𝚄𝙽𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰 𝙾 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾 𝙿𝙰𝚁𝙰 𝙰𝙳𝚅𝙴𝚁𝚃𝙸𝚁 𝙰𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} @0*`;
+
+    await m.reply(`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `*@${who.split`@`[0]}*`} 𝚁𝙴𝙲𝙸𝙱𝙸𝙾 𝚄𝙽𝙰 𝙰𝙳𝚅𝙴𝚁𝚃𝙴𝙽𝙲𝙸𝙰 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾!\nMotivo: ${sdms}\n*ADVERTENCIAS ${user.warn}/4*`, null, { mentions: [who] });
+
+    if (user.warn >= 4) {
+        user.warn = 0;
+        await m.reply(`𝚃𝙴 𝙻𝙾 𝙰𝙳𝚅𝙴𝚁𝚃𝙸 𝚅𝙰𝚁𝙸𝙰𝚂 𝚅𝙴𝙲𝙴𝚂!!\n*@${who.split`@`[0]}* 𝚂𝚄𝙿𝙴𝚁𝙰𝚂𝚃𝙴 𝙻𝙰𝚂 *4* 𝙰𝙳𝚅𝙴𝚁𝚃𝙴𝙽𝙲𝙸𝙰𝚂, 𝙰𝙷𝙾𝚁𝙰 𝚂𝙴𝚁𝙰𝚂 𝙴𝙻𝙸𝙼𝙸𝙽𝙰𝙳𝙾/𝙰 👽`, null, { mentions: [who] });
+        await conn.groupParticipantsUpdate(m.chat, [who], 'remove');
     }
 
-    if (user === ownerBot) {
-        return conn.reply(m.chat, '🚩 No puedo eliminar al propietario del bot (' + OwnerBot + ')', m);
-    }
-
-    await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
+    return !1;
 };
 
-handler.help = ['kick'];
-handler.tags = ['grupo'];
-handler.command = /^(kick|echar|hechar|sacar|ban)$/i;
-handler.admin = true;
+handler.command = /^(advertir|advertencia|warn|warning)$/i;
 handler.group = true;
+handler.admin = true;
 handler.botAdmin = true;
-
 export default handler;
