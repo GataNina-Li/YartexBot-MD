f4827b4b2dbad78198869f5d7c1afb62af0b2903
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-03-18 20:30:33 -0600

    Update gc-warn.js

--- plugins/gc-warn.js
+++ plugins/gc-warn.js
@@ -21,7 +21,7 @@ for (let i = 0; i < global.owner.length; i++) {
 ownerNumber = global.owner[i][0];
 if (usuario.replace(/@s\.whatsapp\.net$/, '') === ownerNumber) {
 aa = ownerNumber + '@s.whatsapp.net'
-await conn.reply(m.chat, `⚠️ `, null, { mentions: [aa] })
+await conn.reply(m.chat, `…`, m, { mentions: [aa] })
 return
 }}
   
