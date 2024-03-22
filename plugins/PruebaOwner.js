a46f3509721644a89fcace012ec623527eff8eb3
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-03-21 20:20:05 -0600

    Update PruebaOwner.js

--- plugins/PruebaOwner.js
+++ plugins/PruebaOwner.js
@@ -1,37 +1,10 @@
-import fetch from 'node-fetch'
-///import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
-
 var handler = async (m, { text,  usedPrefix, command }) => {
 
-//if (!text) return conn.reply(m.chat, `🎌 *Falta bing*`, m, fake, )
 
-//let sexo = nanoid()
-
-//  m.reply(sexo)
-/*try {
-
-c onn.sendPresenceUpdate('composing', m.chat)
-var apii = await fetch(`https://lookup.binlist.net/${text}`)
-var res = await apii.json()
-/*await m.reply(`${res.scheme} 
-${res.type}
-${res.brand} 
-${res.alpha2} 
-${res.numeric.name}
-${res.emoji} 
-${res.bank}`)
-m.reply(apii)
-
-} catch (error) {
-console.error(error)
-return conn.reply(m.chat, `*🚩 Ocurrió un fallo*`, m, fake, )
-}*/
 conn.reply(m.chat, 'Zam', m, fake, )
 
 }
-handler.command = ['Ts']
-//handler.help = ['']
-//handler.tags = ['ai']
+handler.command = ['ts']
 
 handler.premium = false
 
