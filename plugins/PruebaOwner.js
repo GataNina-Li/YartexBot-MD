048a0815fbd7799f1f8197c9bb825e885f65aa65
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-03-21 23:10:29 -0600

    Update PruebaOwner.js

--- plugins/PruebaOwner.js
+++ plugins/PruebaOwner.js
@@ -1,20 +1,28 @@
 var handler = async (m, { text,  usedPrefix, command }) => {
 
+const kalgans = { 
+key: {
+fromMe: true, 
+participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "" } : {}) 
+},
+'message': {
+"interactiveMessage": {
+"header": {
+"hasMediaAttachment": [],
+"jpegThumbnail": imagen1,
+},
+"nativeFlowMessage": {
+"buttons": [
+{
+"name": "review_and_pay",
+"buttonParamsJson": "{\"currency\":\"USD\",\"payment_configuration\":\"\",\"payment_type\":\"\",\"transaction_id\":\"\",\"total_amount\":{\"value\":879912500,\"offset\":100},\"reference_id\":\"4N88TZPXWUM\",\"type\":\"physical-goods\",\"payment_method\":\"\",\"order\":{\"status\":\"pending\",\"description\":\"\",\"subtotal\":{\"value\":990000000,\"offset\":100},\"tax\":{\"value\":8712000,\"offset\":100},\"discount\":{\"value\":118800000,\"offset\":100},\"shipping\":{\"value\":500,\"offset\":100},\"order_type\":\"ORDER\",\"items\":[{\"retailer_id\":\"custom-item-c580d7d5-6411-430c-b6d0-b84c242247e0\",\"name\":\"JAMUR\",\"amount\":{\"value\":1000000,\"offset\":100},\"quantity\":99},{\"retailer_id\":\"custom-item-e645d486-ecd7-4dcb-b69f-7f72c51043c4\",\"name\":\"Wortel\",\"amount\":{\"value\":5000000,\"offset\":100},\"quantity\":99},{\"retailer_id\":\"custom-item-ce8e054e-cdd4-4311-868a-163c1d2b1cc3\",\"name\":\"Tahu\",\"amount\":{\"value\":4000000,\"offset\":100},\"quantity\":99}]},\"additional_note\":\"\"}"
+}
+]
+}
+}
+}}
 
-//conn.reply(m.chat, 'Zam', m, fake, )
-  if (m.quotedMsg && m.quotedMsg.fromMe && m.quotedMsg.isForwarded && m.quotedMsg.forwardingScore !== undefined) {
- /*await conn.forwardMessage(m.chat, wm, {
-      forwardingScore: m.quotedMsg.forwardingScore,
-      isForwarded: true,
-      externalAdReply: {
-        title: 'Título del canal',
-        body: 'Descripción del canal',
-        sourceUrl: channel,
-        thumbnailUrl: fotos,
-      },
-    })}*/
-  await conn.reply(m.chat, '*Test*', m, { contextInfo:{ forwardingScore: m.quoted.forwardingScore, isForwarded: true, externalAdReply: {title: '👋 Hola!!', body: saludo, sourceUrl: channel, thumbnailUrl: fotos}}})
-m.react('🚀') }
+conn.sendMessge(m.chat, { text: wm }, { quoted: kalgans })
 
 }
 handler.command = ['ts']
