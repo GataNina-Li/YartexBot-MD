8b7bb6df253c82700aaaceb6efcc7c45f72e179f
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-03-21 20:23:39 -0600

    Update PruebaOwner.js

--- plugins/PruebaOwner.js
+++ plugins/PruebaOwner.js
@@ -1,7 +1,9 @@
 var handler = async (m, { text,  usedPrefix, command }) => {
 
 
-conn.reply(m.chat, 'Zam', m, fake, )
+//conn.reply(m.chat, 'Zam', m, fake, )
+  await conn.reply(m.chat, '*Test*', { contextInfo:{ forwardingScore: 2022, isForwarded: true, channel: channel, externalAdReply: {title: '👋 Hola!!', body: saludo, sourceUrl: global.ig, thumbnailUrl: ppBot }}})
+m.react('🚀') 
 
 }
 handler.command = ['ts']
