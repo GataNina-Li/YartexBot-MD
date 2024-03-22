59789524fe7ad5fb59067e57d625e5cabaf3f954
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-03-21 20:50:57 -0600

    Update PruebaOwner.js

--- plugins/PruebaOwner.js
+++ plugins/PruebaOwner.js
@@ -2,7 +2,18 @@ var handler = async (m, { text,  usedPrefix, command }) => {
 
 
 //conn.reply(m.chat, 'Zam', m, fake, )
-  await conn.reply(m.chat, '*Test*', { contextInfo:{ forwardingScore: 2022, isForwarded: true, channel: channel, externalAdReply: {title: '👋 Hola!!', body: saludo, sourceUrl: global.ig, thumbnailUrl: ppBot }}})
+  /*if (m.quotedMsg && m.quotedMsg.fromMe && m.quotedMsg.isForwarded && m.quotedMsg.forwardingScore !== undefined) {
+ await conn.forwardMessage(m.chat, wm, {
+      forwardingScore: m.quotedMsg.forwardingScore,
+      isForwarded: true,
+      externalAdReply: {
+        title: 'Título del canal',
+        body: 'Descripción del canal',
+        sourceUrl: channel,
+        thumbnailUrl: fotos,
+      },
+    })}*/
+  await conn.reply(m.chat, '*Test*', { contextInfo:{ forwardingScore: channel, isForwarded: true, externalAdReply: {title: '👋 Hola!!', body: saludo, sourceUrl: global.ig, thumbnailUrl: fotos}}})
 m.react('🚀') 
 
 }
