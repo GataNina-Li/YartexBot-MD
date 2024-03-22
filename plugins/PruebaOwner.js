121d8cb3abf0b9b933f77674ed6b167ebc2ca361
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-03-21 23:17:14 -0600

    Update PruebaOwner.js

--- plugins/PruebaOwner.js
+++ plugins/PruebaOwner.js
@@ -22,7 +22,28 @@ participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "" } : {})
 }
 }}
 
-conn.sendMessage(m.chat, { text: wm }, { quoted: kalgans })
+const hw = { 
+key: {
+fromMe: false, 
+participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "status@broadcast" } : {}) 
+},
+"message": {
+"audioMessage": {
+"url": "https://mmg.whatsapp.net/v/t62.7114-24/56189035_1525713724502608_8940049807532382549_n.enc?ccb=11-4&oh=01_AdR7-4b88Hf2fQrEhEBY89KZL17TYONZdz95n87cdnDuPQ&oe=6489D172&mms3=true",
+"mimetype": "audio/mp4",
+"fileSha256": "oZeGy+La3ZfKAnQ1epm3rbm1IXH8UQy7NrKUK3aQfyo=",
+"fileLength": "1067401",
+"seconds": 60,
+"ptt": true,
+"mediaKey": "PeyVe3/+2nyDoHIsAfeWPGJlgRt34z1uLcV3Mh7Bmfg=",
+"fileEncSha256": "TLOKOAvB22qIfTNXnTdcmZppZiNY9pcw+BZtExSBkIE=",
+"directPath": "/v/t62.7114-24/56189035_1525713724502608_8940049807532382549_n.enc?ccb=11-4&oh=01_AdR7-4b88Hf2fQrEhEBY89KZL17TYONZdz95n87cdnDuPQ&oe=6489D172",
+"mediaKeyTimestamp": "1684161893"
+}}}
+
+
+
+conn.sendMessage(m.chat, { text: wm }, { quoted: hw })
 
 }
 handler.command = ['ts']
