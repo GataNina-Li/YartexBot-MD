e6e29ab2f6ac556cead6336d6300db2fac1c34f8
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-31 08:51:49 -0500

    CURIOSITY : BOT - MD

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,66 +1,12 @@
-//const require sticker from '../lib/sticker.js'
-//const require uploadFile from '../lib/uploadFile.js'
-//const require uploadImage from '../lib/uploadImage.js'
-//const require webp2png from '../lib/webp2mp4.js'
+import { sticker } from '../lib/sticker.js'
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+import { webp2png } from '../lib/webp2mp4.js'
 
-//import {sticker} from "../lib/sticker.js";
-//import uploadFile from "../lib/uploadFile.js";
-//import uploadImage from "../lib/uploadImage.js";
-//import {webp2png} from "../lib/webp2mp4.js";
-
-let handler = async (m, {conn, args, usedPrefix, command}) => {
-  let stiker = false;
-  let username = conn.getName(m.sender);
-  try {
-    let q = m.quoted ? m.quoted : m;
-    let mime = (q.msg || q).mimetype || q.mediaType || "";
-    if (/webp|image|video/g.test(mime)) {
-      if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply("*⚠️ EL VÍDEO NO PUEDE DURAR MAS DE 7 SEGUNDOS*");
-      let img = await q.download?.();
-      if (!img)
-        throw `*⚠️ RESPONDE A UNA IMAGEN O VIDEO CON ${usedPrefix + command}*`;
-      let out;
-      try {
-        stiker = await sticker(img, false, global.packname, global.author);
-      } catch (e) {
-        console.error(e);
-      } finally {
-        await conn.reply(m.chat, `_Calma crack estoy haciendo tu sticker 👏_\n\n_*Recuerda los stickersgif son de 6 segundos*_\n\n_*by CuriosityBot*_`, m)
-        if (!stiker) {
-          if (/webp/g.test(mime)) out = await webp2png(img);
-          else if (/image/g.test(mime)) out = await uploadImage(img);
-          else if (/video/g.test(mime)) out = await uploadFile(img);
-          if (typeof out !== "string") out = await uploadImage(img);
-          stiker = await sticker(false, out, global.packname, global.author);
-        }
-      }
-    } else if (args[0]) {
-      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author);
-      else return m.reply("*⚠️ EL ENLACE / URL / LINK NO ES VÁLIDO*");
-    }
-  } catch (e) {
-    console.error(e);
-    if (!stiker) stiker = e;
-  } finally {
-    if (stiker)
-      conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-    else
-      throw "*⚠️ RESPONDE A UNA IMAGEN O VIDEO CON ${usedPrefix + command}*";
-  }
-};
-handler.help = ["sticker"];
-handler.tags = ["sticker"];
-handler.command = ["s", "sticker", "stiker"];
-
-export default handler;
-
-const isUrl = (text) => {
-  return text.match(
-    new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, "gi")
-  );
-};
-/*let handler = async (m, { conn, args, usedPrefix, command, text }) => {
+let handler = async (m, { conn, args, usedPrefix, command, text }) => {
 let stiker = false
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let autor = await conn.getName(who)
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 
@@ -72,14 +18,14 @@ let img = await q.download?.()
 let out
 stiker = await sticker(img, false, global.packname, global.author)
 await conn.reply(m.chat, `_Calma crack estoy haciendo tu sticker 👏_\n\n_*Recuerda los stickersgif son de 6 segundos*_\n\n_*by CuriosityBot*_`, m)
-  
+
 if (!stiker) {
 if (/webp/g.test(mime)) out = await webp2png(img)
 else if (/image/g.test(mime)) out = await uploadImage(img)
 else if (/video/g.test(mime)) out = await uploadFile(img)
 if (typeof out !== 'string') out = await uploadImage(img)
 stiker = await sticker(false, out, global.packname, global.author)
-  
+
 if (!stiker) errorMessage = 'ERROR'
 }} else if (args[0]) {
 if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
@@ -92,7 +38,9 @@ console.log(stiker)
 }}
 
 handler.command = /^(s(tickers?)?(image|video|gif|img)?)$/i
+handler.help = ['s', 'stickers']
+handler.tags = ['sticker']
 export default handler
 
 const isUrl = (text) => {
-return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))}*/
+return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))}
