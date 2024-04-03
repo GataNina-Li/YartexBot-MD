5a2c3cec2cb2a377e12459c9512410e619c07f31
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-04-03 13:36:53 -0300

    Add files via upload

--- plugins/juegos-ppt.js
+++ plugins/juegos-ppt.js
@@ -1,62 +1,71 @@
-const handler = async (m, {conn, text, command, usedPrefix, args}) => {
-// let pp = 'https://www.bighero6challenge.com/images/thumbs/Piedra,-papel-o-tijera-0003318_1584.jpeg'
-  const pp = 'https://telegra.ph/file/c7924bf0e0d839290cc51.jpg';
-  if (!args[0]) return conn.reply(m.chat, `*𝐏𝐢𝐞𝐝𝐫𝐚 🗿, 𝐏𝐚𝐩𝐞𝐥 📄 𝐨 𝐓𝐢𝐣𝐞𝐫𝐚 ✂️*\n\n*—◉ 𝚙𝚞𝚎𝚍𝚎𝚜 𝚞𝚜𝚊𝚛 𝚎𝚜𝚝𝚘𝚜 𝚌𝚘𝚖𝚊𝚗𝚍𝚘𝚜:*\n*◉ ${usedPrefix + command} piedra*\n*◉ ${usedPrefix + command} papel*\n*◉ ${usedPrefix + command} tijera*`, m);
-  // conn.sendButton(m.chat, `*𝐏𝐢𝐞𝐝𝐫𝐚 🗿, 𝐏𝐚𝐩𝐞𝐥 📄 𝐨 𝐓𝐢𝐣𝐞𝐫𝐚 ✂️*\n\n*—◉  𝙿𝚎𝚍𝚎𝚜 𝚞𝚜𝚊𝚛 𝚕𝚘𝚜 𝚋𝚘𝚝𝚘𝚗𝚎𝚜 𝚙𝚊𝚛𝚊 𝚓𝚞𝚐𝚊𝚛 𝚘 𝚝𝚊𝚖𝚋𝚒𝚎𝚗 𝚙𝚞𝚎𝚍𝚎𝚜 𝚞𝚜𝚊𝚛 𝚎𝚜𝚝𝚘𝚜 𝚌𝚘𝚖𝚊𝚗𝚍𝚘𝚜:*\n*◉ ${usedPrefix + command} piedra*\n*◉ ${usedPrefix + command} papel*\n*◉ ${usedPrefix + command} tijera*`, wm, pp, [['Piedra 🗿', `${usedPrefix + command} piedra`], ['Papel 📄', `${usedPrefix + command} papel`], ['Tijera ✂️', `${usedPrefix + command} tijera`]], m)
-  let astro = Math.random();
-  if (astro < 0.34) {
-    astro = 'piedra';
-  } else if (astro > 0.34 && astro < 0.67) {
-    astro = 'tijera';
-  } else {
-    astro = 'papel';
-  }
-  const textm = text.toLowerCase();
-  if (textm == astro) {
-    global.db.data.users[m.sender].exp += 600;
-    m.reply(`*🔰 Empate!*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +500 XP*`);
-  } else if (text == 'papel') {
-    if (astro == 'piedra') {
-      global.db.data.users[m.sender].exp += 1050;
-      m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +1000 XP*`);
-    } else {
-      global.db.data.users[m.sender].exp -= 300;
-      m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -300 XP*`);
-    }
-  } else if (text == 'tijera') {
-    if (astro == 'papel') {
-      global.db.data.users[m.sender].exp += 1000;
-      m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +1000 XP*`);
-    } else {
-      global.db.data.users[m.sender].exp -= 300;
-      m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -300 XP*`);
-    }
-  } else if (textm == 'tijera') {
-    if (astro == 'papel') {
-      global.db.data.users[m.sender].exp += 1000;
-      m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +1000 XP*`);
-    } else {
-      global.db.data.users[m.sender].exp -= 300;
-      m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -300 XP*`);
-    }
-  } else if (textm == 'papel') {
-    if (astro == 'piedra') {
-      global.db.data.users[m.sender].exp += 1300;
-      m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +1000 XP*`);
-    } else {
-      global.db.data.users[m.sender].exp -= 300;
-      m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -300 XP*`);
-    }
-  } else if (textm == 'piedra') {
-    if (astro == 'tijera') {
-      global.db.data.users[m.sender].exp += 1500;
-      m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +1000 XP*`);
-    } else {
-      global.db.data.users[m.sender].exp -= 300;
-      m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${textm}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -300 XP*`);
-    }
-  }};
-handler.help = ['ppt'];
-handler.tags = ['games'];
-handler.command = /^(ppt|suit|pvp|Ppt)$/i;
-export default handler;
+var handler = async (m, { conn, text, usedPrefix, command }) => {
+
+let poin = 500
+let poin2 = 20
+
+if (!text) return conn.reply(m.chat, `🎌 *Seleccione piedra/papel/tijera*\n\nEjemplo, !${command} papel`, m, fake, )
+
+var astro = Math.random()
+
+if (astro < 0.34) {
+astro = 'piedra'
+} else if (astro > 0.34 && astro < 0.67) {
+astro = 'tijera'
+} else {
+astro = 'papel'
+}
+
+if (text == astro) {
+global.db.data.users[m.sender].exp += 100
+global.db.data.users[m.sender].diamond += 5
+
+conn.reply(m.chat, `🚩 *Empate*\n\n> Tú: ${text}\n> El Bot: ${astro}\n\n(±)100 XP ✨\n(±)5 Diamantes 💎`, m, fake, )
+} else if (text == 'piedra') {
+if (astro == 'tijera') {
+  
+global.db.data.users[m.sender].exp += 300
+global.db.data.users[m.sender].diamond += 10
+
+conn.reply(m.chat, `🎌 *Ganaste*\n\n> Tú: ${text}\n> El Bot: ${astro}\n\n+${poin} XP ✨\n+${poin2} Diamantes 💎`, m, fake, )
+} else {
+global.db.data.users[m.sender].diamond -= 10
+global.db.data.users[m.sender].exp -= 300
+
+conn.reply(m.chat, `🚩 *Perdiste*\n\n> Tú: ${text}\n> El Bot: ${astro}\n\n-${poin} XP ✨\n-${poin2} Diamantes 💎`, m, fake, )
+}
+} else if (text == 'tijera') {
+if (astro == 'papel') {
+global.db.data.users[m.sender].diamond += 20
+global.db.data.users[m.sender].exp += 500
+
+conn.reply(m.chat, `🎌 *Ganaste*\n\n> Tú: ${text}\n> El Bot: ${astro}\n\n+${poin} XP ✨\n+${poin2} Diamantes 💎`, m, fake, )
+} else {
+global.db.data.users[m.sender].diamond -= 15
+global.db.data.users[m.sender].exp -= 150
+
+conn.reply(m.chat, `🚩 *Perdiste*\n\n> Tú: ${text}\n> El Bot: ${astro}\n\n-${poin} XP ✨\n-${poin2} Diamantes 💎`, m, fake, )
+}
+} else if (text == 'papel') {
+if (astro == 'piedra') {
+global.db.data.users[m.sender].diamond += 25
+global.db.data.users[m.sender].exp += 600
+
+conn.reply(m.chat, `🎌 *Ganaste*\n\n> Tú: ${text}\n> El Bot: ${astro}\n\n+${poin} XP ✨\n+${poin2} Diamantes 💎`, m, fake, )
+} else {
+global.db.data.users[m.sender].diamond -= 10
+global.db.data.users[m.sender].exp -= 300
+conn.reply(m.chat, `🚩 *Perdiste*\n\n> Tú: ${text}\n> El Bot: ${astro}\n\n-${poin} XP ✨\n-${poin2} Diamantes 💎`, m, fake, )
+}
+
+} else {
+return conn.reply(m.chat, `🎌 *Seleccione piedra/papel/tijera*\n\nEjemplo, !${command} papel`, m, fake, )
+}
+
+}
+handler.help = ['ppt']
+handler.tags = ['juegos']
+handler.command = ['ppt']
+
+handler.register = true
+
+export default handler
--- plugins/juegos-slot.js
+++ plugins/juegos-slot.js
@@ -2,21 +2,22 @@ let reg = 60
 
 var handler = async (m, { conn, args, usedPrefix, command }) => {
 
-let fa = `🎌 *¿Cuánto quieres apostar?*
+let fa = `*⚠️ CUANTO QUIERES APOSTAR?*
 
-Ejemplo, !${command} 100`
-if (!args[0]) return conn.reply(m.chat, fa, m, fake, )
-if (isNaN(args[0])) return conn.reply(m.chat, fa, m, fake, )
+❕ EJEMPLO:
+*${usedPrefix + command}* 100`.trim()
+if (!args[0]) throw fa
+if (isNaN(args[0])) throw fa
 let apuesta = parseInt(args[0])
 let users = global.db.data.users[m.sender]
 let time = users.lastslot + 10000
-if (new Date - users.lastslot < 10000) return conn.reply(m.chat, `⏰ *Deberá esperar ${msToTime(time - new Date())}*`, m, fake, )
-if (apuesta < 50) return conn.reply(m.chat, '🚩 *Lo mínimo para apostar es 100 XP*', m, fake, )
+if (new Date - users.lastslot < 10000) throw `*⏰ ESPERE ${msToTime(time - new Date())} PARA USAR DE NUEVO*`
+if (apuesta < 50) throw '*⚠️ LO MÍNIMO PARA APOSTAR ES 100 XP*'
 if (users.exp < apuesta) {
-return conn.reply(m.chat, '🚩 *No tienes suficiente XP*', m, fake, )
+throw `*⚠️ NO TIENES SUFICIENTE XP*`
 }
 
-let emojis = ['🔔', '🍒', '💰']
+let emojis = ["🍩", "🍧", "🧃"]
 let a = Math.floor(Math.random() * emojis.length)
 let b = Math.floor(Math.random() * emojis.length)
 let c = Math.floor(Math.random() * emojis.length)
@@ -40,31 +41,54 @@ if (c == emojis.length) c = 0
 }
 let end
 if (a == b && b == c) {
-end = `🎁 *Genial, Ganaste la apuesta*\n*+${apuesta + apuesta + apuesta + apuesta + apuesta} XP*`
+end = `🎁 *GANASTE* 🎁\n*+${apuesta + apuesta + apuesta + apuesta + apuesta} XP*`
 users.exp += apuesta + apuesta
 } else if (a == b || a == c || b == c) {
 end = `🌟 *Casi lo logras sigue intentando*\n*Te regalo ${reg} XP*`
 users.exp += reg
 } else {
-end = `⚠️ *Perdiste la apuesta*\n*Te e quitado ${apuesta} XP*`
+end = `🤯 *PERDISTE* 🤯\n*Te e quitado ${apuesta} XP*`
 users.exp -= apuesta
 }
 users.lastslot = new Date * 1
-return await conn.reply(m.chat,
-`🚩 ┃ *SLOTS* 
+
+
+var hawemod = [
+`${x[1]} : ${y[0]} : ${z[0]}
+${z[0]} : ${y[1]} : ${x[1]}
+${z[1]} : ${x[2]} : ${y[0]}`, 
+`${x[0]} : ${y[1]} : ${z[2]}
+${y[1]} : ${z[2]} : ${x[1]}
+${x[2]} : ${y[0]} : ${z[0]}`, 
+`${x[1]} : ${y[2]} : ${z[1]}
+${y[0]} : ${z[0]} : ${x[2]}
+${x[2]} : ${y[1]} : ${z[0]}`
+]
+
+const maxIterations = 25;
+const arrayCasuale = generaArrayCasuale(hawemod, maxIterations);
+
+const array = [...arrayCasuale, ];
+  
+let { key } = await conn.sendMessage(m.chat, { text: `*A cruzar los dedos*` }, fake);
+
+for (let i = 0; i < maxIterations; i++) {
+
+await conn.sendMessage(m.chat, { text: `🎰 | *RANURAS* | 🎰\n────────\n` + `${array[i]}` + `\n ────────\n🎰 |   *SLOTS*   | 🎰`, edit: key }, { quoted: m });
+await new Promise((resolve) => setTimeout(resolve, 1))}
+  
+return await conn.sendMessage(m.chat, {text: `
+🎰 ┃ *SLOTS* 
 ───────────
 ${x[0]} : ${y[0]} : ${z[0]}
 ${x[1]} : ${y[1]} : ${z[1]}
 ${x[2]} : ${y[2]} : ${z[2]}
 ───────────
-       
-${end}`, m, fake, )
-
+🎰┃🎰┃ 🎰\n\n${end}`, edit: key})
 }
 handler.help = ['slot']
 handler.tags = ['juegos']
 handler.command = ['slot']
-
 handler.register = true
 
 export default handler
@@ -75,9 +99,9 @@ seconds = Math.floor((duration / 1000) % 60),
 minutes = Math.floor((duration / (1000 * 60)) % 60),
 hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
 
-hours = (hours < 10) ? '0' + hours : hours
-minutes = (minutes < 10) ? '0' + minutes : minutes
-seconds = (seconds < 10) ? '0' + seconds : seconds
+hours = (hours < 10) ? "0" + hours : hours
+minutes = (minutes < 10) ? "0" + minutes : minutes
+seconds = (seconds < 10) ? "0" + seconds : seconds
 
-return seconds + ' Segundo(s)'
+return seconds + " Segundo(s)"
 }
