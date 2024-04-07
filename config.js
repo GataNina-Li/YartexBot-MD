7051df5a74d8162a3be7a877ee66f4cf164f247c
Ender incorregible <142455051+Ender-GB-Isis777@users.noreply.github.com>
2024-04-06 22:17:58 -0600

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['524531287294', 'CuriosityBot-MD 🍧', true], ['5217294888993'], ['5214531287294'], ['5492266466080'], ['5492266613038'], ['573012482597'], ['5215610314499'], ['51935531943'], ['33760536110'], ['59162714296'], ['5492964650915']]
+global.owner = [['524531287294', 'CuriosityBot-MD 🍧', true], ['5217294888993'], ['5214531287294'], ['50558124470'], ['5492266466080'], ['5492266613038'], ['573012482597'], ['5215610314499'], ['51935531943'], ['33760536110'], ['59162714296'], ['5492964650915']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
