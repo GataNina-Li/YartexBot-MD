d05e370db17e70ba09671f5b7ce0185f2fbb9053
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-11 22:17:49 -0500

    Actualizar plugins

---
+++ plugins/bot-decir.js
@@ -0,0 +1,19 @@
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let name = conn.getName(who)
+            const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+
+const pp = './storage/logos/Menu2.jpg'
+
+let di = `> Mensaje: ${text} 🚩\n\n*${name} Busca Una Familia*`
+
+conn.sendMessage(m.chat, {text: di, mentions: [m.sender]}, {quoted: fkontak2})
+
+//conn.reply(m.chat, `*❌️ Ocurrió un fallo*`)
+
+}
+handler.help = ['decir']
+handler.tags = ['penelope']
+handler.command = /^(decir|emogis|di)$/i
+//handler.register = true
+export default handler
