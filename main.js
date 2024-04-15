a0c6f0b7b5f4075fd9955a693a821c7b59d25e8e
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-04-15 17:50:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -140,7 +140,7 @@ const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
 mobile: MethodMobile, 
-browser: ['Ubuntu', 'Chrome', '110.0.5585.95'],
+browser: ["Ubuntu", "Chrome", "20.0.04"],
 auth: {
 creds: state.creds,
 keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
