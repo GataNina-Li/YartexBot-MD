ad65f7b8a4b83b20daadc64a3158c58c5ef22856
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-22 22:37:39 -0500

    Actualizar prueba-button.js

--- plugins/prueba-button.js
+++ plugins/prueba-button.js
@@ -1,13 +1,13 @@
 let foto = imagen4
 let handler = async (m, { conn, command }) => {
 let user = db.data.users[m.sender]
-let text = `🔥 𝗕𝘂𝘁𝘁𝗼𝗻 𝗧𝗲𝘀𝘁 🐈🚀`
+let text = `🍍 𝗕𝘂𝘁𝘁𝗼𝗻 𝗧𝗲𝘀𝘁 🥳🍓`
 await conn.sendButton(m.chat, text, `👋🏻 ¡Hola! ` + nombre, foto, [
-['☄️ 𝗠𝗲𝗻𝘂 𝗖𝗼𝗺𝗽𝗹𝗲𝘁𝗼 💫', '!allmenu'],
-['🔥 𝗦𝗰𝗿𝗶𝗽𝘁 🍁', '!sc'],
-['❤️ 𝗖𝗿𝗲𝗮𝗱𝗼𝗿 💖', '#owner'],
-['🌼 𝗕𝗼𝘁 🗒️', '!bot Hola']], null, [
-['🚩 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 🏝️', `${channel}`]], m)}
+['🚩 Menu Completo', '!allmenu'],
+['♨️ Script Bot', '!sc'],
+['❤️ Propietario', '#owner'],
+['🌼 Hablar Al  Bot', '!bot Hola']], null, [
+['🌏 Canal Curiosity', `${channel}`]], fkontak)}
 
 handler.command = /^(prueba|mprueba|button)$/i
 
