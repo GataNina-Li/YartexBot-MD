4606a6895e642bb10d6362d216a26968caab01b7
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-29 08:49:56 -0500

    Actualizar info-creditos.js

--- plugins/info-creditos.js
+++ plugins/info-creditos.js
@@ -1,26 +1,12 @@
 var handler = async (m) => {
 
-let tqto = `🚩 *Desarrollador*
-⬡ *Azami*
-https://github.com/AzamiJs
+let tqto = `👑 *Desarrolladora*
+⬡ *GataNina*
+https://github.com/GataNina-Li
 
 🎌 *Colaboradores*
-⬡ *Mario* 
-⬡ *GataDios*
-⬡ *Endymion*
-⬡ *Wotchito*
-⬡ *HJ*
-⬡ *Ender*
-⬡ *Ax*
-⬡ *Wilmer*
-⬡ *Jose*
-⬡ *Katashi*
-⬡ *Daniel*
-⬡ *X_Arumiii*
-⬡ *Wilson*
-⬡ *Cpw*
+⬡ *AzamiJs* 
 ⬡ *Diego*
-⬡ *Edwardofc*
 
 _Envie "colaboradores" para obtener el enlace del perfil de GitHub de los colaboradores_`
 
@@ -29,24 +15,10 @@ conn.reply(m.chat, tqto, m, fake, )
 handler.before = async m => {
 
 if (/^colaboradores|Colaboradores$/i.test(m.text) ) {
-let texto = `🚩 *GitHub - Colaboradores*
+let texto = `🌟 *GitHub - Colaboradores*
 
-⬡ https://github.com/elrebelde21
-⬡ https://github.com/GataNina-Li
-⬡ https://github.com/ReyEndymion
-⬡ https://github.com/WOTCHITO
-⬡ https://github.com/HACHEJOTA
-⬡ https://github.com/EnderLB
-⬡ https://github.com/Jxtxn17
-⬡ https://github.com/Wilsmac
-⬡ https://github.com/KimdanBot-MD
-⬡ https://github.com/KatashiFukushima
-⬡ https://github.com/MoonContentCreator
-⬡ https://github.com/ArizzVal
-⬡ https://github.com/WilsonWaoz
-⬡ https://github.com/WorkCwp
 ⬡ https://github.com/Diego-YL-177
-⬡ https://github.com/Edwardofc
+⬡ https://github.com/AzamiJs
 
 _Envie "cc" Para Obtener El Contacto De Los Colaboradores_`
 
@@ -54,46 +26,10 @@ conn.reply(m.chat, texto, m, fake, )
 }
 
 if (/^cc$/i.test(m.text) ) {
-let contacto = `🎌 *Contacto - Colaboradores* 
+let contacto = `🌟 *Contacto - Colaboradores* 
 
-⬡ *Gata Dios*
-@593968263524
-
-⬡ *ReyEndymion*
-@5215517489568
-
-⬡ *Wotchito*
-@573027866596
-
-⬡ *HJ*
-@524437863111
-
-⬡ *Ender*
-@50558124470
-
-⬡ *Jxjxn17*
-@51929972576
-
-⬡ *Wilmer*
-@50258115623
-
-⬡ *Jose*
-@573173090446
-
-⬡ *Katashi Fukushima*
-@51948705559
-
-⬡ *Daniel*
-@33760536110
-
-⬡ *X_Arumiii*
-@5215610314499
-
-⬡ *Wilson*
-@5492964650915
-
-⬡ *Cpw*
-@573003025991
+⬡ *Azami*
+@5214434703586
 
 ⬡ *Diego*
 @573012482597`
