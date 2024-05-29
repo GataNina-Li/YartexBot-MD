ee620c98a683f68552aa14beae92d126cacf8ab3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-05-29 02:58:34 -0500

    ✨️ Yartex

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ import { spawn } from 'child_process'
 import syntaxError from 'syntax-error'
 import { fileURLToPath } from 'url'
 import { createRequire } from 'module'
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const require = createRequire(__dirname)
