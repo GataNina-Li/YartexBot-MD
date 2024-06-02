9794648a1bed71416592de27a98206710214e0c3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-02 17:37:30 -0500

    Update store.js

--- store.js
+++ store.js
@@ -3,9 +3,13 @@ import chalk from 'chalk'
 import fs from 'fs'
 import moment from 'moment-timezone'
 import path, { join } from 'path'
+import { getRandomImageBoys } from 'module-gatadios'
 import { fileURLToPath, pathToFileURL } from 'url'
 global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') { return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString() }; global.__dirname = function dirname(pathURL) { return path.dirname(global.__filename(pathURL, true)) }; global.__require = function require(dir = import.meta.url) { return createRequire(dir) }
 const __dirname = global.__dirname(import.meta.url)
+
+// Random
+global.random1 = getRandomImageBoys()
 
 //Imagenes
 global.imagen1 = fs.readFileSync('./storage/logos/Menu1.jpg')
