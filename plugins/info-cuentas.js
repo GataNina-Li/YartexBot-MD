b9b192fe398d1aa68e73a7ead0ebd20e6827997f
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-03 12:09:18 -0500

    Actualizar info-cuentas.js

--- plugins/info-cuentas.js
+++ plugins/info-cuentas.js
@@ -1,25 +1,24 @@
 let media = 'https://telegra.ph/file/e2882a8fb92765b1da2b9.jpg'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let str = `💙 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎 𝘾𝙐𝙀𝙉𝙏𝘼𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎
+let str = `𝖡𝗂𝖾𝗇𝗏𝖾𝗇𝖽𝗂𝗈 𝖠 𝖫𝖺𝗌 𝖢𝗎𝖾𝗇𝗍𝖺𝗌 𝖮𝖿𝗂𝖼𝗂𝖺𝗅𝖾𝗌 😻
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *GITHUB*
+*Github Del Bot* 🌟
 *${md}*
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *INSTAGRAM - ASISTENCIA*
-*${ig}*
+*Canal Dedicado Para Yartex* 🌻
+ *${channel}*\n
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *YOUTUBE*
-*${yt}*
+*Youtube* 🐈
+${yt}
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *FACEBOOK*
-*${fb}*
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*Si tienen dudas, sugerencias, o preguntas solo escriban por Instagram.*\n
-*If you have doubts, suggestions or questions just write on Instagram.*`
+*Facebook* 🍓
+${fb}
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈`
 
-conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)
+await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
 
-handler.command = /^cuentasoficiales|gataig|cuentasyb|cuentayb|accounts|yartexaccounts|account|iggata|cuentasdeyaryex|cuentasdeyartexbot|cuentayartexbot|cuentasgatabot$/i
+handler.command = /^cuentas|cuentasoficiales$/i
 handler.exp = 35
+handler.register = true
 export default handler
