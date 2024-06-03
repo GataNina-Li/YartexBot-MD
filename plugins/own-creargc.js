985e217b0a3c47cffc0b61791928c8fed3a34c69
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-03 11:11:56 -0500

    Actualizar own-creargc.js

--- plugins/own-creargc.js
+++ plugins/own-creargc.js
@@ -1,27 +1,27 @@
 /*
 ⚠ PROHIBIDO EDITAR ⚠
-El codigo de este archivo fue actualizado por:
+El código de este archivo fue actualizado por:
 - Diego-YL-177 >> https://github.com/Diego-YL-177
 */
 
 let handler = async (m, { conn, text }) => {
+  if (!text) return m.reply('𝗘𝗦𝗖𝗥𝗜𝗕𝗔 𝗘𝗟 𝗡𝗢𝗠𝗕𝗥𝗘 𝗗𝗘𝗟 𝗚𝗥𝗨𝗣𝗢 🌟');
+  try {
+    //m.react('🚀')
+    m.reply('😊 𝗖𝗥𝗘𝗔𝗡𝗗𝗢 𝗘𝗟 𝗚𝗥𝗨𝗣𝗢, 𝗕𝗬: 𝗬𝗔𝗥𝗧𝗘𝗫')
+    let group = await conn.groupCreate(text, [m.sender]);
+    let link = await conn.groupInviteCode(group.gid);
+    let url = 'https://chat.whatsapp.com/' + link;
+    m.reply(url);
+  } catch (e) {
+    m.reply(`❌ 𝗘𝗥𝗥𝗢𝗥`);
+  }
+}
 
-if (!text) return m.reply('𝗘𝗦𝗖𝗥𝗜𝗕𝗔 𝗘𝗟 𝗡𝗢𝗠𝗕𝗥𝗘 𝗗𝗘𝗟 𝗚𝗥𝗨𝗣𝗢 🌟')
-try{
-//m.react('🚀')
-m.reply('😊 𝗖𝗥𝗘𝗔𝗡𝗗𝗢 𝗘𝗟 𝗚𝗥𝗨𝗣𝗢, 𝗕𝗬: 𝗬𝗔𝗥𝗧𝗘𝗫')
-m.reply(`${link}`)
-let group = await conn.groupCreate(text, [m.sender])
-let link = await conn.groupInviteCode(group)
-let url = 'https://chat.whatsapp.com/' + link;
-//m.reply(``)
-} catch (e) {
-m.reply(`❌ 𝗘𝗥𝗥𝗢𝗥`)
-}
-}
-handler.help = ['creargc *<nombre>*']
-handler.tags = ['owner']
-handler.command = /^(creargrupo|creargroup|creargc|gccreate)$/
-handler.owner = true
+handler.help = ['creargc *<nombre>*'];
+handler.tags = ['owner'];
+handler.command = /^(creargrupo|creargroup|creargc|gccreate)$/;
+handler.owner = true;
 //handler.premium = false
-export default handler
+
+export default handler;
