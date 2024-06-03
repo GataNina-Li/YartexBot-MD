595e8ecfb4076a19f6f39e032962bc3a159720f6
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-03 11:00:27 -0500

    Actualizar own-creargc.js

--- plugins/own-creargc.js
+++ plugins/own-creargc.js
@@ -13,7 +13,7 @@ m.reply('😊 𝗖𝗥𝗘𝗔𝗡𝗗𝗢 𝗘𝗟 𝗚𝗥𝗨𝗣𝗢, 𝗕�
 let group = await conn.groupCreate(text, [m.sender])
 let link = await conn.groupInviteCode(group.gid)
 let url = 'https://chat.whatsapp.com/' + link;
-await m.reply(url)
+m.reply(``)
 } catch (e) {
 m.reply(`❌ 𝗘𝗥𝗥𝗢𝗥`)
 }
