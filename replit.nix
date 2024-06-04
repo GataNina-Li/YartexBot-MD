34771c9d8606e20e4cf03f272f855880ed3c357a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-04 01:33:22 -0500

    ✨ Yartex

--- replit.nix
+++ replit.nix
@@ -1,20 +1,19 @@
 { pkgs }: {
-    deps = [
-        pkgs.nodejs
-        pkgs.nodePackages.typescript
-        pkgs.ffmpeg
-        pkgs.imagemagick
-        pkgs.git
-        pkgs.neofetch
-        pkgs.libwebp
-        pkgs.speedtest-cli
-        pkgs.wget
-        pkgs.yarn
-        pkgs.libuuid
-    ]
-    env = {
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.libuuid
-        ]
-    }
+deps = [
+pkgs.nodejs
+pkgs.nodePackages.typescript
+pkgs.ffmpeg
+pkgs.imagemagick
+pkgs.git
+pkgs.neofetch
+pkgs.libwebp
+pkgs.speedtest-cli
+pkgs.wget
+pkgs.yarn
+pkgs.libuuid
+]
+env = {
+LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+pkgs.libuuid
+]}
 } 
