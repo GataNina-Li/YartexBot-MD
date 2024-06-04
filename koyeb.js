196430785095b8b96b3f4027dcb82c86f48cc350
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-04 01:37:36 -0500

    ✨ Yartex

--- koyeb.js
+++ koyeb.js
@@ -5,11 +5,11 @@ sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-rel
 sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
 sudo dnf clean all -y
 
-RUN git clone https://github.com/AzamiJs/CuriosityBot-MD
+RUN git clone https://github.com/GataNina-Li/YartexBot-MD
 
-WORKDIR /root/CuriosityBot-MD
+WORKDIR /root/YartexBot-MD
 
-COPY ./root/CuriosityBot-MD
+COPY ./root/YartexBot-MD
 
 RUN yarn install
 
