f184125a1730e22256e254acb84cb86367465bfe
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-06-26 12:14:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1' 
 import './config.js' 
 import './store.js'
 import { createRequire } from 'module'
