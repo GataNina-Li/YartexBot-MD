0da4746dde8d94376a71ff96ed61122ce65df2d4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-07-04 10:54:49 -0500

    Update start-menu.js

--- plugins/start-menu.js
+++ plugins/start-menu.js
@@ -416,7 +416,8 @@ let vid = [
 //'https://telegra.ph/file/42628261be1a83c99907b.mp4',
 //'https://telegra.ph/file/979133eed9684f927804a.mp4'
 //'https://telegra.ph/file/141ad86e7786dbf6ea2dc.mp4' //bl
-'https://telegra.ph/file/7f9922e4d2cb2001b4682.mp4'
+//'https://telegra.ph/file/7f9922e4d2cb2001b4682.mp4'
+'https://telegra.ph/file/da87e85c1e17a51decea9.mp4' //bl
 ].getRandom()
 
 await conn.sendMessage(m.chat, { video: { url: vid }, gifPlayback: true, caption: menu, mentions: [m.sender], contextInfo: {
