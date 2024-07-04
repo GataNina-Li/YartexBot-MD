b804d1af13c0f5a762a3b0925ec3b3abcb0230ae
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-07-04 15:27:56 -0500

    Update start-editMenu.js

--- plugins/start-editMenu.js
+++ plugins/start-editMenu.js
@@ -1,7 +1,9 @@
 let handler = async (m, { conn, usedPrefix, command, isAdmin, isOwner, isROwner }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-let editMenu = global.db.data.chats[m.chat].editMenu
+let editMenuString = global.db.data.chats[m.chat].editMenu
+let editMenu = JSON.parse(editMenuString)
+
 let emojiItem = editMenu.find(item => item.hasOwnProperty('emoji'))
   
 if (m.isGroup && !isAdmin) {
