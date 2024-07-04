dc142313b91c165a307d4464456effe1b0ecddd3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-07-04 14:13:31 -0500

    Update start-editMenu.js

--- plugins/start-editMenu.js
+++ plugins/start-editMenu.js
@@ -8,41 +8,23 @@ return conn.reply(m.chat, '*No tiene permitido usar este comando, debe de ser ad
 return conn.reply(m.chat, '*No tiene permitido usar este comando, no eres dueño de este bot*')
 }
 
-let initialSections = [
-  { header: 'EMOJIS', title: "Actualmente: ", description: `Emojis en el menú completo`, id: `m` },
-  { header: "IMAGEN", title: "Actualmente: ", description: `Usar sólo imágenes para el menú completo`, id: `m` },
-  { header: "VÍDEO", title: "Actualmente: ", description: `Usar sólo vídeos para el menú completo`, id: `m` },
-  { header: "PRESENTACIÓN DINÁMICA", title: "Actualmente: ", description: `Usar Imágenes y Vídeos de forma aleatoria en el menú completo`, id: `m` },
-  { header: "SIMPLE", title: "Actualmente: ", description: `Omitir multimedia en el menú completo`, id: `m` },
-  { header: "MENCIÓN", title: "Actualmente: ", description: `Mencionar al usuario en el menú completo`, id: `m` }
-]
-
-let headers = initialSections.map(section => section.header);
-let titles = initialSections.map(section => section.title);
-let descriptions = initialSections.map(section => section.description);
-let ids = initialSections.map(section => section.id);
-
-
-let sections = [{
-  title: `CONFIGURACIÓN PARA EL MENÚ COMPLETO`,
-  rows: headers.map((header, index) => ({
-    title: titles[index],
-    description: descriptions[index],
-    rowId: ids[index]
-  }))
-}]
-
+let titulo = [ "EMOJIS", "IMAGEN", "VÍDEO", "PRESENTACIÓN DINÁMICA", "SIMPLE", "MENCIÓN" ]
+let nombre = [ "Actualmente: ", "Actualmente: ", "Actualmente: ", "Actualmente: ", "Actualmente: ", "Actualmente: " ]
+let descripción = [ "Emojis en el menú completo", "Usar sólo imágenes para el menú completo", "Usar sólo vídeos para el menú completo", "Usar Imágenes y Vídeos de forma aleatoria en el menú completo", "Omitir multimedia en el menú completo", "Mencionar al usuario en el menú completo" ]
+let comando = [ "m", "m", "m", "m", "m", "m" ]
+let sections = Object.keys(titulo, nombre, descripción, comando).map((v, index) => ({ title: `${titulo[v]}`,
+rows: [{ header: `${1 + index}. ${titulo[v]}`, title: `${nombre[v]}`, description: `${descripción[v]}`, id: comando[v], }], }))
 
 const listMessage = {
-  text: `*Editar menú*`,
-  footer: wm,
-  title: null,
-  buttonText: `AJUSTAR`,
-  sections
+text: `*Editar menú*`,
+footer: wm,
+title: null,
+buttonText: `AJUSTAR`,
+sections
 }
 
 await conn.sendList(m.chat, listMessage.text, listMessage.footer, listMessage.buttonText, sections, null, null, fkontak)
-
+  
 }
 handler.command = /^(editarmenu)$/i
 export default handler
