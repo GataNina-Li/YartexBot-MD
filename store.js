bd081d453998d2484f7bee066b16d1a9fced004c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-07-04 18:23:43 -0500

    Update store.js

--- store.js
+++ store.js
@@ -19,9 +19,15 @@ global.imagen4 = fs.readFileSync('./storage/logos/Menu4.jpg')
 
 var img = 'https://telegra.ph/file/9a9a4851b638c90ebb214.jpg'; var img1 = 'https://telegra.ph/file/c248943164f6a3d28088c.jpg'; var img2 = 'https://telegra.ph/file/f14102fec3b4104e41e98.jpg'; var img3 = 'https://telegra.ph/file/5646b7540b22e8a00615e.jpg'; var img4 = 'https://telegra.ph/file/e6c11fc60a74a77789f56.jpg'; var img5 = 'https://telegra.ph/file/19af932324e24ceb896cd.jpg'
 var img6 = 'https://telegra.ph/file/e1c9c47c389b17de97a03.jpg'; var img7 = 'https://telegra.ph/file/caa791fa59111b4b4311a.jpg'; var img8 = 'https://telegra.ph/file/76fff527eeaa2b61099ca.jpg'; var img9 = 'https://telegra.ph/file/3dad91ec4eea23ea93f3d.jpg'; var img10 = 'https://telegra.ph/file/4443d4eb77531313b1f9b.jpg'
-
 global.ImgAll = [img, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10]
 
+let botImg1 = 'https://telegra.ph/file/75b8ee5025fea7ddddb41.jpg'
+let botImg2 = 'https://telegra.ph/file/95dc64c1182fd7985c838.jpg'
+let botImg3 = 'https://telegra.ph/file/e46ef0f31bc2bd030ce6b.jpg'
+let botImg4 = 'https://telegra.ph/file/5895dd5bee8c8c24a0966.jpg'
+let botImg5 = 'https://telegra.ph/file/96c870b2df851f15b000f.jpg'
+let yartexImg = [botImg1, botImg2, botImg3, botImg4, botImg5].getRamdon()
+  
 //Enlaces
 global.yt = 'https://youtube.com/@gatadios'
 
