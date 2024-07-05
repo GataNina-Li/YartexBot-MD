c85d03b4f1b1534bc533049a838c9d00014279a0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-07-04 22:57:12 -0500

    Update start-editMenu.js

--- plugins/start-editMenu.js
+++ plugins/start-editMenu.js
@@ -195,7 +195,7 @@ conn.reply(m.chat, mensajeConfirmacion, m)
 return conn.reply(m.chat, hasOwnPropertyError, m)
 }}
 
-}
+}}
 handler.command = /^(editarmenu|editmenu|editaremoji01|editarimagen02|editarvideo03|editarvi04|editarsimple05|editarmencion06|editardividir07|editarverificado08|editarmenu09)$/i
 export default handler
 
