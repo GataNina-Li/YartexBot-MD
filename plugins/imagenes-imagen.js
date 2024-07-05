e7e095d571d9fddaf4951664236ceedf8f307bcf
AdrianOficial <120221652+The-DiosBot-MD@users.noreply.github.com>
2024-07-04 22:15:31 -0400

    Update imagenes-imagen.js

--- plugins/imagenes-imagen.js
+++ plugins/imagenes-imagen.js
@@ -5,7 +5,7 @@ var handler = async (m, { conn, text, usedPrefix, command }) => {
 const prohibited = ['se men', 'hen tai', 'se xo', 'te tas', 'cu lo', 'c ulo', 'cul o', 'ntr', 'rule34', 'rule', 'caca', 'polla', 'porno', 'porn', 'gore', 'cum', 'semen', 'puta', 'puto', 'culo', 'putita', 'putito', 'pussy', 'hentai', 'pene', 'coño', 'asesinato', 'zoofilia', 'mia khalifa', 'desnudo', 'desnuda', 'cuca', 'chocha', 'muertos', 'pornhub', 'xnxx', 'xvideos', 'teta', 'vagina', 'marsha may', 'misha cross', 'sexmex', 'furry', 'furro', 'furra', 'xxx', 'rule34', 'panocha', 'pedofilia', 'necrofilia', 'pinga', 'horny', 'ass', 'nude', 'popo', 'nsfw', 'femdom', 'futanari', 'erofeet', 'sexo', 'sex', 'yuri', 'ero', 'ecchi', 'blowjob', 'anal', 'ahegao', 'pija', 'verga', 'trasero', 'violation', 'violacion', 'bdsm', 'cachonda', '+18', 'cp', 'mia marin', 'lana rhoades', 'cepesito', 'hot', 'buceta', 'xxx']
 if (prohibited.some(word => m.text.toLowerCase().includes(word))) return conn.reply(m.chat, '🚩 *No dare resultado a tu solicitud*', m, fake, )
 
-if (!text) return conn.reply(m.chat, `🎌 *Debera ingresar un texto*\n\nEjemplo, !${command} WhatsApp Bot`, m, fake, )
+if (!text) return conn.reply(m.chat, `🎌 *Debera ingresar un texto para ello*\n\nEjemplo, !${command} WhatsApp Bot`, m, fake, )
 m.react(done)
 
 const res = await googleImage(text)
