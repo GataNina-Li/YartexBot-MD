4e29fc9beb598383c7aff8ce1dd633a91d50f0fc
Zam <141449338+AzamiJs@users.noreply.github.com>
2024-07-04 19:38:49 -0600

    ✨ Yartex

--- plugins/config-antitrabas.js
+++ plugins/config-antitrabas.js
@@ -14,9 +14,9 @@ if (chat.antiTraba && m.text.length > 5000) { //Cantidad máxima de caracteres a
 if (isAdmin) return conn.sendMessage(m.chat, { text: `🚩 *El administrador @${m.sender.split("@")[0]} acaba de enviar un texto que contiene muchos caracteres*`, mentions: [m.sender] }, { quoted: fkontak })
 await conn.sendMessage(m.chat, `🚩 *Se detecto un mensaje que contiene muchos caracteres*\n`, `${isBotAdmin ? '' : 'No soy administrador, no puedo hacer nada :/'}`, m)
 if (isBotAdmin && bot.restrict) {
-conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+//conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 setTimeout(() => { 
-conn.sendMessage(m.chat, { text: `🎌 *Marcar el chat como leido*\n${"\n".repeat(400)}\n=> El número : wa.me/${m.sender.split("@")[0]}\n=> Alias : ${name}\nAcaba de enviar un texto que contiene muchos caracteres que puede ocasionar fallos en los dispositivos`, mentions: [m.sender] }, { quoted: fkontak })
+//conn.sendMessage(m.chat, { text: `🎌 *Marcar el chat como leido*\n${"\n".repeat(400)}\n=> El número : wa.me/${m.sender.split("@")[0]}\n=> Alias : ${name}\nAcaba de enviar un texto que contiene muchos caracteres que puede ocasionar fallos en los dispositivos`, mentions: [m.sender] }, { quoted: fkontak })
 }, 0)
 setTimeout(() => { 
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
