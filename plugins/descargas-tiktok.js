efdbf8875c10ccd34205bd4d80d7098d2a78b6f1
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-07-04 20:44:38 -0400

    Update descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -1,53 +1,54 @@
-import fg from 'api-dylux'
-import { tiktokdl } from '@bochilteam/scraper'
-
-var handler = async (m, { conn, text, args, usedPrefix, command}) => {
-
-if (!args[0]) return conn.reply(m.chat, `🎌 *Ingrese un enlace de tiktok*\n\nEjemplo, !${command} https://vm.tiktok.com/ZMYG92bUh/`, m, fake, )
-if (!args[0].match(/tiktok/gi)) return conn.reply(m.chat, `🚩 *Verifica que el enlace sea correcto*`, m, fake, )
-
-m.react(rwait)
-
-const { key } = await conn.sendMessage(m.chat, {text: `${wait}`}, {quoted: m})
-await delay(1000 * 1)
-await conn.sendMessage(m.chat, {text: `${waitt}`, edit: key})
-await delay(1000 * 1)
-await conn.sendMessage(m.chat, {text: `${waittt}`, edit: key})
-await delay(1000 * 1)
-await conn.sendMessage(m.chat, {text: `${waitttt}`, edit: key})
-
-try {
-let p = await fg.tiktok(args[0])
-let te = `*Nombre:* ${p.nickname}
-*Usuario:* ${p.unique_id}
-*Duración:* ${p.duration}
-*Descripción:* ${p.description}`
-conn.sendFile(m.chat, p.play, 'tiktok.mp4', te, m)
-m.react(done)
-} catch {
-
-try {
-
-const { author: { nickname }, video, description } = await tiktokdl(args[0])
-const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-
-m.react(error)
-if (!url) return conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m, fake, )
-conn.sendFile(m.chat, url, 'fb.mp4', `*Nombre:* ${nickname}\n*Descripción:* ${description}`, m)
-m.react(done)
-} catch {
-m.react(error)
-conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m, fake, )
-}}
-    
-}
-handler.help = ['tiktok']
-handler.tags = ['descargas']
-handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i
-
-handler.limit = true
-handler.register = true
-
-export default handler
-
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+import fg from 'api-dylux';
+ import axios from 'axios';
+ import cheerio from 'cheerio';
+ import { tiktok } from "@xct007/frieren-scraper"; 
+ import { generateWAMessageFromContent } from '@whiskeysockets/baileys';
+ import { tiktokdl } from '@bochilteam/scraper';
+ let handler = async (m, { conn, text, args, usedPrefix, command}) => { 
+ if (!text) throw `*✘error✘*\n\n_. ᩭ✎Use el comandó correctamente_\n\n_Ejemplo : ${usedPrefix + command} https://vm.tiktok.com/kandndbwldnig/🍁_` 
+ if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) throw `*✘error✘* _. ᩭ✎Use el comandó correctamente_\n\n_Ejemplo : ${usedPrefix + command} https://vm.tiktok.com/nandlwmso/🍁_` 
+ let texto = `_💜 @${m.sender.split`@`[0]}  ᩭ✎Enviando Video, espere un momento...._` 
+ try { 
+  conn.sendMessage(m.chat, { text: texto, mentions: [m.sender]}, {quoted: m})
+ await conn.relayMessage(m.chat, prep.message, { messageId: prep.key.id, mentions: [m.sender] })     
+ const dataF = await tiktok.v1(args[0]) 
+ //let desc1 =  `*USUARIO:* ${dataF.nickname || 'Indefinido'}` 
+ let desc1 =  `_💜  ᩭ✎Tiktok sin marca de agua descargado con éxito YartexBot-MD_` 
+ await conn.sendMessage(m.chat, { video: { url: dataF.play }, caption: desc1 }, { quoted: m })   
+ } catch (e1) { 
+ try { 
+ const tTiktok = await tiktokdlF(args[0]) 
+ //let desc2 = `🔗 *Url:* ${tTiktok.video}`     
+ let desc2 =  `_💜  ᩭ✎Tiktok sin marca de agua descargado con éxito YartexBot-MD_` 
+ await conn.sendMessage(m.chat, { video: { url: tTiktok.video }, caption: desc2 }, { quoted: m })       
+ } catch (e2) { 
+ try { 
+ let p = await fg.tiktok(args[0])  
+ //let te = `*USUARIO:* ${p.author || 'Indefinido'}` 
+ let te =  `_❌  ᩭ✎Tiktok sin marca de agua descargado con éxito YartexBot-MD_` 
+ await conn.sendMessage(m.chat, { video: { url: p.nowm}, caption: te }, { quoted: m })   
+ } catch (e3) { 
+ try {  
+ const { author: { nickname }, video, description } = await tiktokdl(args[0]) 
+ const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd 
+ //let cap = `*USUARIO:* ${nickname || 'Indefinido'}` 
+ let cap =  `_💜  ᩭ✎Tiktok sin marca de agua descargado con éxito YartexBot-MD_` 
+ await conn.sendMessage(m.chat, { video: { url: url}, caption: cap }, { quoted: m })   
+ } catch { 
+ throw `_❌error❌ _Vuelve a intentarlo_` 
+ }}}}} 
+ handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm|tt|ttnowm|tiktokaudio)$/i 
+ export default handler 
+  
+ async function tiktokdlF(url) { 
+ if (!/tiktok/.test(url)) return 'Enlace incorrecto'; 
+ const gettoken = await axios.get("https://tikdown.org/id"); 
+ const $ = cheerio.load(gettoken.data); 
+ const token = $("#download-form > input[type=hidden]:nth-child(2)").attr( "value" ); 
+ const param = { url: url, _token: token }; 
+ const { data } = await axios.request("https://tikdown.org/getAjax?", { method: "post", data: new URLSearchParams(Object.entries(param)), headers: { "content-type": "application/x-www-form-urlencoded; charset=UTF-8", "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36" }, }); 
+ var getdata = cheerio.load(data.html); 
+ if (data.status) { 
+ return { status: true, thumbnail: getdata("img").attr("src"), video: getdata("div.download-links > div:nth-child(1) > a").attr("href"), audio: getdata("div.download-links > div:nth-child(2) > a").attr("href"), }} else 
+ return { status: false }}
+      
