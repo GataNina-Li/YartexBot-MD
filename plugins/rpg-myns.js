3495de61307a2ea93678320856379761b66237ee
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-07-27 17:57:15 -0500

    Update rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -4,15 +4,47 @@ var handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
 //conn.sendButton(m.chat, '⬇️ *Su número de serie asignado es:* ⬇️', wm2, null, [], ['Copiar Mensaje', sn], null, m)
 //conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', '⬇️ *Su número de serie asignado es:* ⬇️', 'status@broadcast')
-const messages = [[ 
-'⬇️ *Su número de serie asignado es:* ⬇️', 
-wm2,
-null,
-null,
+const sections = [{
+title: `Título de la sección`,
+rows: [
+{ header: 'Encabezado1', title: "Título1", description: 'Descripción1', id: usedPrefix + "menu" }, 
+{ header: 'Encabezado2', title: "Título2", description: 'Descripción2', id: "Id2" }, 
+{ header: 'Encabezado3', title: "Título3", description: 'Descripción3', id: "Id3" }, 
+{ header: 'Encabezado4', title: "Título4", description: 'Descripción4', id: "Id4" }, 
+]},]  
+const messages = [[ // CARRUSEL 1
+'Descripción de Carrusel 1', 
+'Footer de Carrusel 1',
+'https://telegra.ph/file/24b24c495b5384b218b2f.jpg',
+[['Botón1', usedPrefix + 'menu'], ['Botón2', 'Id2'] /* etc... */],
 [['Texto para copiar 1'], ['Texto para copiar 2'] /* etc... */],
-null,
-null
-]]
+[['Enlace1', canal2], ['Enlace2', 'https://example.com/link2'] /* etc... */],
+[['Botón Lista 1', sections], ['Botón Lista 2', sections] /* etc... */]
+], [ // CARRUSEL 2
+'Descripción de Carrusel 2',
+'Footer de Carrusel 2',
+'https://telegra.ph/file/e9239fa926d3a2ef48df2.jpg',
+[['Botón1', 'Id1'], ['Botón2', 'Id2']],
+[['Texto para copiar 1'], ['Texto para copiar 2']],
+[['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
+[['Botón Lista 1', sections], ['Botón Lista 2', sections]]
+], [ // CARRUSEL 3
+'Descripción de Carrusel 3',
+'Footer de Carrusel 3',
+'https://telegra.ph/file/ec725de5925f6fb4d5647.jpg',
+[['Botón1', 'Id1'], ['Botón2', 'Id2']],
+[['Texto para copiar 1'], ['Texto para copiar 2']],
+[['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
+[['Botón Lista 1', sections], ['Botón Lista 2', sections]]
+], [ // CARRUSEL 4
+'Descripción de Carrusel 4',
+'Footer de Carrusel 4',
+'https://telegra.ph/file/7acad0975febb71446da5.jpg',
+[['Botón1', 'Id1'], ['Botón2', 'Id2']],
+[['Texto para copiar 1'], ['Texto para copiar 2']],
+[['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
+[['Botón Lista 1', sections], ['Botón Lista 2', sections]]
+]] /* etc... */
 await conn.sendCarousel(m.chat, 'Texto', 'Footer', 'Titulo de Carrusel', messages, m) 
 }
 handler.command = /^(myns|ceksn|ns|numserie)$/i
