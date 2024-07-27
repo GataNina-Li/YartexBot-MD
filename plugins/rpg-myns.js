a16d30ab0b356a641057786eeb377003509bb574
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-07-27 17:02:49 -0500

    Update rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -1,16 +1,11 @@
 import { createHash } from 'crypto'
 
 var handler = async function (m, { conn, text, usedPrefix }) {
-
-let sn = createHash('md5').update(m.sender).digest('hex')
-
-conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', '⬇️ *Su número de serie asignado es:* ⬇️', 'status@broadcast')
+let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
+conn.sendButton(m.chat, '⬇️ *Su número de serie asignado es:* ⬇️', wm2, null, null, sn, null, m)
+//conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', '⬇️ *Su número de serie asignado es:* ⬇️', 'status@broadcast')
 
 }
-handler.help = ['myns']
-handler.tags = ['rg']
-handler.command = /^(myns|ceksn|ns)$/i
-
+handler.command = /^(myns|ceksn|ns|numserie)$/i
 handler.register = true
-
 export default handler
