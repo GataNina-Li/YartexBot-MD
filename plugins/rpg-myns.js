8322b02d8699b8f91840386d9d3c32c9d34c7b49
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-07-27 17:31:24 -0500

    Update rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -2,7 +2,7 @@ import { createHash } from 'crypto'
 
 var handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
-conn.sendButton(m.chat, '⬇️ *Su número de serie asignado es:* ⬇️', wm2, null, null, ['Copiar Mensaje', sn], null, m)
+conn.sendButton(m.chat, '⬇️ *Su número de serie asignado es:* ⬇️', wm2, null, [], ['Copiar Mensaje', sn], null, m)
 //conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', '⬇️ *Su número de serie asignado es:* ⬇️', 'status@broadcast')
 
 }
