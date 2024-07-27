94b58e3b9c6db59272a418ce73563618a1af421b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-07-27 16:46:59 -0500

    Update start-delregistro.js

--- plugins/start-delregistro.js
+++ plugins/start-delregistro.js
@@ -1,8 +1,11 @@
 let handler = async (m, { conn, text, usedPrefix }) => {
 let user = global.db.data.users[m.sender]
+let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)	 
+if (!text) return conn.reply(m.chat, `🔖 *Falta su número de serie.*\n\n> 🩷 Puede usar el comando *${usedPrefix}numserie* para obtener su número de serie.`, m)
+if (text !== sn) return conn.reply(m.chat, `💔 *Número de serie incorrecto.*\n\n> ✨ Puede usar el comando *${usedPrefix}numserie* para obtener su número de serie.`, m)
 user.registered = false
 m.react('✅') 
-conn.sendMessage(m.chat, { text: `${dis}*Registro eliminado de ${packname}*\n\n> _Si desea hacer un nuevo registro use el comando_\n*${usedPrefix}verificar* \`nombre.edad\``, ...fake }, { quoted: m })
+conn.sendMessage(m.chat, { text: `${dis}*Registro eliminado de ${packname}*\n\n> _Si desea hacer un nuevo registro use el comando._\n*${usedPrefix}verificar* \`nombre.edad\``, ...fake }, { quoted: m })
 }
 handler.command = /^(unreg|delregistro)$/i
 handler.register = true
