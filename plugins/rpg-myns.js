96e36b90f6a27074e50d44f16872663a24c8ae33
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-07-27 18:12:30 -0500

    Update rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -2,16 +2,7 @@ import { createHash } from 'crypto'
 
 var handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
-//conn.sendButton(m.chat, '⬇️ *Su número de serie asignado es:* ⬇️', wm2, null, [], ['Copiar Mensaje', sn], null, m)
-const messages = [[ 
-'⬇️ *Su número de serie asignado es:* ⬇️',
-wm2,
-null,
-[],
-[['Copiar ID', sn]],
-[]
-]] 
-await conn.sendCarousel(m.chat, 'Texto', 'Footer', 'Titulo de Carrusel', messages, m)  
+conn.sendButton(m.chat, '⬇️ *Su número de serie asignado es:* ⬇️', wm2, null, [], sn, null, m)  
 }
 handler.command = /^(myns|ceksn|ns|numserie)$/i
 handler.register = true
