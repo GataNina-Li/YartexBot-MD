4427e22c5b24d61f54449c5cfe4db1598612a122
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-07-27 16:33:29 -0500

    Update start-delregistro.js

--- plugins/start-delregistro.js
+++ plugins/start-delregistro.js
@@ -1,6 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix }) => {
 let user = global.db.data.users[m.sender]
 user.registered = false
+m.react('✅') 
 conn.sendMessage(m.chat, { text: `${dis}*Registro eliminado de ${packname}*\n\n> _Si desea hacer un nuevo registro use el comando_\n*${usedPrefix}verificar* \`nombre.edad\``, ...fake }, { quoted: m })
 }
 handler.command = /^(unreg|delregistro)$/i
