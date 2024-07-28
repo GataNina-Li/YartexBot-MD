5082fb8db577dc35da8538364e97bb318a7839d5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-07-27 19:37:34 -0500

    Update rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -2,7 +2,7 @@ import { createHash } from 'crypto'
 
 var handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
-conn.sendButton(m.chat, `${dis}_Copie y pegue para eliminar su registro._`, wm2, null, null, [['ℕú𝕞𝕖𝕣𝕠 𝕕𝕖 𝕤𝕖𝕣𝕚𝕖 🐋', `${usedPrefix}delregistro ` + sn]], null, null, m)  
+conn.sendButton(m.chat, `${dis}_Copie y pegue para eliminar su registro._`, wm2, null, null, [['ℕú𝕞𝕖𝕣𝕠 𝕕𝕖 𝕤𝕖𝕣𝕚𝕖 🦄', `${usedPrefix}delregistro ` + sn]], null, null, m)  
 }
 handler.command = /^(myns|ceksn|ns|numserie)$/i
 handler.register = true
