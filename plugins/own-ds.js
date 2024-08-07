fca50da6263506bb38f6e969ba1c8e2094428893
Zam <141449338+AzamiJs@users.noreply.github.com>
2024-08-07 12:33:21 -0600

    Update own-ds.js

--- plugins/own-ds.js
+++ plugins/own-ds.js
@@ -11,7 +11,7 @@ return conn.reply(m.chat, '🚩 *Utiliza este comando directamente en el número
 await conn.reply(m.chat, '🚩 *Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...*', m, fake, )
 m.react(rwait)
 
-let sessionPath = './sessions/'
+let sessionPath = './YartexBotSession/'
 
 try {
 
