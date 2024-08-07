a59bb38db6d35cb62f602403d07a616439815034
Zam <141449338+AzamiJs@users.noreply.github.com>
2024-08-07 12:44:32 -0600

    Update ai-bard.js

--- plugins/ai-bard.js
+++ plugins/ai-bard.js
@@ -7,7 +7,7 @@ if (!text) return conn.reply(m.chat, `🎌 *Ingresé una petición*\n\nEjemplo, 
 try {
 
 conn.sendPresenceUpdate('composing', m.chat)
-var apii = await fetch(`https://aemt.me/bard?text=${text}`)
+var apii = await fetch(`https://aemt.me/gemini?text=${text}`)
 var res = await apii.json()
 await m.reply(res.result)
 
