4e1c8c2d91f4f17ea727803fd8d00575b62a7098
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-08-09 15:35:56 -0500

    Update bot-menu.js

--- plugins/bot-menu.js
+++ plugins/bot-menu.js
@@ -56,35 +56,35 @@ rows: [
 const messages = [[ // CARRUSEL 1
 'Descripción de Carrusel 1', 
 'Footer de Carrusel 1',
-random1(),
-[['Botón1', usedPrefix + 'menu'], ['Botón2', 'Id2'] /* etc... */],
+yartexImg.getRandom(),
+[['Botón1', usedPrefix + 'menu'] /* etc... */],
 [[]],
-[['Enlace1', ''], ['Enlace2', 'https://example.com/link2'] /* etc... */],
+[[]],
 [[]]
 ], [ // CARRUSEL 2
 'Descripción de Carrusel 2',
 'Footer de Carrusel 2',
 random1(),
-[['Botón1', 'Id1'], ['Botón2', 'Id2']],
-[['Texto para copiar 1'], ['Texto para copiar 2']],
-[['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
-[['Botón Lista 1', sections], ['Botón Lista 2', sections]]
+[['Botón1', usedPrefix + 'menu'] /* etc... */],
+[[]],
+[[]],
+[[]]
 ], [ // CARRUSEL 3
 'Descripción de Carrusel 3',
 'Footer de Carrusel 3',
 random1(),
-[['Botón1', 'Id1'], ['Botón2', 'Id2']],
-[['Texto para copiar 1'], ['Texto para copiar 2']],
-[['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
-[['Botón Lista 1', sections], ['Botón Lista 2', sections]]
+[['Botón1', usedPrefix + 'menu'] /* etc... */],
+[[]],
+[[]],
+[[]]
 ], [ // CARRUSEL 4
 'Descripción de Carrusel 4',
 'Footer de Carrusel 4',
 random1(),
-[['Botón1', 'Id1'], ['Botón2', 'Id2']],
-[['Texto para copiar 1'], ['Texto para copiar 2']],
-[['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
-[['Botón Lista 1', sections], ['Botón Lista 2', sections]]
+[['Botón1', usedPrefix + 'menu'] /* etc... */],
+[[]],
+[[]],
+[[]]
 ]] /* etc... */
 await conn.sendCarousel(m.chat, 'Texto', 'Footer', 'Titulo de Carrusel', messages, m)     
   
