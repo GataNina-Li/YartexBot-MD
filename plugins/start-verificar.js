0b4d968c822cd23a46c3f9f0fee44a2bf63b2060
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-10-25 02:16:26 -0500

    Update start-verificar.js

--- plugins/start-verificar.js
+++ plugins/start-verificar.js
@@ -99,14 +99,6 @@ user.registered ? '' : conn.sendMessage(m.sender, { delete: msg.key })
 }, 30000)
 }
 m.isGroup ? await conn.reply(m.chat, "📨 El formulario de verificación se ha enviado a tu chat privado. ¡Revísalo!", m) : ''
-} catch (e) {
-user.name = ""
-user.age = 0
-user.registered = false
-user.OTP = "" 
-console.error(e)
-await conn.reply(m.chat, "⚠️ Ocurrió un error al enviar el formulario de verificación. Intenta de nuevo más tarde.", m)
-}}
 handler.before = async function (m, { conn }) {
 user = global.db.data.users[m.sender]
 let isVerified = m.quoted && m.quoted.id == msg.key.id && m.text == otp
@@ -125,6 +117,14 @@ await conn.sendMessage(m.chat, { image: { url: pp }, caption: `*║⫘⫘⫘⫘�
 *║* 💠 *Edad* ${age} años
 *║* 💠 *Número de serie* \`${sn.slice(0, 6)}\`
 *║⫘⫘⫘⫘⫘⫘✨*`, mentions: [m.sender], ...fake }, { quoted: m })
+}}
+} catch (e) {
+user.name = ""
+user.age = 0
+user.registered = false
+user.OTP = "" 
+console.error(e)
+await conn.reply(m.chat, "⚠️ Ocurrió un error al enviar el formulario de verificación. Intenta de nuevo más tarde.", m)
 }}
 handler.command = /^(ver(ify|ificar)|reg(istrar)?)$/i
 export default handler
