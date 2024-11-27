a5d0d208853969ca526bdaa1aec772c9de5d7d3a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-11-27 10:53:20 -0500

    ✨ Yartex

--- plugins/tools-wa-story.js
+++ plugins/tools-wa-story.js
@@ -1,4 +1,4 @@
-/*const handler = async (m, { conn }) => {
+const handler = async (m, { conn }) => {
 if ("status@broadcast" != m.quoted?.chat) return conn.reply(m.chat, "*Por favor, responde a un estado de WhatsApp para descargar su contenido*", m) 
 
 try {
@@ -10,51 +10,4 @@ await conn.reply(m.chat, m.quoted?.text, m)
 }}
 
 handler.command = /^((sw|status|descargar|historia)(dl|download)|(dl|download)(sw|status|descargar|historia))$/i
-export default handler*/
-
-const handler = async (m, { conn, store }) => {
-  
-  if ("status@broadcast" != m.quoted?.chat) {
-    return conn.reply(m.chat, "*Por favor, responde a un estado de WhatsApp para descargar su contenido*", m);
-  }
-
-  try {
-    
-    const stories = store.messages["status@broadcast"] || [];
-
-   
-    const userId = m.quoted?.sender || m.chat; // ID del usuario que respondió al estado
-    const userStories = stories.filter((msg) => msg.key.participant === userId);
-
-   
-    if (userStories.length === 0) {
-      return conn.reply(m.chat, "No se encontraron historias para este usuario.", m);
-    }
-
-   
-    for (let story of userStories) {
-      if (story.message.imageMessage || story.message.videoMessage) {
-        const buffer = await story.download()
-
-        
-        await conn.sendFile(m.chat, buffer, "", "Historia descargada", null, false, {
-          quoted: m,
-        });
-      }
-    }
-
-    
-    await conn.reply(m.chat, `Se descargaron ${userStories.length} historias del usuario.`, m);
-  } catch (e) {
-    console.log(e)
-    await conn.reply(m.chat, "Hubo un error al descargar las historias.", m);
-  }
-};
-
-
-handler.command = /^(allhistorias)$/i;
-export default handler;
-
-
-
-
+export default handler
