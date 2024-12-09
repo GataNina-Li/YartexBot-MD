c681159f7e08d42e3dd5efd80ed815cbe7c5c18a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-12-09 13:46:07 -0500

    ✨ Yartex

--- config.js
+++ config.js
@@ -87,6 +87,9 @@ let botVid10 = 'https://telegra.ph/file/3a3e1bf3825add3679cb9.mp4' //bl
 let botVid11 = 'https://telegra.ph/file/a3485e1c3b115ba186b45.mp4' //bl
 let botVid12 = 'https://telegra.ph/file/1c5f62544f7278899a9be.mp4' //bl
 global.yartexVid = [botVid1, botVid2, botVid3, botVid4, botVid5, botVid6, botVid7, botVid8, botVid9, botVid10, botVid11, botVid12]
+
+// versión de navidad
+global.yartexNavidad = ['https://qu.ax/UsZaq.png', 'https://qu.ax/rGvHH.png', 'https://qu.ax/vyHLe.png']
   
 //Enlaces
 global.yt = 'https://youtube.com/@gatadios'
