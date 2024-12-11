c0824f9292dcb51d8251afd470ddce0f2b4088d6
edar123 <78393307+edar123@users.noreply.github.com>
2024-12-10 23:07:24 -0600

    Update queso.js

--- plugins/queso.js
+++ plugins/queso.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  conn.reply(m.chat, `🧀zo`, m, fake)
+  conn.reply(m.chat, `🧀zo`, m, null, fake)
 }
 
 handler.customPrefix = /^(Que|que|qe|ke|Qe|k|Ke|Kee|Quee)$/i
