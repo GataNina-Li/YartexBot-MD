db077962faaba13b8dfb4f40710e492e1e1275d0
edar123 <78393307+edar123@users.noreply.github.com>
2024-12-11 14:53:55 -0600

    Update bot-ping.js

--- plugins/bot-ping.js
+++ plugins/bot-ping.js
@@ -33,7 +33,7 @@ let texto = `*🚀 Velocidad*
 *💻 Servidor*
 • *Ram:* ${format(totalmem() - freemem())} / ${format(totalmem())}`.trim()
 
-conn.sendMessage(m.chat, { text: texto, contextInfo: { externalAdReply: { title: '', body: 'CURIOSITY - BOT - MD', thumbnailUrl: 'https://telegra.ph/file/6cbf9148b572711e9b000.jpg', sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
+conn.sendMessage(m.chat, { text: texto, contextInfo: { externalAdReply: { title: '', body: `${wm}`, thumbnailUrl: 'https://telegra.ph/file/6cbf9148b572711e9b000.jpg', sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
 
 }
 handler.help = ['ping']
