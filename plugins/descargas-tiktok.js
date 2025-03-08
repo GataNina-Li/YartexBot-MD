29e97edacd5ef10cccdc860a86d2f334a31e0fd6
OfcKing <argumedodie@gmail.com>
2025-03-07 20:55:21 -0500

    ✨ Yartex

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -1,50 +1,34 @@
-import axios from "axios"
-import fg from 'api-dylux';
-import cheerio from 'cheerio';
-import { Tiktok } from '../lib/tiktok.js';
-let handler = async (m, { conn, text, args, usedPrefix, command}) => {
-if (!text) throw `✨ Ingrese un enlace de tiktok\nEjemplo: *${usedPrefix + command} https://vm.tiktok.com/ZM6n8r8Dk*`
-if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) throw `🚩 El enlace enviado no es un enlace de tiktok.`  
-try {
-m.react(rwait)
-const data = await Tiktok(args)
-conn.sendMessage(m.chat, {video: {url: data.nowm}, caption: `🍧 Usuario: ${data.author}`}, {quoted: m})
-} catch {
-try {
-const tTiktok = await tiktokdlF(args[0]);
-await conn.sendMessage(m.chat, {video: {url: tTiktok.video}, caption: `${wm}`}, {quoted: m});            
-} catch {
-try {
-const response = await axios.get(`https://api.dorratz.com/v2/tiktok-dl?url=${args}`);
-if (response.data.status && response.data.data) {
-const videoData = response.data.data.media;
-const videoUrl = videoData.org; 
-await conn.sendMessage(m.chat, { video: { url: videoUrl }, caption: `🍧 Usuario:\n${response.data.data.author.nickname}\n${wm}` }, { quoted: m });
-}} catch {
-try {
-const p = await fg.tiktok(args[0]);
-await conn.sendMessage(m.chat, {video: {url: p.nowm}, caption: `${wm}`}, {quoted: m}); 
-} catch (e) {
-console.log(e) 
-m.react(`❌`)         
-}}}}}
-handler.help = ['tiktok']
-handler.tags = ['dl']
-handler.command = /^(tt|tiktok)(dl|nowm)?$/i
-handler.limit = 2
-export default handler
+const handler = async (m, { conn, text }) => {
 
-async function tiktokdlF(url) {
-  if (!/tiktok/.test(url)) return `*Ejemplo:* _${usedPrefix + command} https://vm.tiktok.com/ZM686Q4ER/_`;
-  const gettoken = await axios.get('https://tikdown.org/id');
-  const $ = cheerio.load(gettoken.data);
-  const token = $('#download-form > input[type=hidden]:nth-child(2)').attr( 'value' );
-  const param = {url: url, _token: token};
-  const {data} = await axios.request('https://tikdown.org/getAjax?', {method: 'post', data: new URLSearchParams(Object.entries(param)), headers: {'content-type': 'application/x-www-form-urlencoded; charset=UTF-8', 'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'}});
-  const getdata = cheerio.load(data.html);
-  if (data.status) {
-    return {status: true, thumbnail: getdata('img').attr('src'), video: getdata('div.download-links > div:nth-child(1) > a').attr('href'), audio: getdata('div.download-links > div:nth-child(2) > a').attr('href')};
-  } else {
-    return {status: false};
-  }
+if (!text) return conn.reply(m.chat, `✨ Ingrese un enlace de tiktok\nEjemplo: *${usedPrefix + command} https://vm.tiktok.com/ZM6n8r8Dk*`, m);
+m.react(rwait)
+const isUrl = /https:\/\/(vt\.tiktok\.com|vm\.tiktok\.com)/gi.test(text);
+if (!isUrl) {
+return conn.reply(m.chat, '🚩 El enlace enviado no es un enlace de tiktok.', m);
 }
+const apiUrl = `https://delirius-apiofc.vercel.app/download/tiktok?url=${text}`;
+try {
+const res = await fetch(apiUrl);
+if (!res.ok) throw new Error(`El servidor respondió con el estado ${res.status}`);
+const json = await res.json();
+if (!json || !json.data) return conn.reply(m.chat, '❌ El enlace proporcionado no es válido o no contiene datos de video. Por favor, intenta con otro enlace.', m);
+const videoData = json.data.meta.media.find(media => media.type === 'video');
+if (!videoData) return conn.reply(m.chat, '❌ El enlace proporcionado no parece ser un video de TikTok.', m);
+const { title, author, like, comment, repro, share, download } = json.data;
+const videoUrl = videoData.hd;
+const caption = createCaption(title, author?.nickname, like, comment, repro, share, download);
+await conn.sendMessage(m.chat, { video: { url: videoUrl }, caption }, { quoted: m });
+} catch (e) {
+conn.reply(m.chat, `❗ *Error:* ${e.message}`, m);
+console.error('Error:', e);
+}};
+function createCaption(creator) {
+return `🍧 *Usuario* ${creator || 'No disponible'}\n\n${wm}`;
+}
+
+handler.help = ['tiktok'];
+handler.tags = ['dl'];
+handler.command = /^(tt|tiktok)(dl|nowm)?$/i;
+handler.limit = 2;
+
+export default handler;
