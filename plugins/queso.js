a858dc91e1ccd7ff448753b08db73c9a86e12d3c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-04-15 21:58:03 -0500

    Delete plugins directory

--- plugins/queso.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-
-  conn.reply(m.chat, `🧀zo`, m, null, fake)
-}
-
-handler.customPrefix = /^(Que|que|qe|ke|Qe|k|Ke|Kee|Quee)$/i
-handler.command = new RegExp
-export default handler
